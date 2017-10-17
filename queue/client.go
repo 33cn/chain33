f9f3c74badbbed881227bcb2d37bf119dad8d0d5
king wang <kingwang@kingdeMacBook-Pro.local>
2017-10-17 23:13:49 +0800

    fix bug: queue reply

--- queue/client.go
+++ queue/client.go
@@ -19,97 +19,62 @@ import (
 
 type IClient interface {
 	Send(msg Message, wait bool) (err error) //异步发送消息
-	Wait(Id int64) (Message, error)          //等待消息处理完成
+	Wait(msg Message) (Message, error)       //等待消息处理完成
 	Recv() chan Message
 	Sub(topic string) //订阅消息
-	NewMessage(topic string, ty int64, parentId int64, data interface{}) (msg Message)
+	NewMessage(topic string, ty int64, data interface{}) (msg Message)
 }
 
 type Client struct {
-	q     *Queue
-	waits map[int64]chan Message
-	topic string
-	id    int64
-	mu    sync.Mutex
+	q    *Queue
+	recv chan Message
+	id   int64
+	mu   sync.Mutex
 }
 
 func newClient(q *Queue) IClient {
 	client := &Client{}
 	client.q = q
-	client.waits = make(map[int64]chan Message)
-	client.topic = ""
+	client.recv = make(chan Message, DefaultChanBuffer)
 	return client
-}
-
-func (client *Client) setWait(id int64) {
-	client.mu.Lock()
-	defer client.mu.Unlock()
-	client.waits[id] = make(chan Message, 1)
-}
-
-func (client *Client) delWait(id int64) {
-	client.mu.Lock()
-	defer client.mu.Unlock()
-	delete(client.waits, id)
-}
-
-func (client *Client) getWait(id int64) (chan Message, error) {
-	client.mu.Lock()
-	defer client.mu.Unlock()
-
-	ch, ok := client.waits[id]
-	if ok {
-		return ch, nil
-	}
-	return nil, errors.New("No Wait Id")
 }
 
 //1. 系统保证send出去的消息就是成功了，除非系统崩溃
 //2. 系统保证每个消息都有对应的 response 消息
 func (client *Client) Send(msg Message, wait bool) (err error) {
-	if wait {
-		client.setWait(msg.Id)
+	if !wait {
+		msg.ChReply = nil
 	}
 	client.q.Send(msg)
 	return nil
 }
 
-func (client *Client) NewMessage(topic string, ty int64, parentId int64, data interface{}) (msg Message) {
+func (client *Client) NewMessage(topic string, ty int64, data interface{}) (msg Message) {
 	msg.Id = atomic.AddInt64(&client.id, 1)
-	msg.ParentId = parentId
+	msg.Ty = ty
 	msg.Data = data
 	msg.Topic = topic
+	msg.ChReply = make(chan Message, 1)
 	return msg
 }
 
-func (client *Client) Wait(id int64) (Message, error) {
-	defer client.delWait(id)
-	ch, err := client.getWait(id)
-	if err != nil {
-		return Message{}, err
+func (client *Client) Wait(msg Message) (Message, error) {
+	if msg.ChReply == nil {
+		return Message{}, errors.New("empty wait channel")
 	}
-	msg := <-ch
+	msg = <-msg.ChReply
 	return msg, nil
 }
 
 func (client *Client) Recv() chan Message {
-	if client.topic == "" {
-		panic("client not sub anything")
-	}
-	chrecv, _ := client.q.getChannel(client.topic)
-	return chrecv
+	return client.recv
 }
 
 func (client *Client) Sub(topic string) {
-	_, reply := client.q.getChannel(topic)
-	client.topic = topic
+	recv := client.q.getChannel(topic)
 	go func() {
-		for msg := range reply {
-			ch, err := client.getWait(msg.ParentId)
-			if err != nil {
-				continue
-			}
-			ch <- msg
+		for msg := range recv {
+			client.recv <- msg
 		}
 	}()
 }
