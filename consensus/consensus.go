ab963bbc0b7ee624f3591163438ec1461c05d5a7
fkeitxflf <fkeitxflf@gmail.com>
2017-10-19 00:30:28 +0800

    开发中，阶段上传

---
+++ consensus/consensus.go
@@ -0,0 +1,21 @@
+package consensus
+
+import "code.aliyun.com/chain33/chain33/consensus/solo"
+import "code.aliyun.com/chain33/chain33/queue"
+
+// 从mempool获取消息接口
+type Consumer interface {
+	SetQueue(q *queue.Queue)
+	RequestTx(txNum int64) queue.Message
+}
+
+// 查询区块相关信息接口
+type ReadLeadger interface {
+	GetPreBlock()
+	QueryTransaction()
+}
+
+// 广播消息接口
+type Communicator interface {
+	BroadcastBlock() error
+}
