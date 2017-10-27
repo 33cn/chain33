f7fe825382d226b6ccdfb56f54a050ff69d7f62a
king wang <kingwang@kingdeMacBook-Pro.local>
2017-10-27 15:30:52 +0800

    add transaction hash

--- common/hash.go
+++ common/hash.go
@@ -1,6 +1,11 @@
 package common
 
-import "encoding/hex"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"math/big"
+)
 
 const (
 	HashLength = 32
@@ -69,4 +74,65 @@ func hexEncode(b []byte) string {
 	copy(enc, "0x")
 	hex.Encode(enc[2:], b)
 	return string(enc)
+}
+
+func ToHex(b []byte) string {
+	hex := Bytes2Hex(b)
+	// Prefer output of "0x0" instead of "0x"
+	if len(hex) == 0 {
+		hex = "0"
+	}
+	return "0x" + hex
+}
+
+func FromHex(s string) []byte {
+	if len(s) > 1 {
+		if s[0:2] == "0x" || s[0:2] == "0X" {
+			s = s[2:]
+		}
+		if len(s)%2 == 1 {
+			s = "0" + s
+		}
+		return Hex2Bytes(s)
+	}
+	return nil
+}
+
+// Copy bytes
+//
+// Returns an exact copy of the provided bytes
+func CopyBytes(b []byte) (copiedBytes []byte) {
+	if b == nil {
+		return nil
+	}
+	copiedBytes = make([]byte, len(b))
+	copy(copiedBytes, b)
+
+	return
+}
+
+func HasHexPrefix(str string) bool {
+	l := len(str)
+	return l >= 2 && str[0:2] == "0x"
+}
+
+func IsHex(str string) bool {
+	l := len(str)
+	return l >= 4 && l%2 == 0 && str[0:2] == "0x"
+}
+
+func Bytes2Hex(d []byte) string {
+	return hex.EncodeToString(d)
+}
+
+func Sha256(b []byte) Hash {
+	return sha256.Sum256(b)
+}
+
+func Hex2Bytes(str string) []byte {
+	h, err := hex.DecodeString(str)
+	if err != nil {
+		panic(err)
+	}
+	return h
 }
