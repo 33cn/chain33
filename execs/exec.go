74ae3e2c120303154e2d69f8b94e985da9bc98e7
kingwang <kingwang@kingwangdeiMac.local>
2017-11-29 13:17:13 +0800

    add transafer

--- execs/exec.go
+++ execs/exec.go
@@ -2,11 +2,10 @@ package execs
 
 //store package store the world - state data
 import (
-	"fmt"
-
 	"code.aliyun.com/chain33/chain33/common"
-	dbm "code.aliyun.com/chain33/chain33/common/db"
-	_ "code.aliyun.com/chain33/chain33/execs/coins"
+	"code.aliyun.com/chain33/chain33/execs/execdrivers"
+	_ "code.aliyun.com/chain33/chain33/execs/execdrivers/coins"
+	_ "code.aliyun.com/chain33/chain33/execs/execdrivers/none"
 	"code.aliyun.com/chain33/chain33/queue"
 	"code.aliyun.com/chain33/chain33/types"
 	log "github.com/inconshreveable/log15"
@@ -67,34 +66,6 @@ func (exec *Execs) SetQueue(q *queue.Queue) {
 	}()
 }
 
-type Executer interface {
-	SetDB(dbm.KVDB)
-	Exec(tx *types.Transaction) *types.Receipt
-}
-
-var (
-	drivers = make(map[string]Executer)
-)
-
-func Register(name string, driver Executer) {
-	if driver == nil {
-		panic("Execute: Register driver is nil")
-	}
-	if _, dup := drivers[name]; dup {
-		panic("Execute: Register called twice for driver " + name)
-	}
-	drivers[name] = driver
-}
-
-func LoadExecute(name string) (c Executer, err error) {
-	c, ok := drivers[name]
-	if !ok {
-		err = fmt.Errorf("unknown driver %q", name)
-		return
-	}
-	return c, nil
-}
-
 //执行器 -> db 环境
 type Execute struct {
 	cache map[string][]byte
@@ -106,9 +77,9 @@ func NewExecute(stateHash []byte, q *queue.Queue) *Execute {
 }
 
 func (e *Execute) Exec(tx *types.Transaction) *types.Receipt {
-	exec, err := LoadExecute(string(tx.Execer))
+	exec, err := execdrivers.LoadExecute(string(tx.Execer))
 	if err != nil {
-		exec, err = LoadExecute("none")
+		exec, err = execdrivers.LoadExecute("none")
 		if err != nil {
 			panic(err)
 		}
