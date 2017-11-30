2337ba4985f08cf0ddf65184f40643bea586c9f9
kingwang <kingwang@kingwangdeiMac.local>
2017-11-30 17:18:10 +0800

    exec genesis block error

--- main.go
+++ main.go
@@ -10,14 +10,16 @@ package main
 import (
 	"code.aliyun.com/chain33/chain33/blockchain"
 	"code.aliyun.com/chain33/chain33/consensus"
+	"code.aliyun.com/chain33/chain33/execs"
 	"code.aliyun.com/chain33/chain33/mempool"
 	"code.aliyun.com/chain33/chain33/p2p"
 	"code.aliyun.com/chain33/chain33/queue"
 	"code.aliyun.com/chain33/chain33/rpc"
+	"code.aliyun.com/chain33/chain33/store"
 	log "github.com/inconshreveable/log15"
 )
 
-const Version = "v0.0.1"
+const Version = "v0.0.2"
 
 func main() {
 	//channel, rabitmq 等
@@ -29,14 +31,24 @@ func main() {
 	chain := blockchain.New()
 	chain.SetQueue(q)
 
-	log.Info("loading consensus module")
-	consensus.New("solo", q)
-
+	log.Info("loading mempool module")
 	mem := mempool.New()
 	mem.SetQueue(q)
 
+	log.Info("loading p2p module")
 	network := p2p.New()
 	network.SetQueue(q)
+
+	log.Info("loading execs module")
+	exec := execs.New()
+	exec.SetQueue(q)
+
+	log.Info("loading store module")
+	s := store.New()
+	s.SetQueue(q)
+
+	log.Info("loading consensus module")
+	consensus.New("solo", q)
 
 	//jsonrpc, grpc, channel 三种模式
 	api := rpc.NewServer("jsonrpc", ":8801")
