74467b961ed368c66208d30c563a41a839695359
helpylee <helpylee@126.com>
2017-12-15 01:04:11 -0800

    更新p2p,rpc

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -52,26 +52,27 @@ func (network *P2p) subP2pMsg() {
 	network.c.Sub("p2p")
 	go func() {
 		for msg := range network.c.Recv() {
+			log.Debug("SubP2pMsg", "Ty", msg.Ty)
 			switch msg.Ty {
 			case types.EventTxBroadcast: //广播tx
 				log.Debug("QUEUE P2P EventTxBroadcast", "Recv from mempool message EventTxBroadcast will broadcast outnet")
 				intrans := NewInTrans(network)
-				go intrans.TransToBroadCast(&msg)
+				go intrans.TransToBroadCast(msg)
 			case types.EventBlockBroadcast: //广播block
 				intrans := NewInTrans(network)
-				go intrans.BlockBroadcast(&msg)
+				go intrans.BlockBroadcast(msg)
 				//case types.EventGetBlocks:
 				//intrans := NewInTrans(network)
 				//go intrans.GetBlocks(&msg)
 			case types.EventFetchBlocks:
 				intrans := NewInTrans(network)
-				go intrans.GetBlocks(&msg)
+				go intrans.GetBlocks(msg)
 			case types.EventGetMempool:
 				intrans := NewInTrans(network)
-				go intrans.GetMemPool(&msg)
+				go intrans.GetMemPool(msg)
 			case types.EventPeerInfo:
 				intrans := NewInTrans(network)
-				go intrans.GetPeerInfo(&msg)
+				go intrans.GetPeerInfo(msg)
 
 			default:
 				log.Error("unknown msgtype:", msg.Ty)
