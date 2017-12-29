0bab9a0e0f408d5817715b575a8da11a06480882
RENS <lauren9292@163.com>
2017-12-29 11:43:15 +0800

    添加判断哈希字符串长度

--- cli/loopSendTx.sh
+++ cli/loopSendTx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for((i=0;i<1000000;i++))
 do
-./cli sendtoaddress 14KEKbYtKKQm4wMthSK9J4La4nAiidGozt 1JpeYVxmbagmvChJRaBkybUpFjBvwHKF4H 1 ceshi
+./cli sendtoaddress 14KEKbYtKKQm4wMthSK9J4La4nAiidGozt 1DaetjMf4MwnJ2W33iMcJZwh5agNU9Jy3d 1 ceshi
 echo "."
 done
