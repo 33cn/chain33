b954a0f6faeb8d5d55ddb0e4beedc4912a755743
king wang <kingwang@kingdeMacBook-Pro.local>
2018-01-04 13:03:09 +0800

    and benchmark for store

---
+++ store/store_test.go
@@ -0,0 +1,108 @@
+package store
+
+import (
+	"fmt"
+	"testing"
+
+	"code.aliyun.com/chain33/chain33/common"
+	"code.aliyun.com/chain33/chain33/common/config"
+	"code.aliyun.com/chain33/chain33/queue"
+	"code.aliyun.com/chain33/chain33/types"
+)
+
+func init() {
+	common.SetLogLevel("warn")
+}
+
+func initEnv() (*queue.Queue, *Store) {
+	var q = queue.New("channel")
+	cfg := config.InitCfg("chain33.toml")
+	s := New(cfg.Store)
+	s.SetQueue(q)
+	return q, s
+}
+
+func set(qclient queue.IClient, hash, key, value []byte) ([]byte, error) {
+	kv := &types.KeyValue{key, value}
+	set := &types.StoreSet{}
+	set.StateHash = hash
+	set.KV = append(set.KV, kv)
+
+	msg := qclient.NewMessage("store", types.EventStoreSet, set)
+	qclient.Send(msg, true)
+	msg, err := qclient.Wait(msg)
+	if err != nil {
+		return nil, err
+	}
+	return msg.GetData().(*types.ReplyHash).GetHash(), nil
+}
+
+func get(qclient queue.IClient, hash, key []byte) ([]byte, error) {
+	query := &types.StoreGet{hash, [][]byte{key}}
+	msg := qclient.NewMessage("store", types.EventStoreGet, query)
+	qclient.Send(msg, true)
+	msg, err := qclient.Wait(msg)
+	if err != nil {
+		return nil, err
+	}
+	values := msg.GetData().(*types.StoreReplyValue).GetValues()
+	return values[0], nil
+}
+
+func TestGetAndSet(t *testing.T) {
+	q, s := initEnv()
+	qclient := q.GetClient()
+	var stateHash [32]byte
+	//先set一个数
+	key := []byte("hello")
+	value := []byte("world")
+
+	hash, err := set(qclient, stateHash[:], key, value)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	value2, err := get(qclient, hash, key)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if string(value2) != string(value) {
+		t.Errorf("values not match")
+		return
+	}
+	s.Close()
+}
+
+func BenchmarkGetKey(b *testing.B) {
+	q, s := initEnv()
+	qclient := q.GetClient()
+	var stateHash [32]byte
+	hash := stateHash[:]
+	var err error
+	for i := 0; i < 10000; i++ {
+		key := []byte(fmt.Sprintf("%020d", i))
+		value := []byte(fmt.Sprintf("%020d", i))
+		hash, err = set(qclient, hash, key, value)
+		if err != nil {
+			b.Error(err)
+			return
+		}
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		key := []byte(fmt.Sprintf("%020d", i%10000))
+		value := fmt.Sprintf("%020d", i%10000)
+		value2, err := get(qclient, hash, key)
+		if err != nil {
+			b.Error(err)
+			return
+		}
+		if string(value2) != value {
+			b.Error(err)
+			return
+		}
+	}
+	s.Close()
+}
