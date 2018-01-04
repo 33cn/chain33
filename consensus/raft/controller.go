b8c4ca190b544499bba2a03ebec89760cbcf482f
lihailei <18761806026@163.com>
2018-01-04 13:53:17 +0800

    将raft传入参数改为从配置文件chain33.toml中读取
    Former-commit-id: 5fc609730777f909c97725e486ddc31098067a7b

--- consensus/raft/controller.go
+++ consensus/raft/controller.go
@@ -22,9 +22,11 @@ func NewRaftCluster(cfg *types.Consensus) *RaftClient {
 	}
 	rlog.Info("Start to create raft cluster")
 
-	// TODO:目前单节点测试用，需要改成从toml文件中读取配置
-	urls := "http://127.0.0.1:9021"
-	nodeId := 1
+	if int(cfg.NodeId) == 0 || strings.Compare(cfg.PeersURL, "") == 0 {
+		rlog.Error("Please check whether the configuration of nodeId and peersURL is empty!")
+		//TODO 当传入的参数异常时，返回给主函数的是个nil,这时候需要做异常处理
+		return nil
+	}
 	// validator节点（客户端），此节点用来处理区块打包等工作，并把区块发往raft集群来做共识
 	// 相对于raft集群来说，发送区块的是客户端，客户端不用考虑raft的leader节点是哪个
 	// 就算发给follower也会自动移交给leader。
@@ -38,7 +40,7 @@ func NewRaftCluster(cfg *types.Consensus) *RaftClient {
 
 	// raft集群的建立,1. 初始化两条channel： propose channel用于客户端和raft底层交互, commit channel用于获取commit消息
 	// 2. raft集群中的节点之间建立http连接
-	commitC, errorC, snapshotterReady := NewRaft(nodeId, strings.Split(urls, ","), getSnapshot, proposeC, confChangeC)
+	commitC, errorC, snapshotterReady := NewRaft(int(cfg.NodeId), strings.Split(cfg.PeersURL, ","), getSnapshot, proposeC, confChangeC)
 
 	// 监听commit channel,取block
 	b = NewBlockstore(<-snapshotterReady, proposeC, commitC, errorC)
