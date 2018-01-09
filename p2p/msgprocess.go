83c60dda7efb728734dfd8c347eb4eaa0230f32b
helpylee <helpylee@126.com>
2018-01-08 23:53:41 -0800

    修改grpc 配置

--- p2p/msgprocess.go
+++ p2p/msgprocess.go
@@ -303,24 +303,28 @@ func (m *Msg) downloadBlock(index int, interval *intervalInfo, invs *pb.P2PInv, 
 		log.Debug("downloadBlock", "interval invs", p2pdata.Invs)
 		//判断请求的节点的高度是否在节点的实际范围内
 		if index >= peersize {
+			log.Error("download", "index", index, "peersise", peersize)
 			continue
 		}
 
 		peer := peers[index]
 		if peer == nil {
 			index++
+			log.Error("download", "peer", "nil")
 			continue
 		}
 		if pinfo, ok := pinfos[peer.Addr()]; ok {
 			if pinfo.GetHeader().GetHeight() < int64(invs.Invs[interval.end].GetHeight()) {
 				index++
+				log.Error("download", "much height", pinfo.GetHeader().GetHeight(), "invs height", int64(invs.Invs[interval.end].GetHeight()))
 				continue
 			}
 		} else {
+			log.Error("download", "pinfo", "no this addr", peer.Addr())
 			index++
 			continue
 		}
-		log.Error("downloadBlock", "index", index, "peersize", peersize, "peeraddr", peer.Addr())
+		log.Error("downloadBlock", "index", index, "peersize", peersize, "peeraddr", peer.Addr(), "p2pdata", p2pdata)
 		resp, err := peer.mconn.conn.GetData(context.Background(), &p2pdata)
 		if err != nil {
 			log.Error("downloadBlock", "GetData err", err.Error())
@@ -332,6 +336,7 @@ func (m *Msg) downloadBlock(index int, interval *intervalInfo, invs *pb.P2PInv, 
 		invdatas, err := resp.Recv()
 		if err != nil && err != io.EOF {
 			index++
+			log.Error("download", "resp,Recv err", err.Error())
 			resp.CloseSend()
 			continue
 		}
@@ -340,9 +345,13 @@ func (m *Msg) downloadBlock(index int, interval *intervalInfo, invs *pb.P2PInv, 
 			len(invdatas.GetItems()) == interval.end-interval.start+1 {
 			maxInvDatas = invdatas
 			resp.CloseSend()
-			log.Debug("download", "success,invs", p2pdata.Invs)
+			log.Error("download", "success,invs", p2pdata.Invs)
 			break
 		}
+		if len(invdatas.GetItems()) == 0 {
+			log.Error("download", "getinvsdata.Item", 0)
+		}
+		log.Error("download", "download err", invdatas.GetItems())
 		resp.CloseSend()
 	}
 
