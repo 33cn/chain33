36af1e20002870df8f574df8410278f80c9d8005
weijixing <wjx@localhost.localdomain>
2018-01-11 20:35:45 -0800

    add hashlock

--- execs/exec.go
+++ execs/exec.go
@@ -6,6 +6,7 @@ import (
 	"code.aliyun.com/chain33/chain33/common"
 	"code.aliyun.com/chain33/chain33/execs/execdrivers"
 	_ "code.aliyun.com/chain33/chain33/execs/execdrivers/coins"
+	_ "code.aliyun.com/chain33/chain33/execs/execdrivers/hashlock"
 	_ "code.aliyun.com/chain33/chain33/execs/execdrivers/none"
 	_ "code.aliyun.com/chain33/chain33/execs/execdrivers/ticket"
 	"code.aliyun.com/chain33/chain33/queue"
