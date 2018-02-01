3fd372e6f70fa3ccbb914e4b7549bab903883728
king wang <kingwang@kingdeMacBook-Pro.local>
2018-02-01 13:06:11 +0800

    queue db add statedb

--- execs/exec.go
+++ execs/exec.go
@@ -214,8 +214,8 @@ type Execute struct {
 
 func NewExecute(stateHash []byte, q *queue.Queue, height, blocktime int64) *Execute {
 	return &Execute{
-		stateDB:   NewStateDB(q, stateHash),
-		localDB:   NewLocalDB(q),
+		stateDB:   execdrivers.NewStateDB(q, stateHash),
+		localDB:   execdrivers.NewLocalDB(q),
 		height:    height,
 		blocktime: blocktime,
 	}
@@ -311,103 +311,4 @@ func (e *Execute) ExecDelLocal(tx *types.Transaction, r *types.ReceiptData, inde
 	exec.SetLocalDB(e.localDB)
 	exec.SetEnv(e.height, e.blocktime)
 	return exec.ExecDelLocal(tx, r, index)
-}
-
-type StateDB struct {
-	cache map[string][]byte
-	db    *DataBase
-}
-
-func NewStateDB(q *queue.Queue, stateHash []byte) *StateDB {
-	return &StateDB{make(map[string][]byte), NewDataBase(q, stateHash)}
-}
-
-func (e *StateDB) Get(key []byte) (value []byte, err error) {
-	if value, ok := e.cache[string(key)]; ok {
-		return value, nil
-	}
-	value, err = e.db.Get(key)
-	if err != nil {
-		return nil, err
-	}
-	e.cache[string(key)] = value
-	return value, nil
-}
-
-func (e *StateDB) Set(key []byte, value []byte) error {
-	e.cache[string(key)] = value
-	return nil
-}
-
-type LocalDB struct {
-	cache map[string][]byte
-	db    *DataBaseLocal
-}
-
-func NewLocalDB(q *queue.Queue) *LocalDB {
-	return &LocalDB{make(map[string][]byte), NewDataBaseLocal(q)}
-}
-
-func (e *LocalDB) Get(key []byte) (value []byte, err error) {
-	if value, ok := e.cache[string(key)]; ok {
-		return value, nil
-	}
-	value, err = e.db.Get(key)
-	if err != nil {
-		return nil, err
-	}
-	e.cache[string(key)] = value
-	return value, nil
-}
-
-func (e *LocalDB) Set(key []byte, value []byte) error {
-	e.cache[string(key)] = value
-	return nil
-}
-
-type DataBase struct {
-	qclient   queue.IClient
-	stateHash []byte
-}
-
-func NewDataBase(q *queue.Queue, stateHash []byte) *DataBase {
-	return &DataBase{q.GetClient(), stateHash}
-}
-
-func (db *DataBase) Get(key []byte) (value []byte, err error) {
-	query := &types.StoreGet{db.stateHash, [][]byte{key}}
-	msg := db.qclient.NewMessage("store", types.EventStoreGet, query)
-	db.qclient.Send(msg, true)
-	resp, err := db.qclient.Wait(msg)
-	if err != nil {
-		panic(err) //no happen for ever
-	}
-	value = resp.GetData().(*types.StoreReplyValue).Values[0]
-	if value == nil {
-		return nil, types.ErrNotFound
-	}
-	return value, nil
-}
-
-type DataBaseLocal struct {
-	qclient queue.IClient
-}
-
-func NewDataBaseLocal(q *queue.Queue) *DataBaseLocal {
-	return &DataBaseLocal{q.GetClient()}
-}
-
-func (db *DataBaseLocal) Get(key []byte) (value []byte, err error) {
-	query := &types.LocalDBGet{[][]byte{key}}
-	msg := db.qclient.NewMessage("blockchain", types.EventLocalGet, query)
-	db.qclient.Send(msg, true)
-	resp, err := db.qclient.Wait(msg)
-	if err != nil {
-		panic(err) //no happen for ever
-	}
-	value = resp.GetData().(*types.LocalReplyValue).Values[0]
-	if value == nil {
-		return nil, types.ErrNotFound
-	}
-	return value, nil
 }
