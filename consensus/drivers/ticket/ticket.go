7b4cefda67f0a197daee710e655d20d06ef857f1
king wang <kingwang@kingdeMacBook-Pro.local>
2018-02-05 00:37:35 +0800

    ticket miner ok

--- consensus/drivers/ticket/ticket.go
+++ consensus/drivers/ticket/ticket.go
@@ -3,13 +3,18 @@ package ticket
 import (
 	"fmt"
 	"math/big"
+	"sync"
 	"time"
 
+	"code.aliyun.com/chain33/chain33/account"
 	"code.aliyun.com/chain33/chain33/common"
+	"code.aliyun.com/chain33/chain33/common/crypto"
 	"code.aliyun.com/chain33/chain33/common/merkle"
 	"code.aliyun.com/chain33/chain33/consensus/drivers"
 	"code.aliyun.com/chain33/chain33/execs/execdrivers"
+	"code.aliyun.com/chain33/chain33/queue"
 	"code.aliyun.com/chain33/chain33/types"
+	"code.aliyun.com/chain33/chain33/util"
 	log "github.com/inconshreveable/log15"
 )
 
@@ -18,12 +23,14 @@ var slog = log.New("module", "ticket")
 type TicketClient struct {
 	*drivers.BaseClient
 	//ticket list for miner
-	tlist *types.TicketList
+	tlist   *types.ReplyTicketList
+	privmap map[string]crypto.PrivKey
+	mu      sync.Mutex
 }
 
 func New(cfg *types.Consensus) *TicketClient {
 	c := drivers.NewBaseClient(cfg)
-	t := &TicketClient{c, &types.TicketList{}}
+	t := &TicketClient{c, &types.ReplyTicketList{}, nil, sync.Mutex{}}
 	c.SetChild(t)
 	return t
 }
@@ -69,6 +76,61 @@ func (client *TicketClient) CreateGenesisTx() (ret []*types.Transaction) {
 	return
 }
 
+func (client *TicketClient) ProcEvent(msg queue.Message) {
+	if msg.Ty == types.EventFlushTicket {
+		client.flushTicket(msg)
+	}
+}
+
+func (client *TicketClient) privFromBytes(privkey []byte) (crypto.PrivKey, error) {
+	cr, err := crypto.New(types.GetSignatureTypeName(types.SECP256K1))
+	if err != nil {
+		return nil, err
+	}
+	return cr.PrivKeyFromBytes(privkey)
+}
+
+func (client *TicketClient) getTickets() ([]*types.Ticket, []crypto.PrivKey, error) {
+	msg := client.GetQueueClient().NewMessage("wallet", types.EventWalletGetTickets, nil)
+	client.GetQueueClient().Send(msg, true)
+	resp, err := client.GetQueueClient().Wait(msg)
+	if err != nil {
+		return nil, nil, err
+	}
+	reply := resp.GetData().(types.Message).(*types.ReplyWalletTickets)
+	var keys []crypto.PrivKey
+	for i := 0; i < len(reply.Privkeys); i++ {
+		priv, err := client.privFromBytes(reply.Privkeys[i])
+		if err != nil {
+			return nil, nil, err
+		}
+		keys = append(keys, priv)
+	}
+	return reply.Tickets, keys, nil
+}
+
+func (client *TicketClient) flushTicket(msg queue.Message) {
+	//list accounts
+	tickets, privs, err := client.getTickets()
+	if err != nil {
+		msg.ReplyErr("FlushTicket", err)
+		return
+	}
+	client.mu.Lock()
+	client.privmap = getPrivMap(privs)
+	client.tlist.Tickets = tickets
+	client.mu.Unlock()
+}
+
+func getPrivMap(privs []crypto.PrivKey) map[string]crypto.PrivKey {
+	list := make(map[string]crypto.PrivKey)
+	for _, priv := range privs {
+		addr := account.PubKeyToAddress(priv.PubKey().Bytes()).String()
+		list[addr] = priv
+	}
+	return list
+}
+
 func (client *TicketClient) CheckBlock(parent *types.Block, current *types.BlockDetail) error {
 	//检查第一个笔交易的execs, 以及执行状态
 	if len(current.Block.Txs) == 0 {
@@ -96,10 +158,14 @@ func (client *TicketClient) CheckBlock(parent *types.Block, current *types.Block
 	if miner.Reward != types.CoinReward {
 		return types.ErrCoinbaseReward
 	}
-	//通过判断区块的难度difficult
-	diff := client.getNextTarget(parent, miner.Bits)
+	if miner.Bits != current.Block.Difficulty {
+		return types.ErrBlockHeaderDifficulty
+	}
+	//通过判断区块的难度Difficulty
+	bits := parent.Difficulty
+	diff := client.getNextTarget(parent, bits)
 	currentdiff := client.getCurrentTarget(current.Block.BlockTime, miner.TicketId, miner.Modify)
-	if currentdiff.Cmp(CompactToBig(miner.Bits)) != 0 {
+	if currentdiff.Cmp(common.CompactToBig(miner.Bits)) != 0 {
 		return types.ErrCoinBaseTarget
 	}
 	if currentdiff.Cmp(diff) > 0 {
@@ -113,13 +179,13 @@ func (client *TicketClient) getNextTarget(block *types.Block, bits uint32) *big.
 	if err != nil {
 		panic(err)
 	}
-	return CompactToBig(targetBits)
+	return common.CompactToBig(targetBits)
 }
 
 func (client *TicketClient) getCurrentTarget(blocktime int64, id string, modify []byte) *big.Int {
 	s := fmt.Sprint("%d:%s:%x", blocktime, id, modify)
 	hash := common.Sha2Sum([]byte(s))
-	return HashToBig(hash[:])
+	return common.HashToBig(hash[:])
 }
 
 // calcNextRequiredDifficulty calculates the required difficulty for the block
@@ -170,7 +236,7 @@ func (client *TicketClient) GetNextRequiredDifficulty(block *types.Block, bits u
 	// The result uses integer division which means it will be slightly
 	// rounded down.  Bitcoind also uses integer division to calculate this
 	// result.
-	oldTarget := CompactToBig(bits)
+	oldTarget := common.CompactToBig(bits)
 	newTarget := new(big.Int).Mul(oldTarget, big.NewInt(adjustedTimespan))
 	targetTimeSpan := int64(types.TargetTimespan / time.Second)
 	newTarget.Div(newTarget, big.NewInt(targetTimeSpan))
@@ -184,15 +250,57 @@ func (client *TicketClient) GetNextRequiredDifficulty(block *types.Block, bits u
 	// intentionally converting the bits back to a number instead of using
 	// newTarget since conversion to the compact representation loses
 	// precision.
-	newTargetBits := BigToCompact(newTarget)
+	newTargetBits := common.BigToCompact(newTarget)
 	slog.Info("Difficulty retarget at ", "block height %d", block.Height+1)
 	slog.Info("Old target ", "%08x", bits, "(%064x)", oldTarget)
-	slog.Info("New target ", "%08x", newTargetBits, "(%064x)", CompactToBig(newTargetBits))
+	slog.Info("New target ", "%08x", newTargetBits, "(%064x)", common.CompactToBig(newTargetBits))
 	slog.Info("Timespan", "Actual timespan", time.Duration(actualTimespan)*time.Second,
 		"adjusted timespan", time.Duration(adjustedTimespan)*time.Second,
 		"target timespan", types.TargetTimespan)
 
 	return newTargetBits, nil
+}
+
+func (client *TicketClient) Miner(block *types.Block) bool {
+	//add miner address
+	parent := client.GetCurrentBlock()
+	bits := parent.Difficulty
+	diff := client.getNextTarget(parent, bits)
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	for i := 0; i < len(client.tlist.Tickets); i++ {
+		ticket := client.tlist.Tickets[i]
+		//已经到成熟器
+		if !ticket.IsGenesis && block.BlockTime-ticket.CreateTime <= 10*86400 {
+			continue
+		}
+		//find priv key
+		priv := client.privmap[ticket.MinerAddress]
+		currentdiff := client.getCurrentTarget(block.BlockTime, ticket.TicketId, []byte("modify"))
+		if currentdiff.Cmp(diff) > 0 { //难度要大于前一个，注意数字越小难度越大
+			continue
+		}
+		var ticketAction types.TicketAction
+		miner := &types.TicketMiner{}
+		miner.TicketId = ticket.TicketId
+		miner.Bits = common.BigToCompact(currentdiff)
+		miner.Modify = []byte("modify")
+		miner.Reward = types.CoinReward
+		ticketAction.Value = &types.TicketAction_Miner{miner}
+		//构造transaction
+		tx := &types.Transaction{}
+		tx.Execer = []byte("ticket")
+		tx.Fee = types.MinFee
+		tx.Nonce = client.RandInt64()
+		tx.To = account.ExecAddress("ticket").String()
+		tx.Payload = types.Encode(&ticketAction)
+		tx.Sign(types.SECP256K1, priv)
+		//unshift
+		block.Difficulty = miner.Bits
+		block.Txs = append([]*types.Transaction{tx}, block.Txs...)
+		return true
+	}
+	return false
 }
 
 func (client *TicketClient) CreateBlock() {
@@ -218,15 +326,36 @@ func (client *TicketClient) CreateBlock() {
 		newblock.ParentHash = lastBlock.Hash()
 		newblock.Height = lastBlock.Height + 1
 		newblock.Txs = txs
-		newblock.TxHash = merkle.CalcMerkleRoot(newblock.Txs)
 		newblock.BlockTime = time.Now().Unix()
 		if lastBlock.BlockTime >= newblock.BlockTime {
 			newblock.BlockTime = lastBlock.BlockTime + 1
 		}
+		//add miner tx
+		if !client.Miner(&newblock) {
+			continue
+		}
+		newblock.TxHash = merkle.CalcMerkleRoot(newblock.Txs)
 		err := client.WriteBlock(lastBlock.StateHash, &newblock)
 		if err != nil {
 			issleep = true
 			continue
 		}
 	}
+}
+
+func (client *TicketClient) ExecBlock(prevHash []byte, block *types.Block) (*types.BlockDetail, error) {
+	//exec block
+	blockdetail, err := util.ExecBlock(client.GetQueue(), prevHash, block, false)
+	if err != nil { //never happen
+		return nil, err
+	}
+	if len(blockdetail.Block.Txs) == 0 {
+		return nil, types.ErrNoTx
+	}
+	//判断txs[0] 是否执行OK
+	err = client.CheckBlock(client.GetCurrentBlock(), blockdetail)
+	if err != nil { //never happen
+		return nil, err
+	}
+	return blockdetail, nil
 }
