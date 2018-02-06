202073b7ea4351b377de87f111751aa94f71ca2b
suyanlong <592938410@qq.com>
2018-02-06 15:32:27 +0800

    refactor BlockStore: 1. rename method 2. instead store with blockstore name 3. remove mutex in BlockStore structure

--- blockchain/blockstore.go
+++ blockchain/blockstore.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"sync"
+	"sync/atomic"
 
 	dbm "code.aliyun.com/chain33/chain33/common/db"
 	"code.aliyun.com/chain33/chain33/queue"
@@ -13,39 +13,34 @@ import (
 )
 
 var blockStoreKey = []byte("blockStoreHeight")
-var storelog = chainlog.New("submodule", "store")
+var storeLog = chainlog.New("submodule", "store")
 var MaxTxsPerBlock int64 = 100000
 
 type BlockStore struct {
-	db      dbm.DB
-	mtx     sync.RWMutex
-	qclient queue.Client
-	height  int64
+	db     dbm.DB
+	client queue.Client
+	height int64
 }
 
 func NewBlockStore(db dbm.DB, q *queue.Queue) *BlockStore {
 	height := LoadBlockStoreHeight(db)
 	return &BlockStore{
-		height:  height,
-		db:      db,
-		qclient: q.NewClient(),
+		height: height,
+		db:     db,
+		client: q.NewClient(),
 	}
 }
 
 // 返回BlockStore保存的当前block高度
 func (bs *BlockStore) Height() int64 {
-	bs.mtx.RLock()
-	defer bs.mtx.RUnlock()
-	return bs.height
+	return atomic.LoadInt64(&bs.height)
 }
 
 // 更新db中的block高度到BlockStore.Height
 func (bs *BlockStore) UpdateHeight() {
 	height := LoadBlockStoreHeight(bs.db)
-	bs.mtx.Lock()
-	bs.height = height
-	bs.mtx.Unlock()
-	storelog.Info("UpdateHeight", "curblockheight", height)
+	atomic.StoreInt64(&bs.height, height)
+	storeLog.Info("UpdateHeight", "curblockheight", height)
 }
 
 func (bs *BlockStore) Get(keys *types.LocalDBGet) *types.LocalReplyValue {
@@ -59,65 +54,61 @@ func (bs *BlockStore) Get(keys *types.LocalDBGet) *types.LocalReplyValue {
 
 //从db数据库中获取指定高度的block信息
 func (bs *BlockStore) LoadBlock(height int64) *types.BlockDetail {
-
-	var blockdetail types.BlockDetail
-	blockbytes := bs.db.Get(calcBlockHeightKey(height))
-	if blockbytes == nil {
+	blockBytes := bs.db.Get(calcBlockHeightKey(height))
+	if blockBytes == nil {
 		return nil
 	}
-	err := proto.Unmarshal(blockbytes, &blockdetail)
+	var blockDetail types.BlockDetail
+	err := proto.Unmarshal(blockBytes, &blockDetail)
 	if err != nil {
-		storelog.Error("LoadBlock", "Could not unmarshal bytes:", blockbytes)
+		storeLog.Error("LoadBlock", "Could not unmarshal bytes:", blockBytes)
 		return nil
 	}
-	return &blockdetail
+	return &blockDetail
 }
 
 //  批量保存blocks信息到db数据库中
-func (bs *BlockStore) SaveBlock(storeBatch dbm.Batch, blockdetail *types.BlockDetail) error {
+func (bs *BlockStore) SaveBlock(storeBatch dbm.Batch, blockDetail *types.BlockDetail) error {
 
-	height := blockdetail.Block.Height
-	if len(blockdetail.Receipts) == 0 && len(blockdetail.Block.Txs) != 0 {
-		storelog.Error("SaveBlock Receipts is nil ", "height", blockdetail.Block.Height)
+	height := blockDetail.Block.Height
+	if len(blockDetail.Receipts) == 0 && len(blockDetail.Block.Txs) != 0 {
+		storeLog.Error("SaveBlock Receipts is nil ", "height", blockDetail.Block.Height)
 	}
 	// Save block
-	blockbytes, err := proto.Marshal(blockdetail)
+	blockBytes, err := proto.Marshal(blockDetail)
 	if err != nil {
-		storelog.Error("SaveBlock Could not Encode block", "height", blockdetail.Block.Height, "error", err)
+		storeLog.Error("SaveBlock Could not Encode block", "height", blockDetail.Block.Height, "error", err)
 		return err
 	}
-	storeBatch.Set(calcBlockHeightKey(height), blockbytes)
+	storeBatch.Set(calcBlockHeightKey(height), blockBytes)
 
-	bytes, err := json.Marshal(height)
-	if err != nil {
-		storelog.Error("SaveBlock  Could not marshal hight bytes", "error", err)
+	if bytes, err := json.Marshal(height); err != nil {
+		storeLog.Error("SaveBlock  Could not marshal hight bytes", "error", err)
 		return err
+	} else {
+		storeBatch.Set(blockStoreKey, bytes)
+		//存储block hash和height的对应关系，便于通过hash查询block
+		storeBatch.Set(calcBlockHashKey(blockDetail.Block.Hash()), bytes)
 	}
-	storeBatch.Set(blockStoreKey, bytes)
-
-	//存储block hash和height的对应关系，便于通过hash查询block
-	storeBatch.Set(calcBlockHashKey(blockdetail.Block.Hash()), bytes)
-
-	storelog.Debug("SaveBlock success", "blockheight", height)
+	storeLog.Debug("SaveBlock success", "blockheight", height)
 	return nil
 }
 
 // 批量删除block信息从db数据库中
-func (bs *BlockStore) DelBlock(storeBatch dbm.Batch, blockdetail *types.BlockDetail) error {
-	height := blockdetail.Block.Height
+func (bs *BlockStore) DelBlock(storeBatch dbm.Batch, blockDetail *types.BlockDetail) error {
+	height := blockDetail.Block.Height
 	// del block
 	storeBatch.Delete(calcBlockHeightKey(height))
 	//更新最新的block高度为前一个高度
-	bytes, err := json.Marshal(height - 1)
-	if err != nil {
-		storelog.Error("DelBlock  Could not marshal hight bytes", "error", err)
+	if bytes, err := json.Marshal(height - 1); err != nil {
+		storeLog.Error("DelBlock  Could not marshal hight bytes", "error", err)
 		return err
+	} else {
+		storeBatch.Set(blockStoreKey, bytes)
 	}
-	storeBatch.Set(blockStoreKey, bytes)
 	//删除block hash和height的对应关系
-	storeBatch.Delete(calcBlockHashKey(blockdetail.Block.Hash()))
-
-	storelog.Debug("DelBlock success", "blockheight", height)
+	storeBatch.Delete(calcBlockHashKey(blockDetail.Block.Hash()))
+	storeLog.Debug("DelBlock success", "blockheight", height)
 	return nil
 }
 
@@ -134,12 +125,12 @@ func (bs *BlockStore) GetTx(hash []byte) (*types.TxResult, error) {
 		return nil, err
 	}
 
-	var txresult types.TxResult
-	err := proto.Unmarshal(rawBytes, &txresult)
+	var txResult types.TxResult
+	err := proto.Unmarshal(rawBytes, &txResult)
 	if err != nil {
 		return nil, err
 	}
-	return &txresult, nil
+	return &txResult, nil
 }
 
 func (bs *BlockStore) NewBatch(sync bool) dbm.Batch {
@@ -148,10 +139,10 @@ func (bs *BlockStore) NewBatch(sync bool) dbm.Batch {
 }
 
 // 通过批量存储tx信息到db中
-func (bs *BlockStore) AddTxs(storeBatch dbm.Batch, blockdetail *types.BlockDetail) error {
-	kv, err := bs.getLocakKV(blockdetail)
+func (bs *BlockStore) AddTxs(storeBatch dbm.Batch, blockDetail *types.BlockDetail) error {
+	kv, err := bs.getLocalKV(blockDetail)
 	if err != nil {
-		storelog.Error("indexTxs getLocalKV err", "Height", blockdetail.Block.Height, "err", err)
+		storeLog.Error("indexTxs getLocalKV err", "Height", blockDetail.Block.Height, "err", err)
 		return err
 	}
 	for i := 0; i < len(kv.KV); i++ {
@@ -161,13 +152,13 @@ func (bs *BlockStore) AddTxs(storeBatch dbm.Batch, blockdetail *types.BlockDetai
 }
 
 //通过批量删除tx信息从db中
-func (bs *BlockStore) DelTxs(storeBatch dbm.Batch, blockdetail *types.BlockDetail) error {
+func (bs *BlockStore) DelTxs(storeBatch dbm.Batch, blockDetail *types.BlockDetail) error {
 	//存储key:addr:flag:height ,value:txhash
 	//flag :0-->from,1--> to
 	//height=height*10000+index 存储账户地址相关的交易
-	kv, err := bs.getDelLocakKV(blockdetail)
+	kv, err := bs.getDelLocalKV(blockDetail)
 	if err != nil {
-		storelog.Error("indexTxs getLocalKV err", "Height", blockdetail.Block.Height, "err", err)
+		storeLog.Error("indexTxs getLocalKV err", "Height", blockDetail.Block.Height, "err", err)
 		return err
 	}
 	for i := 0; i < len(kv.KV); i++ {
@@ -187,14 +178,14 @@ func calcBlockHashKey(hash []byte) []byte {
 
 //从db数据库中获取指定hash对应的block高度
 func (bs *BlockStore) GetHeightByBlockHash(hash []byte) int64 {
-	heightbytes := bs.db.Get(calcBlockHashKey(hash))
-	if heightbytes == nil {
+	data := bs.db.Get(calcBlockHashKey(hash))
+	if data == nil {
 		return -1
 	}
 	var height int64
-	err := json.Unmarshal(heightbytes, &height)
+	err := json.Unmarshal(data, &height)
 	if err != nil {
-		storelog.Error("GetHeightByBlockHash Could not unmarshal height bytes", "error", err)
+		storeLog.Error("GetHeightByBlockHash Could not unmarshal height data", "error", err)
 	}
 	return height
 }
@@ -207,7 +198,7 @@ func calcBlockHeightKey(height int64) []byte {
 func SaveBlockStoreHeight(db dbm.DB, height int64) {
 	bytes, err := json.Marshal(height)
 	if err != nil {
-		storelog.Error("SaveBlockStoreHeight  Could not marshal hight bytes", "error", err)
+		storeLog.Error("SaveBlockStoreHeight  Could not marshal hight bytes", "error", err)
 	}
 	db.SetSync(blockStoreKey, bytes)
 }
@@ -221,18 +212,18 @@ func LoadBlockStoreHeight(db dbm.DB) int64 {
 
 	err := json.Unmarshal(bytes, &height)
 	if err != nil {
-		storelog.Error("LoadBlockStoreHeight Could not unmarshal height bytes", "error", err)
+		storeLog.Error("LoadBlockStoreHeight Could not unmarshal height bytes", "error", err)
 	}
 	return height
 }
 
-func (bs *BlockStore) getLocakKV(detail *types.BlockDetail) (*types.LocalDBSet, error) {
-	if bs.qclient == nil {
+func (bs *BlockStore) getLocalKV(detail *types.BlockDetail) (*types.LocalDBSet, error) {
+	if bs.client == nil {
 		panic("client not bind message queue.")
 	}
-	msg := bs.qclient.NewMessage("execs", types.EventAddBlock, detail)
-	bs.qclient.Send(msg, true)
-	resp, err := bs.qclient.Wait(msg)
+	msg := bs.client.NewMessage("execs", types.EventAddBlock, detail)
+	bs.client.Send(msg, true)
+	resp, err := bs.client.Wait(msg)
 	if err != nil {
 		return nil, err
 	}
@@ -240,13 +231,13 @@ func (bs *BlockStore) getLocakKV(detail *types.BlockDetail) (*types.LocalDBSet, 
 	return kv, nil
 }
 
-func (bs *BlockStore) getDelLocakKV(detail *types.BlockDetail) (*types.LocalDBSet, error) {
-	if bs.qclient == nil {
+func (bs *BlockStore) getDelLocalKV(detail *types.BlockDetail) (*types.LocalDBSet, error) {
+	if bs.client == nil {
 		panic("client not bind message queue.")
 	}
-	msg := bs.qclient.NewMessage("execs", types.EventDelBlock, detail)
-	bs.qclient.Send(msg, true)
-	resp, err := bs.qclient.Wait(msg)
+	msg := bs.client.NewMessage("execs", types.EventDelBlock, detail)
+	bs.client.Send(msg, true)
+	resp, err := bs.client.Wait(msg)
 	if err != nil {
 		return nil, err
 	}
