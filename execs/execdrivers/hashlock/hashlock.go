fa894a2a28aca79a550136e0637932af77b5ace5
朱梦凡 <935524254@qq.com>
2018-02-08 10:23:23 +0800

    update

--- execs/execdrivers/hashlock/hashlock.go
+++ execs/execdrivers/hashlock/hashlock.go
@@ -83,14 +83,102 @@ func (h *Hashlock) Exec(tx *types.Transaction, index int) (*types.Receipt, error
 	return nil, types.ErrActionNotSupport
 }
 
+//获取运行状态名
 func (h *Hashlock) GetActionName(tx *types.Transaction) string {
-	return "hashlock"
+	var action types.HashlockAction
+	err := types.Decode(tx.Payload, &action)
+	if err != nil {
+		return "unknow"
+	}
+	if err = account.CheckAddress(tx.To); err != nil {
+		return "unknow"
+	}
+	if  action.Ty == types.HashlockActionLock && action.GetHlock() != nil {
+		return "lock"
+	} else if action.Ty == types.HashlockActionUnlock && action.GetHunlock() != nil {
+		return "unlock"
+	} else if action.Ty == types.HashlockActionSend && action.GetHsend() != nil  {
+		return "send"
+	} else {
+		return "unknow"
+	}
 }
+//把信息进行存储
+//将运行结果内容存入本地地址
 
 func (h *Hashlock) ExecLocal(tx *types.Transaction, receipt *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	return h.ExecLocalCommon(tx, receipt, index)
+	set, err := n.ExecLocalCommon(tx, receipt, index)
+	if err != nil {
+		return nil, err
+	}
+	if receipt.GetTy() != types.ExecOk {
+		return set, nil
+	}
+	//执行成功
+	var action types.HashlockAction
+	err = types.Decode(tx.GetPayload(), &action)
+	if err != nil {
+		panic(err)
+	}
+	var kv *types.KeyValue
+	if action.Ty == types.HashlockActionLock && action.GetHlock() != nil {
+		hlock := action.GetHlock()
+		kv, err = updateHashReciver(h.GetLocalDB(), hlock.hash, hlock.Amount, 1)
+	} else if action.Ty == types.HashlockActionUnlock && action.GetHunlock() != nil {
+		hunlock := action.GetHunlock()
+		//from := account.PubKeyToAddress(tx.Signature.Pubkey).String()
+		kv, err = updateHashReciver(h.GetLocalDB(), hunlock.hash, hunlock.Amount, 3)
+	} else if action.Ty == types.HashlockActionSend && action.GetHsend() != nil {
+		hsend := action.GetHsend()
+		kv, err = updateHashReciver(h.GetLocalDB(), hsend.hash, hsend.Amount, 2)
+	}
+	if err != nil {
+		return set, nil
+	}
+	if kv != nil {
+		set.KV = append(set.KV, kv)
+	}
+	return set, nil
 }
 
 func (h *Hashlock) ExecDelLocal(tx *types.Transaction, receipt *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	return h.ExecDelLocalCommon(tx, receipt, index)
+	set, err := n.ExecDelLocalCommon(tx, receipt, index)
+	if err != nil {
+		return nil, err
+	}
+	if receipt.GetTy() != types.ExecOk {
+		return set, nil
+	}
+	//执行成功
+	var action types.HashlockAction
+	err = types.Decode(tx.GetPayload(), &action)
+	if err != nil {
+		panic(err)
+	}
+	var kv *types.KeyValue
+	if action.Ty == types.HashlockActionLock && action.GetHlock() != nil {
+		hlock := action.GetHlock()
+		kv, err = updateHashReciver(h.GetLocalDB(), hlock.hash, hlock.Amount, 1)
+	} else if action.Ty == types.HashlockActionUnlock && action.GetHunlock() != nil {
+		hunlock := action.GetHunlock()
+//		from := account.PubKeyToAddress(tx.Signature.Pubkey).String()
+		kv, err = updateHashReciver(h.GetLocalDB(), hunlock.hash, hunlock.Amount, 3)
+	}else if action.Ty == types.HashlockActionSend && action.GetHsend() != nil {
+		hsend := action.GetHsend()
+		kv, err = updateHashReciver(h.GetLocalDB(), hsend.hash, hsend.Amount, 2)
+	}
+	if err != nil {
+		return set, nil
+	}
+	if kv != nil {
+		set.KV = append(set.KV, kv)
+	}
+	return set, nil
+}
+
+func (n *Hashlock) Query(funcName string, HashlockId []byte) (execs.Hashlockquery, error) {
+	if funcName == "GetHashlocKById" {
+		return n.GetHashReciver(HashlockId)
+	}
+	return nil, types.ErrActionNotSupport
 }
