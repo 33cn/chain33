85a2db7ec128d1e8364b7fa3801949c832d78557
huanggang <harbour_982@126.com>
2018-02-22 18:38:22 +0800

    tendermint

---
+++ consensus/drivers/tendermint/types/heartbeat.go
@@ -0,0 +1,31 @@
+package types
+
+import (
+	"github.com/tendermint/go-crypto"
+	"github.com/tendermint/go-wire/data"
+	"io"
+	"github.com/tendermint/go-wire"
+)
+
+// Heartbeat is a simple vote-like structure so validators can
+// alert others that they are alive and waiting for transactions.
+// Note: We aren't adding ",omitempty" to Heartbeat's
+// json field tags because we always want the JSON
+// representation to be in its canonical form.
+type Heartbeat struct {
+	ValidatorAddress data.Bytes       `json:"validator_address"`
+	ValidatorIndex   int              `json:"validator_index"`
+	Height           int64            `json:"height"`
+	Round            int              `json:"round"`
+	Sequence         int              `json:"sequence"`
+	Signature        crypto.Signature `json:"signature"`
+}
+
+// WriteSignBytes writes the Heartbeat for signing.
+// It panics if the Heartbeat is nil.
+func (heartbeat *Heartbeat) WriteSignBytes(chainID string, w io.Writer, n *int, err *error) {
+	wire.WriteJSON(CanonicalJSONOnceHeartbeat{
+		chainID,
+		CanonicalHeartbeat(heartbeat),
+	}, w, n, err)
+}
