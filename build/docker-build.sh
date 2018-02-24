08622176a5ac80b9945e2b6d3a56c2fb78ff6a56
suyanlong <592938410@qq.com>
2018-02-24 14:09:51 +0800

    Add makefile help information
    Signed-off-by: suyanlong <592938410@qq.com>

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # https://hub.docker.com/r/suyanlong/golang-dev/
 # https://github.com/suyanlong/golang-dev
+# sudo docker pull suyanlong/golang-dev:latest
 
-sudo docker run -it -p 0.0.0.0:8081:8081 -l linux-chain33-build -v $GOPATH/src/code.aliyun.com/chain33/chain33/build:/root -w /root suyanlong/golang-dev:latest
+sudo docker run -it -p 0.0.0.0:8081:8081 -l linux-chain33-build -v $GOPATH/src/code.aliyun.com/chain33/chain33/build:/root/build -w /root suyanlong/golang-dev:latest
