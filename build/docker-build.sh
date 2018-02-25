73a53fccd55960819a38d27fcef6bd9bcd32cc7a
suyanlong <592938410@qq.com>
2018-02-25 16:26:10 +0800

    fixed bug

--- build/docker-build.sh
+++ build/docker-build.sh
@@ -3,4 +3,6 @@
 # https://github.com/suyanlong/golang-dev
 # sudo docker pull suyanlong/golang-dev:latest
 
-sudo docker run -it -p 0.0.0.0:8081:8081 -l linux-chain33-build -v $GOPATH/src/code.aliyun.com/chain33/chain33/build:/root/build -w /root suyanlong/golang-dev:latest
+sudo docker run -it -p 8801:8801 -p 8802:8802 -p 6060:6060 -p 50051:50051 -l linux-chain33-build \
+    -v $GOPATH/src/code.aliyun.com/chain33/chain33:/go/src/code.aliyun.com/chain33/chain33 \
+    -w /go/src/code.aliyun.com/chain33/chain33 suyanlong/golang-dev:latest
