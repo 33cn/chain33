09f6b1fb818d2cbfdb0d0fe010d836965f1e8416
king wang <kingwang@kingdeMacBook-Pro.local>
2018-03-06 02:13:02 +0800

    account build pass

--- account/genesis.go
+++ account/genesis.go
@@ -1,38 +1,37 @@
 package account
 
 import (
-	dbm "code.aliyun.com/chain33/chain33/common/db"
 	"code.aliyun.com/chain33/chain33/types"
 )
 
-func GenesisInit(db dbm.KVDB, addr string, amount int64) (*types.Receipt, error) {
-	accTo := LoadAccount(db, addr)
+func (acc *AccountDB) GenesisInit(addr string, amount int64) (*types.Receipt, error) {
+	accTo := acc.LoadAccount(addr)
 	copyto := *accTo
 	accTo.Balance = accTo.GetBalance() + amount
 	receiptBalanceTo := &types.ReceiptAccountTransfer{&copyto, accTo}
-	SaveAccount(db, accTo)
-	receipt := genesisReceipt(accTo, receiptBalanceTo)
+	acc.SaveAccount(accTo)
+	receipt := acc.genesisReceipt(accTo, receiptBalanceTo)
 	return receipt, nil
 }
 
-func GenesisInitExec(db dbm.KVDB, addr string, amount int64, execaddr string) (*types.Receipt, error) {
-	accTo := LoadAccount(db, execaddr)
+func (acc *AccountDB) GenesisInitExec(addr string, amount int64, execaddr string) (*types.Receipt, error) {
+	accTo := acc.LoadAccount(execaddr)
 	copyto := *accTo
 	accTo.Balance = accTo.GetBalance() + amount
 	receiptBalanceTo := &types.ReceiptAccountTransfer{&copyto, accTo}
-	SaveAccount(db, accTo)
-	receipt := genesisReceipt(accTo, receiptBalanceTo)
-	receipt2, err := execDeposit(db, addr, execaddr, amount)
+	acc.SaveAccount(accTo)
+	receipt := acc.genesisReceipt(accTo, receiptBalanceTo)
+	receipt2, err := acc.execDeposit(addr, execaddr, amount)
 	if err != nil {
 		panic(err)
 	}
 	receipt2.Ty = types.TyLogGenesisDeposit
-	receipt = mergeReceipt(receipt, receipt2)
+	receipt = acc.mergeReceipt(receipt, receipt2)
 	return receipt, nil
 }
 
-func genesisReceipt(accTo *types.Account, receiptTo *types.ReceiptAccountTransfer) *types.Receipt {
+func (acc *AccountDB) genesisReceipt(accTo *types.Account, receiptTo *types.ReceiptAccountTransfer) *types.Receipt {
 	log2 := &types.ReceiptLog{types.TyLogGenesisTransfer, types.Encode(receiptTo)}
-	kv := GetKVSet(accTo)
+	kv := acc.GetKVSet(accTo)
 	return &types.Receipt{types.ExecOk, kv, []*types.ReceiptLog{log2}}
 }
