ad2b6f27a341c9d6c322b0818548936a923e0b1e
suyanlong <592938410@qq.com>
2018-03-06 11:12:39 +0800

    rewrite init function and abstract DriverBase structure

--- executor/drivers/none/none.go
+++ executor/drivers/none/none.go
@@ -6,11 +6,11 @@ package none
 
 import (
 	"code.aliyun.com/chain33/chain33/executor/drivers"
-	"code.aliyun.com/chain33/chain33/types"
 )
 
 func init() {
-	drivers.Register("none", newNone())
+	n := newNone()
+	drivers.Register(n.GetName(), n)
 }
 
 type None struct {
@@ -25,20 +25,4 @@ func newNone() *None {
 
 func (n *None) GetName() string {
 	return "none"
-}
-
-func (n *None) GetActionName(tx *types.Transaction) string {
-	return tx.ActionName()
-}
-
-func (n *None) Exec(tx *types.Transaction, index int) (*types.Receipt, error) {
-	return n.ExecCommon(tx, index)
-}
-
-func (n *None) ExecLocal(tx *types.Transaction, receipt *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	return n.ExecLocalCommon(tx, receipt, index)
-}
-
-func (n *None) ExecDelLocal(tx *types.Transaction, receipt *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	return n.ExecDelLocalCommon(tx, receipt, index)
 }
