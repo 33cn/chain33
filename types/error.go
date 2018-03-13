6c9317be14b2b31e4a86a2bf505dbb39f9e65503
linjingljlj <linjingljlj@163.com>
2018-03-13 15:24:49 +0800

    fix impl wallet token precreate

--- types/error.go
+++ types/error.go
@@ -54,6 +54,7 @@ var (
 	//err for token
 	ErrTokenNameLen               = errors.New("ErrTokenNameLength")
 	ErrTokenSymbolLen             = errors.New("ErrTokenSymbolLength")
+	ErrTokenSymbolUpper           = errors.New("ErrTokenSymbolUpper")
 	ErrTokenIntroLen              = errors.New("ErrTokenIntroductionLen")
 	ErrTokenExist                 = errors.New("ErrTokenSymbolExistAlready")
 	ErrTokenNotPrecreated         = errors.New("ErrTokenNotPrecreated")
