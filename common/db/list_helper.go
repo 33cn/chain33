b24aa69d0b61985ecedc7739ad6a50090eb66f83
zjb0807 <zjb0807@qq.com>
2018-03-20 02:06:49 -0400

    modify database interface

--- common/db/list_helper.go
+++ common/db/list_helper.go
@@ -1,35 +1,36 @@
 package db
 
+import (
+	log "github.com/inconshreveable/log15"
+)
+
 type ListHelper struct {
 	db DB
 }
+
+var listlog = log.New("module", "db.ListHelper")
 
 func NewListHelper(db DB) *ListHelper {
 	return &ListHelper{db}
 }
 
-func (db *ListHelper) PrefixScan(key []byte) (txhashs [][]byte) {
-	db.db.Iterator(key, false)
+func (db *ListHelper) PrefixScan(prefix []byte) (values [][]byte) {
+	it := db.db.Iterator(prefix, false)
+	if !it.Valid() {
+		listlog.Error("PrefixScan", "it.Valid", it.Valid())
+		return
+	}
+	defer it.Close()
 
-
-
-	err := db.db.View(func(txn *badger.Txn) error {
-		it := txn.NewIterator(badger.DefaultIteratorOptions)
-		defer it.Close()
-		for it.Seek(key); it.ValidForPrefix(key); it.Next() {
-			item := it.Item()
-			value, err := item.ValueCopy(nil)
-			if err != nil {
-				return err
-			}
-			// blog.Debug("PrefixScan", "key", string(item.Key()), "value", value)
-			txhashs = append(txhashs, value)
+	for ; it.Valid(); it.Next() {
+		value := it.Value()
+		if it.Error() != nil {
+			listlog.Error("PrefixScan it.Value()", "error", it.Error())
+			values = nil
+			return 
 		}
-		return nil
-	})
-	if err != nil {
-		blog.Error("PrefixScan", "error", err)
-		txhashs = txhashs[:]
+		// blog.Debug("PrefixScan", "key", string(item.Key()), "value", value)
+		values = append(values, value)
 	}
 	return
 }
@@ -37,208 +38,95 @@ func (db *ListHelper) PrefixScan(key []byte) (txhashs [][]byte) {
 func (db *ListHelper) List(prefix, key []byte, count, direction int32) (values [][]byte) {
 	if len(key) == 0 {
 		if direction == 1 {
-			return db.IteratorScanFromFirst(prefix, count, direction)
+			return db.IteratorScanFromFirst(prefix, count)
 		} else {
-			return db.IteratorScanFromLast(prefix, count, direction)
+			return db.IteratorScanFromLast(prefix, count)
 		}
 	}
 	return db.IteratorScan(prefix, key, count, direction)
 }
 
-func (db *ListHelper) IteratorScan(Prefix []byte, key []byte, count int32, direction int32) (values [][]byte) {
-	err := db.db.View(func(txn *badger.Txn) error {
-		var i int32 = 0
-		opts := badger.DefaultIteratorOptions
-		if direction == 0 {
-			opts.Reverse = true
-		}
-		it := txn.NewIterator(opts)
-		defer it.Close()
-		it.Seek(Prefix)
-		for it.Seek(key); it.ValidForPrefix(Prefix); it.Next() {
-			item := it.Item()
-			value, err := item.ValueCopy(nil)
-			if err != nil {
-				return err
-			}
-			// blog.Debug("IteratorScan", "key", string(item.Key()), "value", value)
-			values = append(values, value)
-			i++
-			if i == count {
-				break
-			}
-		}
-		return nil
-	})
-
-	if err != nil {
-		blog.Error("IteratorScan", "error", err)
-		values = values[:]
+func (db *ListHelper) IteratorScan(prefix []byte, key []byte, count int32, direction int32) (values [][]byte) {
+	var reserse bool = false	
+	if direction == 0 {
+		reserse = true
 	}
-	return
-}
-
-func (db *ListHelper) IteratorScanFromFirst(key []byte, count int32, direction int32) (values [][]byte) {
-	err := db.db.View(func(txn *badger.Txn) error {
-		var i int32 = 0
-		it := txn.NewIterator(badger.DefaultIteratorOptions)
-		defer it.Close()
-		for it.Seek(key); it.ValidForPrefix(key); it.Next() {
-			item := it.Item()
-			value, err := item.ValueCopy(nil)
-			if err != nil {
-				return err
-			}
-			// blog.Debug("IteratorScanFromFirst", "key", string(item.Key()), "value", value)
-			values = append(values, value)
-			i++
-			if i == count {
-				break
-			}
-		}
-		return nil
-	})
-
-	if err != nil {
-		blog.Error("IteratorScanFromFirst", "error", err)
-		values = values[:]
+	it := db.db.Iterator(prefix, reserse)
+	if !it.Valid() {
+		listlog.Error("IteratorScan", "it.Valid", it.Valid())
+		return
 	}
-	return
-}
+	defer it.Close()
 
-func (db *ListHelper) IteratorScanFromLast(key []byte, count int32, direction int32) (values [][]byte) {
-	err := db.db.View(func(txn *badger.Txn) error {
-		var i int32 = 0
-		opts := badger.DefaultIteratorOptions
-		opts.Reverse = true
-		it := txn.NewIterator(opts)
-		defer it.Close()
-		prefix := append(key, 0xFF)
-		for it.Seek(prefix); it.ValidForPrefix(key); it.Next() {
-			item := it.Item()
-			value, err := item.ValueCopy(nil)
-			if err != nil {
-				return err
-			}
-			// blog.Debug("IteratorScanFromLast", "key", string(item.Key()), "value", value)
-			values = append(values, value)
-			i++
-			if i == count {
-				break
-			}
-		}
-		return nil
-	})
-
-	if err != nil {
-		blog.Error("IteratorScanFromLast", "error", err)
-		values = values[:]
-	}
-	return
-}
---------------------------------------------
-
-func (db *GoLevelDB) PrefixScan(key []byte) (txhashs [][]byte) {
-	iter := db.db.NewIterator(util.BytesPrefix(key), nil)
-	for iter.Next() {
-		value := iter.Value()
-		//fmt.Printf("PrefixScan:%s\n", string(iter.Key()))
-		value1 := make([]byte, len(value))
-		copy(value1, value)
-		txhashs = append(txhashs, value1)
-	}
-	iter.Release()
-	err := iter.Error()
-	if err != nil {
-		return nil
-	}
-	return txhashs
-}
-
-func (db *GoLevelDB) List(prefix, key []byte, count, direction int32) (values [][]byte) {
-	if len(key) == 0 {
-		if direction == 1 {
-			return db.IteratorScanFromFirst(prefix, count, direction)
-		} else {
-			return db.IteratorScanFromLast(prefix, count, direction)
-		}
-	}
-	return db.IteratorScan(prefix, key, count, direction)
-}
-
-func (db *GoLevelDB) IteratorScan(Prefix []byte, key []byte, count int32, direction int32) (values [][]byte) {
-	iter := db.db.NewIterator(util.BytesPrefix(Prefix), nil)
 	var i int32 = 0
-	for ok := iter.Seek(key); ok; {
-		if i == 0 && !bytes.Equal(key, iter.Key()) {
-			log.Info("IteratorScan Equal ", "key", string(iter.Key()))
-			return nil
+	for it.Seek(key); it.Valid(); it.Next() {
+		value := it.Value()
+		if it.Error() != nil {
+			listlog.Error("PrefixScan it.Value()", "error", it.Error())
+			values = nil
+			return 
 		}
-		if i != 0 {
-			value := iter.Value()
-			//log.Info("IteratorScan", "key", string(iter.Key()))
-			value1 := make([]byte, len(value))
-			copy(value1, value)
-			values = append(values, value1)
-		}
-		if direction == 0 {
-			ok = iter.Prev()
-		} else {
-			ok = iter.Next()
-		}
-		i++
-		if i > count {
-			break
-		}
-	}
-	iter.Release()
-	err := iter.Error()
-	if err != nil {
-		return nil
-	}
-	return values
-}
-
-func (db *GoLevelDB) IteratorScanFromFirst(key []byte, count int32, direction int32) (values [][]byte) {
-	iter := db.db.NewIterator(util.BytesPrefix(key), nil)
-	var i int32 = 0
-	for ok := iter.First(); ok; {
-		value := iter.Value()
-		value1 := make([]byte, len(value))
-		copy(value1, value)
-		values = append(values, value1)
-		ok = iter.Next()
+		// blog.Debug("PrefixScan", "key", string(item.Key()), "value", value)
+		values = append(values, value)
 		i++
 		if i == count {
 			break
 		}
 	}
-	iter.Release()
-	err := iter.Error()
-	if err != nil {
-		return nil
-	}
-	return values
+	return
 }
 
-func (db *GoLevelDB) IteratorScanFromLast(key []byte, count int32, direction int32) (values [][]byte) {
-	iter := db.db.NewIterator(util.BytesPrefix(key), nil)
+func (db *ListHelper) IteratorScanFromFirst(prefix []byte, count int32) (values [][]byte) {
+	it := db.db.Iterator(prefix, false)
+	if !it.Valid() {
+		listlog.Error("IteratorScan", "it.Valid", it.Valid())
+		return
+	}
+	defer it.Close()
+
 	var i int32 = 0
-	for ok := iter.Last(); ok; {
-		value := iter.Value()
-		value1 := make([]byte, len(value))
-		copy(value1, value)
-		values = append(values, value1)
-		ok = iter.Prev()
+	for ; it.Valid(); it.Next() {
+		value := it.Value()
+		if it.Error() != nil {
+			listlog.Error("PrefixScan it.Value()", "error", it.Error())
+			values = nil
+			return 
+		}
+		// blog.Debug("PrefixScan", "key", string(item.Key()), "value", value)
+		values = append(values, value)
 		i++
 		if i == count {
 			break
 		}
 	}
-	iter.Release()
-	err := iter.Error()
-	if err != nil {
-		return nil
-	}
-	return values
+	return
 }
+
+func (db *ListHelper) IteratorScanFromLast(prefix []byte, count int32) (values [][]byte) {
+	//it := db.db.Iterator(bytesPrefix(prefix), true)
+	//it := db.db.Iterator(append(prefix, 0xFF), true)
+	it := db.db.Iterator(prefix, true)
+	if !it.Valid() {
+		listlog.Error("IteratorScan", "it.Valid", it.Valid())
+		return
+	}
+	defer it.Close()
+
+	var i int32 = 0
+	for ; it.Valid(); it.Next() {
+		value := it.Value()
+		if it.Error() != nil {
+			listlog.Error("PrefixScan it.Value()", "error", it.Error())
+			values = nil
+			return 
+		}
+		// blog.Debug("PrefixScan", "key", string(item.Key()), "value", value)
+		values = append(values, value)
+		i++
+		if i == count {
+			break
+		}
+	}
+	return
+}
+
