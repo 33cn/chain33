1cf53415d7e7509587acb8ae1d5fb3990f98b39c
lihailei <18761806026@163.com>
2018-04-03 13:37:40 +0800

    remove  unused code.

--- consensus/drivers/raft/controller.go
+++ consensus/drivers/raft/controller.go
@@ -13,7 +13,6 @@ var genesisAddr = "14KEKbYtKKQm4wMthSK9J4La4nAiidGozt"
 var rlog = log.New("module", "raft")
 
 var (
-	//isValidator bool = false
 	isLeader    bool = false
 	leaderIsOK  bool = false
 	confChangeC chan raftpb.ConfChange
@@ -30,10 +29,6 @@ func NewRaftCluster(cfg *types.Consensus) *RaftClient {
 		//TODO 当传入的参数异常时，返回给主函数的是个nil,这时候需要做异常处理
 		return nil
 	}
-	// validator节点（客户端），此节点用来处理区块打包等工作，并把区块发往raft集群来做共识
-	// 相对于raft集群来说，发送区块的是客户端，客户端不用考虑raft的leader节点是哪个
-	// 就算发给follower也会自动移交给leader。
-
 	// propose channel
 	proposeC := make(chan *types.Block)
 	confChangeC = make(chan raftpb.ConfChange)
@@ -42,7 +37,6 @@ func NewRaftCluster(cfg *types.Consensus) *RaftClient {
 	getSnapshot := func() ([]byte, error) { return b.getSnapshot() }
 	// raft集群的建立,1. 初始化两条channel： propose channel用于客户端和raft底层交互, commit channel用于获取commit消息
 	// 2. raft集群中的节点之间建立http连接
-	//TODO:URL应该加入正则表达式进行校验
 	peers := strings.Split(cfg.PeersURL, ",")
 	if len(peers) == 1 && peers[0] == "" {
 		peers = []string{}
