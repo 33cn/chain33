62120f17acd878608dddc445334bcccb9be2e2fe
sanghg <shg@disanbo.com>
2018-04-13 15:37:50 +0800

    repair commit warning

--- client/coordinator.go
+++ client/coordinator.go
@@ -17,20 +17,20 @@ type QueueProtocolAPI interface {
 }
 
 const (
-	mempoolKey 		= "mempool"		// 未打包交易池
+	mempoolKey = "mempool" // 未打包交易池
 	//p2pKey			= "p2p"			//
 	//rpcKey			= "rpc"
 	//consensusKey	= "consensus"	// 共识系统
 	//accountKey		= "accout"		// 账号系统
 	//executorKey		= "execs"		// 交易执行器
 	//walletKey		= "wallet"		// 钱包
-	blockchainKey	= "blockchain"	// 区块
+	blockchainKey = "blockchain" // 区块
 	//storeKey		= "store"
 )
 
 // 消息通道协议实现
 type QueueCoordinator struct {
-	client		queue.Client	// 消息队列
+	client queue.Client // 消息队列
 }
 
 func New() *QueueCoordinator {
@@ -39,23 +39,24 @@ func New() *QueueCoordinator {
 	return q
 }
 
-func (q *QueueCoordinator) SetQueueClient(client queue.Client)  {
+func (q *QueueCoordinator) SetQueueClient(client queue.Client) {
 	q.client = client
 }
 
-func (q *QueueCoordinator) Close()  {
+func (q *QueueCoordinator) Close() {
 	q.client.Close()
 }
 
-func (q *QueueCoordinator) query(topic string, ty int64,  data interface{}) (queue.Message, error) {
-	msg := q.client.NewMessage(topic, ty, data)
-	q.client.Send(msg, true)
-	return q.client.Wait(msg)
+func (q *QueueCoordinator) query(topic string, ty int64, data interface{}) (queue.Message, error) {
+	client := q.client.Clone()
+	msg := client.NewMessage(topic, ty, data)
+	client.Send(msg, true)
+	return client.Wait(msg)
 }
 
 func (q *QueueCoordinator) QueryTx(data interface{}) (*types.Reply, error) {
 	msg, err := q.query(mempoolKey, types.EventTx, data)
-	if nil!=err {
+	if nil != err {
 		return nil, err
 	}
 	return msg.GetData().(*types.Reply), nil
@@ -63,22 +64,22 @@ func (q *QueueCoordinator) QueryTx(data interface{}) (*types.Reply, error) {
 
 func (q *QueueCoordinator) QueryTxList(count int64, hashes [][]byte) (*types.ReplyTxList, error) {
 	msg, err := q.query(mempoolKey, types.EventTxList, &types.TxHashList{Count: count, Hashes: hashes})
-	if nil!=err {
+	if nil != err {
 		return nil, err
 	}
 	return msg.GetData().(*types.ReplyTxList), nil
 }
 
 func (q *QueueCoordinator) QueryGetBlocksRange(start int64, end int64) (*types.BlockDetails, error) {
-	return q.QueryGetBlocks(&types.ReqBlocks{start,
-	end,
-	false,
-	[]string{""}})
+	return q.QueryGetBlocks(&types.ReqBlocks{Start: start,
+	End: end,
+	Isdetail:false,
+	Pid: []string{""}})
 }
 
 func (q *QueueCoordinator) QueryGetBlocks(data interface{}) (*types.BlockDetails, error) {
 	msg, err := q.query(blockchainKey, types.EventGetBlocks, data)
-	if nil!=err {
+	if nil != err {
 		return nil, err
 	}
 	return msg.GetData().(*types.BlockDetails), nil
