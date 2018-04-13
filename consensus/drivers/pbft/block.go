e6b156cf32714090bddc0f9dac7d0d52b1bbef33
loujiahui <634353255@qq.com>
2018-04-13 14:22:35 +0800

    fix debug
    Former-commit-id: d83f0ece9674069ce818fb64acfac876644100f6
    Former-commit-id: 3b2b6406930c97ea32e4dd1d76568b8085fbec9d

--- consensus/drivers/pbft/block.go
+++ consensus/drivers/pbft/block.go
@@ -38,7 +38,7 @@ func (client *PbftClient) ExecBlock(prevHash []byte, block *pb.Block) (*pb.Block
 	if block.Height == 0 {
 		block.Difficulty = pb.GetP(0).PowLimitBits
 	}
-	blockdetail, deltx, err := util.ExecBlock(client.GetQueueClient(), prevHash, block, false, true)
+	blockdetail, deltx, err := util.ExecBlock(client.GetQueueClient(), prevHash, block, false, false)
 	if err != nil { //never happen
 		return nil, deltx, err
 	}
