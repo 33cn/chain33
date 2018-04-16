58a7648fceab86177eee0c67a0f25771e432c31d
sanghg <shg@disanbo.com>
2018-04-16 18:47:47 +0800

    add test mock, add test case.

--- client/coordinator_test.go
+++ client/coordinator_test.go
@@ -1,122 +1,82 @@
 package client
 
 import (
-	"flag"
-	_ "gitlab.33.cn/chain33/chain33/account"
-	"gitlab.33.cn/chain33/chain33/blockchain"
-	"gitlab.33.cn/chain33/chain33/common"
-	"gitlab.33.cn/chain33/chain33/common/config"
-	"gitlab.33.cn/chain33/chain33/common/crypto"
-	_ "gitlab.33.cn/chain33/chain33/common/limits"
-	"gitlab.33.cn/chain33/chain33/consensus"
-	"gitlab.33.cn/chain33/chain33/executor"
-	"gitlab.33.cn/chain33/chain33/mempool"
-	_ "gitlab.33.cn/chain33/chain33/p2p"
-	"gitlab.33.cn/chain33/chain33/queue"
-	"gitlab.33.cn/chain33/chain33/store"
-	"gitlab.33.cn/chain33/chain33/types"
 	"testing"
+
+	"fmt"
+	"gitlab.33.cn/chain33/chain33/queue"
+	"gitlab.33.cn/chain33/chain33/types"
 )
 
-//----------------------------- data for testing ---------------------------------
-var (
-	amount   = int64(1e8)
-	v        = &types.CoinsAction_Transfer{&types.CoinsTransfer{Amount: amount}}
-	transfer = &types.CoinsAction{Value: v, Ty: types.CoinsActionTransfer}
-	tx1      = &types.Transaction{Execer: []byte("coins"), Payload: types.Encode(transfer), Fee: 1000000, Expire: 1}
-	tx2      = &types.Transaction{Execer: []byte("coins"), Payload: types.Encode(transfer), Fee: 100000000, Expire: 0}
-	tx3      = &types.Transaction{Execer: []byte("coins"), Payload: types.Encode(transfer), Fee: 200000000, Expire: 0}
-	tx4      = &types.Transaction{Execer: []byte("coins"), Payload: types.Encode(transfer), Fee: 300000000, Expire: 0}
-	tx5      = &types.Transaction{Execer: []byte("coins"), Payload: types.Encode(transfer), Fee: 400000000, Expire: 0}
-	tx6      = &types.Transaction{Execer: []byte("coins"), Payload: types.Encode(transfer), Fee: 500000000, Expire: 0}
-	tx7      = &types.Transaction{Execer: []byte("coins"), Payload: types.Encode(transfer), Fee: 600000000, Expire: 0}
-	tx8      = &types.Transaction{Execer: []byte("coins"), Payload: types.Encode(transfer), Fee: 700000000, Expire: 0}
-	tx9      = &types.Transaction{Execer: []byte("coins"), Payload: types.Encode(transfer), Fee: 800000000, Expire: 0}
-	tx10     = &types.Transaction{Execer: []byte("coins"), Payload: types.Encode(transfer), Fee: 900000000, Expire: 0}
-	tx11     = &types.Transaction{Execer: []byte("coins"), Payload: types.Encode(transfer), Fee: 450000000, Expire: 0}
-	tx12     = &types.Transaction{Execer: []byte("coins"), Payload: types.Encode(transfer), Fee: 460000000, Expire: 0}
-
-	c, _       = crypto.New(types.GetSignatureTypeName(types.SECP256K1))
-	hex_val    = "CC38546E9E659D15E6B4893F0AB32A06D103931A8230B0BDE71459D2B27D6944"
-	a, _       = common.FromHex(hex_val)
-	privKey, _ = c.PrivKeyFromBytes(a)
-	//tx_hash	   = "611dde807369601588ed1b901f116c9b647b960e55f0798603ce0f47c271f985"
-	//tx_hash_b, _  = common.FromHex(tx_hash)
-	//random     *rand.Rand
-	//mainPriv   crypto.PrivKey
-)
-
-/*
-var blk = &types.Block{
-	Version:    1,
-	ParentHash: []byte("parent hash"),
-	TxHash:     []byte("tx hash"),
-	Height:     1,
-	BlockTime:  1,
-	Txs:        []*types.Transaction{tx3, tx5},
+type mockBlockChain struct {
 }
-*/
+
+func (m *mockBlockChain) SetQueueClient(q queue.Queue) {
+	go func() {
+		client := q.Client()
+		client.Sub(blockchainKey)
+		for msg := range client.Recv() {
+			if types.EventLocalGet == msg.Ty {
+				msg.Reply(client.NewMessage(blockchainKey, types.EventLocalGet, &types.Reply{IsOk: true, Msg: []byte("word")}))
+			}
+		}
+	}()
+}
+
+func (m *mockBlockChain) Close() {
+}
+
+type mockMempool struct {
+	client queue.Client
+}
+
+func (m *mockMempool) SetQueueClient(q queue.Queue) {
+	go func() {
+		client := q.Client()
+		client.Sub(mempoolKey)
+		for msg := range client.Recv() {
+			fmt.Println("Recv :", msg.Ty)
+			switch msg.Ty {
+			case types.EventTx:
+				msg.Reply(client.NewMessage(mempoolKey, types.EventReply, &types.Reply{IsOk: true, Msg: []byte("word")}))
+				break
+			case types.EventTxList:
+				msg.Reply(client.NewMessage(mempoolKey, types.EventReply, &types.ReplyTxList{}))
+				break
+			default:
+				msg.ReplyErr("Do not support", types.ErrNotSupport)
+				break
+			}
+		}
+	}()
+}
+
+func (m *mockMempool) Close() {
+}
 
 type mockSystem struct {
 	q     queue.Queue
-	mem   *mempool.Mempool
-	exec  *executor.Executor
-	chain *blockchain.BlockChain
-	store queue.Module
-	cons  queue.Module
+	chain *mockBlockChain
+	mem   *mockMempool
 }
 
 func (mock *mockSystem) startup(size int) QueueProtocolAPI {
 	var q = queue.New("channel")
-	flag.Parse()
-	cfg := config.InitCfg("chain33.toml")
-	chain := blockchain.New(cfg.BlockChain)
-	chain.SetQueueClient(q.Client())
+	chain := &mockBlockChain{}
+	chain.SetQueueClient(q)
 
-	exec := executor.New(cfg.Exec)
-	exec.SetQueueClient(q.Client())
-
-	s := store.New(cfg.Store)
-	s.SetQueueClient(q.Client())
-
-	cs := consensus.New(cfg.Consensus)
-	cs.SetQueueClient(q.Client())
-
-	mem := mempool.New(cfg.MemPool)
-	mem.SetQueueClient(q.Client())
-	//mem.sync = true
-	if size > 0 {
-		mem.Resize(size)
-	}
-	mem.SetMinFee(types.MinFee)
-
-	tx1.Sign(types.SECP256K1, privKey)
-	tx2.Sign(types.SECP256K1, privKey)
-	tx3.Sign(types.SECP256K1, privKey)
-	tx4.Sign(types.SECP256K1, privKey)
-	tx5.Sign(types.SECP256K1, privKey)
-	tx6.Sign(types.SECP256K1, privKey)
-	tx7.Sign(types.SECP256K1, privKey)
-	tx8.Sign(types.SECP256K1, privKey)
-	tx9.Sign(types.SECP256K1, privKey)
-	tx10.Sign(types.SECP256K1, privKey)
-	tx11.Sign(types.SECP256K1, privKey)
-	tx12.Sign(types.SECP256K1, privKey)
+	mem := &mockMempool{}
+	mem.SetQueueClient(q)
 
 	mock.q = q
-	mock.exec = exec
 	mock.chain = chain
-	mock.store = s
-	mock.cons = cs
-
+	mock.mem = mem
 	return mock.getAPI()
 }
 
 func (mock *mockSystem) stop() {
-	mock.cons.Close()
-	mock.store.Close()
 	mock.chain.Close()
-	mock.exec.Close()
+	mock.mem.Close()
 	mock.q.Close()
 }
 
@@ -125,16 +85,32 @@ func (mock *mockSystem) getAPI() QueueProtocolAPI {
 	return api
 }
 
-func TestQueueCoordinator_GetTx(t *testing.T) {
+func TestCoordinator(t *testing.T) {
 	var mock mockSystem
 	api := mock.startup(0)
-	defer mock.stop()
 
-	_, err := api.GetTx(tx1)
+	testGetTx(t, api)
+	testGetTxList(t, api)
+
+	mock.stop()
+
+}
+
+func testGetTxList(t *testing.T, api QueueProtocolAPI) {
+	_, err := api.GetTxList(&types.TxHashList{})
 	if nil != err {
-		t.Error("QueryTx error. ", err)
+		t.Error("Call GetTxList Failed.", err)
 	}
 }
+
+func testGetTx(t *testing.T, api QueueProtocolAPI) {
+	_, err := api.GetTx(&types.Transaction{})
+	if nil != err {
+		t.Error("Call GetTx Failed.", err)
+	}
+}
+
+/*
 
 func TestQueueCoordinator_QueryTxList(t *testing.T) {
 	var mock mockSystem
@@ -184,14 +160,6 @@ func TestQueueCoordinator_QueryTx(t *testing.T) {
 	var mock mockSystem
 	mock.startup(0)
 	defer mock.stop()
-
-	// 需要获取正确的交易hash值
-	/*
-		_, err := api.QueryTx(&types.ReqHash{tx_hash_b})
-		if nil != err {
-			t.Error("QueryTx error. ", err)
-		}
-	*/
 }
 
 func TestQueueCoordinator_GetTransactionByAddr(t *testing.T) {
@@ -298,3 +266,4 @@ func TestQueueCoordinator_PeerInfo(t *testing.T) {
 
 func TestQueueCoordinator_GetTicketCount(t *testing.T) {
 }
+*/
