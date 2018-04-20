17d6eb0c4168c0513ab0328eeb3595f9641fd3d7
hzj <hzj@disanbo.com>
2018-04-20 15:37:13 +0800

    add paid for private tx fee

--- common/ed25519/edwards25519/edwards25519.go
+++ common/ed25519/edwards25519/edwards25519.go
@@ -1501,17 +1501,17 @@ func ScMulSub(s, a, b, c *[32]byte) {
 	var carry [23]int64
 
 	s0 := c0 - a0*b0
-	s1 := c1 - a0*b1 + a1*b0
-	s2 := c2 - a0*b2 + a1*b1 + a2*b0
-	s3 := c3 - a0*b3 + a1*b2 + a2*b1 + a3*b0
-	s4 := c4 - a0*b4 + a1*b3 + a2*b2 + a3*b1 + a4*b0
-	s5 := c5 - a0*b5 + a1*b4 + a2*b3 + a3*b2 + a4*b1 + a5*b0
-	s6 := c6 - a0*b6 + a1*b5 + a2*b4 + a3*b3 + a4*b2 + a5*b1 + a6*b0
-	s7 := c7 - a0*b7 + a1*b6 + a2*b5 + a3*b4 + a4*b3 + a5*b2 + a6*b1 + a7*b0
-	s8 := c8 - a0*b8 + a1*b7 + a2*b6 + a3*b5 + a4*b4 + a5*b3 + a6*b2 + a7*b1 + a8*b0
-	s9 := c9 - a0*b9 + a1*b8 + a2*b7 + a3*b6 + a4*b5 + a5*b4 + a6*b3 + a7*b2 + a8*b1 + a9*b0
-	s10 := c10 - a0*b10 + a1*b9 + a2*b8 + a3*b7 + a4*b6 + a5*b5 + a6*b4 + a7*b3 + a8*b2 + a9*b1 + a10*b0
-	s11 := c11 - a0*b11 + a1*b10 + a2*b9 + a3*b8 + a4*b7 + a5*b6 + a6*b5 + a7*b4 + a8*b3 + a9*b2 + a10*b1 + a11*b0
+	s1 := c1 - (a0*b1 + a1*b0)
+	s2 := c2 - (a0*b2 + a1*b1 + a2*b0)
+	s3 := c3 - (a0*b3 + a1*b2 + a2*b1 + a3*b0)
+	s4 := c4 - (a0*b4 + a1*b3 + a2*b2 + a3*b1 + a4*b0)
+	s5 := c5 - (a0*b5 + a1*b4 + a2*b3 + a3*b2 + a4*b1 + a5*b0)
+	s6 := c6 - (a0*b6 + a1*b5 + a2*b4 + a3*b3 + a4*b2 + a5*b1 + a6*b0)
+	s7 := c7 - (a0*b7 + a1*b6 + a2*b5 + a3*b4 + a4*b3 + a5*b2 + a6*b1 + a7*b0)
+	s8 := c8 - (a0*b8 + a1*b7 + a2*b6 + a3*b5 + a4*b4 + a5*b3 + a6*b2 + a7*b1 + a8*b0)
+	s9 := c9 - (a0*b9 + a1*b8 + a2*b7 + a3*b6 + a4*b5 + a5*b4 + a6*b3 + a7*b2 + a8*b1 + a9*b0)
+	s10 := c10 - (a0*b10 + a1*b9 + a2*b8 + a3*b7 + a4*b6 + a5*b5 + a6*b4 + a7*b3 + a8*b2 + a9*b1 + a10*b0)
+	s11 := c11 - (a0*b11 + a1*b10 + a2*b9 + a3*b8 + a4*b7 + a5*b6 + a6*b5 + a7*b4 + a8*b3 + a9*b2 + a10*b1 + a11*b0)
 	s12 := -(a1*b11 + a2*b10 + a3*b9 + a4*b8 + a5*b7 + a6*b6 + a7*b5 + a8*b4 + a9*b3 + a10*b2 + a11*b1)
 	s13 := -(a2*b11 + a3*b10 + a4*b9 + a5*b8 + a6*b7 + a7*b6 + a8*b5 + a9*b4 + a10*b3 + a11*b2)
 	s14 := -(a3*b11 + a4*b10 + a5*b9 + a6*b8 + a7*b7 + a8*b6 + a9*b5 + a10*b4 + a11*b3)
