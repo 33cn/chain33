eb8e8da7d4bf0fdb8292362e0f3893adf6f3fafb
madengji <mdj@disanbo.com>
2018-04-24 16:24:11 +0800

    added relay exec_local process
    Former-commit-id: fa6929f74962a679dc9a6aec4fe71472240c42f2

--- executor/drivers/relay/kv.go
+++ executor/drivers/relay/kv.go
@@ -3,19 +3,54 @@ package relay
 import "fmt"
 
 const (
-	sellOrderSHTAS = "token-sellorder-shtas:"
-	sellOrderASTS  = "token-sellorder-asts:"
-	sellOrderATSS  = "token-sellorder-atss:"
-	sellOrderTSPAS = "token-sellorder-tspas:"
-	buyOrderAHTSB  = "token-buyorder-ahtsb:"
-	buyOrderTAHSB  = "token-buyorder-tahsb:"
-	sellIDPrefix   = "mavl-relay-sell-"
+	relaySellOrderTSHSES = "relay-sellorder-tshses:"
+	relaySellOrderASTESS = "relay-sellorder-astess:"
+	relaySellOrderATSESS = "relay-sellorder-atsess:"
+	relayBuyOrderATSSE   = "relay-buyorder-atsse:"
+	sellIDPrefix         = "mavl-relay-sell-"
 )
 
-//特定状态下的卖单
-func calcTokenSellOrderKey(token string, addr string, status int32, sellOrderID string, height int64) []byte {
-	key := fmt.Sprintf(sellOrderSHTAS+"%d:%d:%s:%s:%s", status, height, token, addr, sellOrderID)
+//特定token+状态下的卖单
+func getSellOrderKeyToken(token string, status int32, sellOrderID string, sellamount, exchgamount int64, height int64) []byte {
+	key := fmt.Sprintf(relaySellOrderTSHSES+"%s:%d:%d:%d:%d:%s", token, status, height, sellamount, exchgamount, sellOrderID)
 	return []byte(key)
+}
+
+//特定账户下特定状态的卖单
+func getSellOrderKeyAddrStatus(token string, addr string, status int32, sellOrderID string, sellamount, exchgamount int64) []byte {
+	key := fmt.Sprintf(relaySellOrderASTESS+"%s:%d:%s:%d:%d:%s", addr, status, token, exchgamount, sellamount, sellOrderID)
+	return []byte(key)
+}
+
+//特定账户下特定token的卖单
+func getSellOrderKeyAddrToken(token string, addr string, status int32, sellOrderID string, sellamount, exchgamount int64) []byte {
+	key := fmt.Sprintf(relaySellOrderATSESS+"%s:%s:%d:%d:%d:%s", addr, token, status, exchgamount, sellamount, sellOrderID)
+	return []byte(key)
+}
+
+func getSellOrderPrefixTokenStatus(token string, status int32) []byte {
+	prefix := fmt.Sprintf(relaySellOrderTSHSES+"%s:%d:", token, status)
+	return []byte(prefix)
+}
+
+//特定账户下指定token的卖单
+func getSellOrderPrefixAddrToken(token string, addr string) []byte {
+	key := fmt.Sprintf(relaySellOrderATSESS+"%s:%s", addr, token)
+	return []byte(key)
+}
+
+//特定账户下的卖单
+func getSellOrderPrefixAddr(addr string) []byte {
+	return []byte(fmt.Sprintf(relaySellOrderATSESS+"%s", addr))
+}
+
+func getBuyOrderKeyAddr(addr string, token string, sellOrderID string, sellamount, exchgamount int64) []byte {
+	return []byte(fmt.Sprintf(relayBuyOrderATSSE+"%s:%s:%s:%d:%d", addr, token, sellOrderID, sellamount, exchgamount))
+}
+
+//特定账户下的买单
+func getBuyOrderPrefixAddr(addr string) []byte {
+	return []byte(fmt.Sprintf(relayBuyOrderATSSE+"%s", addr))
 }
 
 func calcRelaySellID(hash string) string {
