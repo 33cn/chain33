60242778c43554e0b7ab78765a92e1b28532162b
suyanlong <592938410@qq.com>
2018-04-25 15:16:56 +0800

    modify relayd function
    Former-commit-id: 093b1d19fddc36b1a21188ca997c91ed1d496e0e

--- cmd/relayd/relayd/db.go
+++ cmd/relayd/relayd/db.go
@@ -1,6 +1,8 @@
 package relayd
 
 import (
+	"fmt"
+
 	"gitlab.33.cn/chain33/chain33/common/db"
 )
 
@@ -9,12 +11,13 @@ import (
 
 // Namespace keys
 var (
-	hashKey     = []byte("h")
-	blockHeader = []byte("b")
+	blockHashPrefix = []byte("blockHash")
+	heightPrefix    = []byte("height")
+	orderPrefix     = []byte("order")
 )
 
 type relaydDB struct {
-	*db.GoLevelDB
+	db *db.GoLevelDB
 }
 
 func NewRelayDB(name string, dir string, cache int) *relaydDB {
@@ -23,4 +26,60 @@ func NewRelayDB(name string, dir string, cache int) *relaydDB {
 		panic(err)
 	}
 	return &relaydDB{d}
+}
+
+func (r *relaydDB) Get(key []byte) ([]byte, error) {
+	return r.db.Get(key)
+}
+
+func (r *relaydDB) Set(key, value []byte) error {
+	return r.db.Set(key, value)
+}
+
+func (r *relaydDB) storeOrder() {
+	panic("unimplemented")
+	// TODO 分两两种存储，一中是prefix + hash，另一种是prefix + status
+}
+
+func (r *relaydDB) storeHeader() {
+	panic("unimplemented")
+	// TODO 分两两种存储，一中是prefix + hash，另一种是prefix + height
+}
+
+func (r *relaydDB) queryOrderByHash(hash []byte) ([]byte, error) {
+	return r.db.Get(append(orderPrefix, hash...))
+}
+
+func (r *relaydDB) queryOrderByStatus(status uint64) [][]byte {
+	iter := r.db.Iterator(append(orderPrefix, fmt.Sprintf("%d", status)...), false)
+	var orders [][]byte
+	for {
+		if iter.Next() {
+			orders = append(orders, iter.Value())
+		} else {
+			break
+		}
+	}
+	return orders
+}
+
+func (r *relaydDB) BlockHeader(value interface{}) ([]byte, error) {
+	switch val := value.(type) {
+	case uint64:
+		return r.db.Get(makeHeightKey(val))
+
+	case []byte:
+		return r.db.Get(makeBlockHashKey(val))
+
+	default:
+		panic(val)
+	}
+}
+
+func makeHeightKey(height uint64) []byte {
+	return append(heightPrefix, []byte(fmt.Sprintf("%d", height))...)
+}
+
+func makeBlockHashKey(hash []byte) []byte {
+	return append(blockHashPrefix, hash...)
 }
