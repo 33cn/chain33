05ce8e8d03bf04da04e86cc9ada24b03ced16123
sanghg <houguisang@vip.qq.com>
2018-04-28 10:38:55 +0800

    add rpc_ctx_test.go, change name client to client_test

--- client/mockBlockChain_test.go
+++ client/mockBlockChain_test.go
@@ -1,4 +1,4 @@
-package client
+package client_test
 
 import (
 	"gitlab.33.cn/chain33/chain33/queue"
@@ -10,6 +10,7 @@ type mockBlockChain struct {
 
 func (m *mockBlockChain) SetQueueClient(q queue.Queue) {
 	go func() {
+		blockchainKey := "blockchain"
 		client := q.Client()
 		client.Sub(blockchainKey)
 		for msg := range client.Recv() {
