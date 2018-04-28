6b1c38c3e0d9ff445a691bd78a8ce0bd324ef43d
king wang <kingwang@kingdeMacBook-Pro.local>
2018-04-28 11:42:15 +0800

    fixbug build error
    Former-commit-id: 4f21250e9323420979c724b3cb10ffdc5c9d5ad6
    Former-commit-id: bbd3fa66080964e4947cb51acac94ddd07a541f5

--- consensus/drivers/ticket/ticket.go
+++ consensus/drivers/ticket/ticket.go
@@ -527,7 +527,7 @@ func (client *Client) addMinerTx(parent, block *types.Block, diff *big.Int, priv
 	//unshift
 	block.Difficulty = miner.Bits
 	//判断是替换还是append
-	ticketAction, err := client.getMinerTx(current.Block)
+	_, err := client.getMinerTx(block)
 	if err != nil {
 		block.Txs = append([]*types.Transaction{tx}, block.Txs...)
 	} else {
