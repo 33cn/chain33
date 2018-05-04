9a117bd72e94d4ab2ad2b7b62ca460df536aa585
pengjun <pengj@disanbo.com>
2018-05-04 15:55:25 +0800

    authority management:添加bccsp相关的代码
    Former-commit-id: ddd7a3275a3d5ea84c901cb7f4705eb6c62e8888
    Former-commit-id: b94e2120b53713b32b912ec9edf23434a2b55bdf

---
+++ authority/bccsp/factory/pkcs11/pkcs11factory.go
@@ -0,0 +1,64 @@
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+/*
+Notice: This file has been modified for Hyperledger Fabric SDK Go usage.
+Please review third_party pinning scripts and patches for more details.
+*/
+package pkcs11
+
+import (
+	"gitlab.33.cn/chain33/chain33/authority/bccsp"
+	"gitlab.33.cn/chain33/chain33/authority/bccsp/pkcs11"
+	"gitlab.33.cn/chain33/chain33/authority/bccsp/sw"
+	"github.com/pkg/errors"
+)
+
+const (
+	// PKCS11BasedFactoryName is the name of the factory of the hsm-based BCCSP implementation
+	PKCS11BasedFactoryName = "PKCS11"
+)
+
+// PKCS11Factory is the factory of the HSM-based BCCSP.
+type PKCS11Factory struct{}
+
+// Name returns the name of this factory
+func (f *PKCS11Factory) Name() string {
+	return PKCS11BasedFactoryName
+}
+
+// Get returns an instance of BCCSP using Opts.
+func (f *PKCS11Factory) Get(p11Opts *pkcs11.PKCS11Opts) (bccsp.BCCSP, error) {
+	// Validate arguments
+	if p11Opts == nil {
+		return nil, errors.New("Invalid config. It must not be nil.")
+	}
+
+	//TODO: PKCS11 does not need a keystore, but we have not migrated all of PKCS11 BCCSP to PKCS11 yet
+	var ks bccsp.KeyStore
+	if p11Opts.Ephemeral == true {
+		ks = sw.NewDummyKeyStore()
+	} else if p11Opts.FileKeystore != nil {
+		fks, err := sw.NewFileBasedKeyStore(nil, p11Opts.FileKeystore.KeyStorePath, false)
+		if err != nil {
+			return nil, errors.Wrapf(err, "Failed to initialize software key store")
+		}
+		ks = fks
+	} else {
+		// Default to DummyKeystore
+		ks = sw.NewDummyKeyStore()
+	}
+	return pkcs11.New(*p11Opts, ks)
+}
---
+++ authority/common/providers/core/provider.go
@@ -0,0 +1,34 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package core
+
+//CryptoSuiteConfig contains sdk configuration items for cryptosuite.
+type CryptoSuiteConfig interface {
+	IsSecurityEnabled() bool
+	SecurityAlgorithm() string
+	SecurityLevel() int
+	SecurityProvider() string
+	SoftVerify() bool
+	SecurityProviderLibPath() string
+	SecurityProviderPin() string
+	SecurityProviderLabel() string
+	KeyStorePath() string
+}
+
+// Providers represents the SDK configured core providers context.
+type Providers interface {
+	CryptoSuite() CryptoSuite
+	SigningManager() SigningManager
+}
+
+//ConfigProvider provides config backend for SDK
+type ConfigProvider func() (ConfigBackend, error)
+
+//ConfigBackend backend for all config types in SDK
+type ConfigBackend interface {
+	Lookup(key string) (interface{}, bool)
+}
