836e31b9a6b83271e01b45ba5b4e580f6d5761ee
bangzhu.li <bangzhu.li@foxmail.com>
2018-05-07 11:29:22 +0800

    更新
    Former-commit-id: cee430a246d50e29db4fdb5a4cdf4e800d637ee7
    Former-commit-id: feed8ba9786a7d4da71b650fc6676e60d6ca9b16

--- p2p/netaddress.go
+++ p2p/netaddress.go
@@ -144,7 +144,7 @@ func (na *NetAddress) DialTimeout(version int32) (*grpc.ClientConn, error) {
 	cliparm.Timeout = 10 * time.Second //等待10秒，如果Ping 没有响应，则超时
 	cliparm.PermitWithoutStream = true //启动keepalive 进行检查
 	keepaliveOp := grpc.WithKeepaliveParams(cliparm)
-	timeoutOp := grpc.WithTimeout(time.Duration(time.Second * 3))
+	timeoutOp := grpc.WithTimeout(time.Second * 3)
 
 	conn, err := grpc.Dial(na.String(), grpc.WithInsecure(),
 		grpc.WithDefaultCallOptions(grpc.UseCompressor("gzip")), grpc.WithServiceConfig(ch), keepaliveOp, timeoutOp)
