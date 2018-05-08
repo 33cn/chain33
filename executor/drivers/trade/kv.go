3ea32224505e4cdbc0d1748a53e3e00b9e885874
linjingljlj <linjingljlj@163.com>
2018-05-08 15:48:09 +0800

    move calcPriceOfToken to kv.go, it is part of key
    Former-commit-id: 1d5e09e6379cbb81dd427f69afbcc0e92b2b4203
    Former-commit-id: 6c7f3e1dba7982202c293a455cad2fb4dd45358c

--- executor/drivers/trade/kv.go
+++ executor/drivers/trade/kv.go
@@ -224,3 +224,10 @@ func genSellMarketOrderKeyValue(kv []*types.KeyValue, receipt *types.ReceiptTrad
 
 	return kv
 }
+
+// make a number as token's price whether cheap or dear
+// support 1e8 bty pre token or 1/1e8 bty pre token, [1Coins, 1e16Coins]
+// the number in key is used to sort buy orders and pages
+func calcPriceOfToken(priceBoardlot, AmountPerBoardlot int64) int64 {
+	return 1e8 * priceBoardlot / AmountPerBoardlot
+}
