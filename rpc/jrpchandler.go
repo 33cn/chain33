14fe6e8ecf23e0072abba9e52bb42138e3137498
linjingljlj <linjingljlj@163.com>
2018-05-09 11:27:45 +0800

    update sell market receipt log
    Former-commit-id: 455fdbc17b8c293a7796163e8c807f3151c476b6

--- rpc/jrpchandler.go
+++ rpc/jrpchandler.go
@@ -1070,7 +1070,7 @@ func DecodeLog(rlog *ReceiptData) (*ReceiptDataResult, error) {
 			logIns = logTmp
 		case types.TyLogTradeSellMarket:
 			lTy = "LogTradeSellMarket"
-			var logTmp types.ReceiptSellBase
+			var logTmp types.ReceiptSellMarket
 			err = types.Decode(lLog, &logTmp)
 			if err != nil {
 				return nil, err
