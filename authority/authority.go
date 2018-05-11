30746fe55a71446477ab01ab61db33b75610f208
wjx <13770935170@163.com>
2018-05-11 17:48:22 +0800

    update auth func
    Former-commit-id: ca1e67566061734bc2507a5f7e4cbfaa3848c48c

--- authority/authority.go
+++ authority/authority.go
@@ -1,27 +1,27 @@
 package authority
 
 import (
-	"gitlab.33.cn/chain33/chain33/types"
-	"gitlab.33.cn/chain33/chain33/queue"
-	"gitlab.33.cn/chain33/chain33/authority/common/providers/core"
-    "gitlab.33.cn/chain33/chain33/authority/cryptosuite/bccsp/sw"
-	"gitlab.33.cn/chain33/chain33/authority/cryptosuite"
-	"github.com/pkg/errors"
-	"gitlab.33.cn/chain33/chain33/authority/signingmgr"
 	log "github.com/inconshreveable/log15"
+	"github.com/pkg/errors"
+	"gitlab.33.cn/chain33/chain33/authority/common/providers/core"
+	"gitlab.33.cn/chain33/chain33/authority/cryptosuite"
+	"gitlab.33.cn/chain33/chain33/authority/cryptosuite/bccsp/sw"
+	"gitlab.33.cn/chain33/chain33/authority/signingmgr"
+	"gitlab.33.cn/chain33/chain33/queue"
+	"gitlab.33.cn/chain33/chain33/types"
 )
 
 var alog = log.New("module", "autority")
 
 type Authority struct {
-	cryptoPath string
-	client     queue.Client
-	cfg        *types.Authority
+	cryptoPath  string
+	client      queue.Client
+	cfg         *types.Authority
 	cryptoSuite core.CryptoSuite
-	signer   *signingmgr.SigningManager
+	signer      *signingmgr.SigningManager
 }
 
-func New(conf *types.Authority) *Authority{
+func New(conf *types.Authority) *Authority {
 	auth := &Authority{}
 	auth.initConfig(conf)
 	auth.cryptoPath = conf.CryptoPath
@@ -30,28 +30,48 @@ func New(conf *types.Authority) *Authority{
 	return auth
 }
 
-func (auth *Authority)initConfig(conf *types.Authority) error {
+func (auth *Authority) initConfig(conf *types.Authority) error {
 	config := &cryptosuite.CryptoConfig{conf}
 
-	cryptoSuite,err := sw.GetSuiteByConfig(config)
+	cryptoSuite, err := sw.GetSuiteByConfig(config)
 	if err != nil {
 		return errors.WithMessage(err, "Failed to initialize crypto suite")
 	}
 	auth.cryptoSuite = cryptoSuite
 
-
-	signer,err := signingmgr.New(cryptoSuite)
+	signer, err := signingmgr.New(cryptoSuite)
 	if err != nil {
 		return errors.WithMessage(err, "Failed to initialize signing manage")
 	}
 	auth.signer = signer
 
-
-
 	return nil
 }
 
 func (auth *Authority) SetQueueClient(client queue.Client) {
+	auth.client = client
+	auth.client.Sub("authority")
+
+	//recv 消息的处理
+	go func() {
+		for msg := range client.Recv() {
+			alog.Debug("authority recv", "msg", msg)
+			if msg.Ty == types.EventAuthoritySignTx {
+				go auth.procSignTx(msg)
+			} else if msg.Ty == types.EventAuthorityCheckTx {
+				go auth.procCheckTx(msg)
+			}
+		}
+	}()
+}
+
+func (auth *Authority) procSignTx(msg queue.Message) {
+
+	//msg.Reply(exec.client.NewMessage("", types.EventReceipts,
+	//	&types.Receipts{receipts}))
+}
+
+func (auth *Authority) procCheckTx(msg queue.Message) {
 
 }
 
