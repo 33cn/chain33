7d77f3951fe095770ecd6c160fe20aa87c38b118
linjingljlj <linjingljlj@163.com>
2018-05-14 15:40:59 +0800

    soldboardlot -> soldBoardlot

--- cmd/cli/commands/trade.go
+++ cmd/cli/commands/trade.go
@@ -199,7 +199,7 @@ func parseSellOrders(res types.ReplySellOrders) {
 		sellOrders2show.Minboardlot = sellorder.MinBoardlot
 		sellOrders2show.Priceperboardlot = strconv.FormatFloat(float64(sellorder.PricePerBoardlot)/float64(types.Coin), 'f', 8, 64)
 		sellOrders2show.Totalboardlot = sellorder.TotalBoardlot
-		sellOrders2show.Soldboardlot = sellorder.Soldboardlot
+		sellOrders2show.Soldboardlot = sellorder.SoldBoardlot
 		sellOrders2show.SellID = sellorder.SellID
 		sellOrders2show.Status = types.SellOrderStatus[sellorder.Status]
 		sellOrders2show.Height = sellorder.Height
