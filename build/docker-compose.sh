522c3ea00cecfce9bc377bc9cf3315c195be7b52
lordcherrywood <lauren9292@163.com>
2018-05-23 18:05:27 +0800

    test
    Former-commit-id: c9d1c83756f6215413f444ab35e5d1e203fe81d3
    Former-commit-id: 1b9bbf0fb01e7ba85d29e8441ce21929efd8c910

--- build/docker-compose.sh
+++ build/docker-compose.sh
@@ -71,8 +71,8 @@ fi
 
 sleep 2
 
-echo "=========== # import private key transer ============="
-result=$(./chain33-cli account import_key -k CC38546E9E659D15E6B4893F0AB32A06D103931A8230B0BDE71459D2B27D6944 -l transfer | jq ".label")
+echo "=========== # import private key transfer ============="
+result=$(./chain33-cli account import_key -k 56942AD84CCF4788ED6DACBC005A1D0C4F91B63BCF0C99A02BE03C8DEAE71138 -l transfer | jq ".label")
 echo ${result}
 if [ -z "${result}" ]; then
     exit 1
@@ -81,7 +81,7 @@ fi
 sleep 2
 
 echo "=========== # import private key mining ============="
-result=$(./chain33-cli account import_key -k 4257D8692EF7FE13C68B65D6A52F03933DB2FA5CE8FAF210B5B8B80C721CED01 -l mining | jq ".label")
+result=$(./chain33-cli account import_key -k B0BB75BC49A787A71F4834DA18614763B53A18291ECE6B5EDEC3AD19D150C3E7 -l mining | jq ".label")
 echo ${result}
 if [ -z "${result}" ]; then
     exit 1
