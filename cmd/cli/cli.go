8970f3a1e06de4ccc1d8f73caf8ef4e71ee9bc6c
madengji <mdj@disanbo.com>
2018-05-24 20:31:08 +0800

    add relay commands
    Former-commit-id: e885e72c2e8bf91a8925999032f7553d019dfc89

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -37,6 +37,7 @@ func init() {
 		commands.TicketCmd(),
 		commands.TokenCmd(),
 		commands.TradeCmd(),
+		commands.RelayCmd(),
 		commands.TxCmd(),
 		commands.WalletCmd(),
 		commands.VersionCmd(),
