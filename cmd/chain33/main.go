a3d16eeec4960ac4e3bb39084260532ee55b1679
king wang <kingwang@kingdeMacBook-Pro.local>
2018-05-27 23:12:02 +0800

    change main.go, not only for bty

--- cmd/chain33/main.go
+++ cmd/chain33/main.go
@@ -60,7 +60,7 @@ func main() {
 	cfg := config.InitCfg(*configPath)
 	//set test net flag
 	types.SetTestNet(cfg.TestNet)
-	types.SetTitle("bityuan")
+	types.SetTitle(cfg.Title)
 	//compare minFee in wallet, mempool, exec
 	if cfg.Exec.MinExecFee > cfg.MemPool.MinTxFee || cfg.MemPool.MinTxFee > cfg.Wallet.MinFee {
 		panic("config must meet: wallet.minFee >= mempool.minTxFee >= exec.minExecFee")
@@ -95,7 +95,7 @@ func main() {
 
 	//开始区块链模块加载
 	//channel, rabitmq 等
-	log.Info(cfg.Title + version.GetVersion())
+	log.Info(cfg.Title + " " + version.GetVersion())
 	log.Info("loading queue")
 	q := queue.New("channel")
 
