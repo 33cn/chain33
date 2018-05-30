62d89a68fd5f58d0026531a8c16f944bb55da194
lordcherrywood <lauren9292@163.com>
2018-05-30 15:24:03 +0800

    test
    Former-commit-id: ff3b50fa7032b4b59354782fa5e74b2f7670e750
    Former-commit-id: f26f818f9339899479e6a8b4fd05113ee6c19cd8

--- cmd/cli/commands/utils.go
+++ cmd/cli/commands/utils.go
@@ -195,7 +195,7 @@ func GetExecAddr(exec string) (string, error) {
 func isAllowExecName(exec string) (bool, error) {
 	// exec name长度不能超过50
 	if len(exec) > 50 {
-		return false, types.ErrExecNameNotAllow
+		return false, types.ErrExecNameNotMatch
 	}
 	// exec name中不允许有 "-"
 	if strings.Contains(exec, "-") {
