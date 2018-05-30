04d1ffd0f640642d9d59064a70b9a6aa3c914d58
Shuo Ren <rens@disanbo.com>
2018-05-30 16:32:30 +0800

    test

--- build/docker-compose.sh
+++ build/docker-compose.sh
@@ -156,7 +156,7 @@ function transfer(){
 
     sleep 2
     echo "=========== # withdraw ============="
-    hash=$(chain33-cli send bty send_exec -a 2 -n test -e hashlock -k CC38546E9E659D15E6B4893F0AB32A06D103931A8230B0BDE71459D2B27D6944)
+    hash=$(chain33-cli send bty send_exec -a 2 -n deposit -e hashlock -k CC38546E9E659D15E6B4893F0AB32A06D103931A8230B0BDE71459D2B27D6944)
     echo "${hash}"
     sleep 20
     before=$(chain33-cli account balance -a 14KEKbYtKKQm4wMthSK9J4La4nAiidGozt -e hashlock | jq ".balance")
@@ -166,7 +166,7 @@ function transfer(){
         exit 1
     fi
 
-    hash=$(chain33-cli send bty withdraw -a 1 -n withdraw -e hashlock -k CC38546E9E659D15E6B4893F0AB32A06D103931A8230B0BDE71459D2B27D6944)
+    hash=$(chain33-cli send bty withdraw -a 2 -n withdraw -e hashlock -k CC38546E9E659D15E6B4893F0AB32A06D103931A8230B0BDE71459D2B27D6944)
     echo "${hash}"
     sleep 20
     txs=$(chain33-cli tx query_hash -s "${hash}" | jq ".txs")
