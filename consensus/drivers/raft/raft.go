5f6f9537d9b30ad3957b263852df7feeb8554e88
lihailei <18761806026@163.com>
2018-05-30 14:42:58 +0800

    增加stopC通道用于外界可控关闭raft共识，对raft_test.go测试后留下的数据进行自动清理

--- consensus/drivers/raft/raft.go
+++ consensus/drivers/raft/raft.go
@@ -62,7 +62,7 @@ type raftNode struct {
 }
 
 func NewRaftNode(id int, join bool, peers []string, readOnlyPeers []string, addPeers []string, getSnapshot func() ([]byte, error), proposeC <-chan *types.Block,
-	confChangeC <-chan raftpb.ConfChange) (<-chan *types.Block, <-chan error, <-chan *snap.Snapshotter, <-chan bool,*raftNode) {
+	confChangeC <-chan raftpb.ConfChange) (<-chan *types.Block, <-chan error, <-chan *snap.Snapshotter, <-chan bool, chan<- struct{}) {
 
 	rlog.Info("Enter consensus raft")
 	// commit channel
@@ -91,7 +91,7 @@ func NewRaftNode(id int, join bool, peers []string, readOnlyPeers []string, addP
 	}
 	go rc.startRaft()
 
-	return commitC, errorC, rc.snapshotterReady, rc.validatorC,rc
+	return commitC, errorC, rc.snapshotterReady, rc.validatorC, rc.stopc
 }
 
 //  启动raft节点
@@ -185,7 +185,6 @@ func (rc *raftNode) serveRaft() {
 	err = (&http.Server{Handler: rc.transport.Handler()}).Serve(ln)
 	if err != nil {
 		rlog.Error(fmt.Sprintf("raft: Failed to serve rafthttp (%v)", err.Error()))
-		panic(err)
 	}
 	select {
 	case <-rc.httpstopc:
@@ -437,10 +436,13 @@ func (rc *raftNode) openWAL(snapshot *raftpb.Snapshot) *wal.WAL {
 
 // 关闭http连接和channel
 func (rc *raftNode) stop() {
+	rc.wal.Close()
 	rc.stopHTTP()
 	close(rc.commitC)
 	close(rc.errorC)
+	close(rc.stopc)
 	rc.node.Stop()
+	close(rc.validatorC)
 }
 
 func (rc *raftNode) stopHTTP() {
@@ -452,6 +454,7 @@ func (rc *raftNode) stopHTTP() {
 func (rc *raftNode) writeError(err error) {
 	rc.stopHTTP()
 	close(rc.commitC)
+	close(rc.stopc)
 	rc.errorC <- err
 	close(rc.errorC)
 	rc.node.Stop()
@@ -557,7 +560,4 @@ func (rc *raftNode) addReadOnlyPeers() {
 			}
 		}
 	}
-}
-func (rc *raftNode)Close(){
-	rc.wal.Close()
 }
