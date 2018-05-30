b8bf084ae2ea9697856d6cddabc8c5073f857502
任硕 <rens@disanbo.com>
2018-05-30 17:58:39 +0800

    Update send.go
    Former-commit-id: bde69bbfa130c8d70a4e91ba852bcda30a089f8d

--- cmd/cli/commands/send.go
+++ cmd/cli/commands/send.go
@@ -22,33 +22,32 @@ func OneStepSend(args []string) {
 		return
 	}
 	hasKey := false
-	keyIndex := -1
-	hasRpc := false
 	var key string
-	var rpcAddr string
 	size := len(params)
-
-	// 先查找需要的参数并记录索引
 	for i, v := range params {
 		if v == "-k" {
 			hasKey = true
 			if i < size-1 {
 				key = params[i+1]
-				keyIndex = i
+				params = append(params[:i], params[i+2:]...)
 			} else {
 				fmt.Fprintln(os.Stderr, "no private key found")
 				return
 			}
-		} else if v == "--rpc_laddr" {
-			hasRpc = true
+		}
+	}
+	hasAddr := false
+	var rpcAddr string
+	size := len(params)
+	for i, v := range params {
+		if v == "--rpc_laddr" {
+			hasAddr = true
 			if i < size-1 {
 				rpcAddr = params[i+1]
+				params = append(params[:i], params[i+2:]...)
 			}
 		}
 	}
-	// 再修改参数数组，将不需要的参数过滤掉
-	params = append(params[:keyIndex], params[keyIndex+2:]...)
-
 	var isAddr bool
 	err := account.CheckAddress(key)
 	if err != nil {
@@ -82,6 +81,7 @@ func OneStepSend(args []string) {
 	if isAddr {
 		addrOrKey = "-a"
 	}
+
 	cParams := []string{"wallet", "sign", "-d", string(bufCreate[:len(bufCreate)-1]), addrOrKey, key}
 	if hasRpc {
 		cParams = append(cParams, "--rpc_laddr", rpcAddr)
@@ -102,12 +102,10 @@ func OneStepSend(args []string) {
 	//fmt.Println("signedTx", outSign.String(), errSign.String())
 
 	bufSign := outSign.Bytes()
-
 	cParams = []string{"wallet", "send", "-d", string(bufSign[:len(bufSign)-1])}
 	if hasRpc {
 		cParams = append(cParams, "--rpc_laddr", rpcAddr)
 	}
-
 	cmdSend := exec.Command(name, cParams...)
 	var outSend bytes.Buffer
 	var errSend bytes.Buffer
