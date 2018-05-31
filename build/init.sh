a0ec338c56db62c8b59d8a3f448cb895e4f38286
Litian <lit@disanbo.com>
2018-05-31 11:20:26 +0800

    Delete init.sh

--- build/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cli seed save -s "crew clown absent desert grit nerve whale toilet clean action point wall actress equip describe" -p "litian"
-
-cli wallet unlock -p litian
-
-cli account import_key -k "CC38546E9E659D15E6B4893F0AB32A06D103931A8230B0BDE71459D2B27D6944" -l test1
-
-cli account create -l test001
