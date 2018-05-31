3789d4416a9a3813a99c84568fa2f5afeb53560f
suyanlong <592938410@qq.com>
2018-05-31 21:17:25 +0800

    Add TestJSONClient_Call unit
    Former-commit-id: 6a2bcc53e42d071fa09e3649a4486e74ac266fa5
    Former-commit-id: a20c24e7fc4fa06035876ef567c9e00f274826dc

--- rpc/server_test.go
+++ rpc/server_test.go
@@ -3,7 +3,13 @@ package rpc
 import (
 	"testing"
 
+	"time"
+
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"gitlab.33.cn/chain33/chain33/client/mocks"
+	qmocks "gitlab.33.cn/chain33/chain33/queue/mocks"
+	"gitlab.33.cn/chain33/chain33/types"
 )
 
 func TestCheckWhitlist(t *testing.T) {
@@ -18,4 +24,52 @@ func TestCheckWhitlist(t *testing.T) {
 	assert.True(t, checkWhitlist(address))
 	assert.True(t, checkWhitlist("192.168.3.2"))
 
+}
+
+func TestJSONClient_Call(t *testing.T) {
+	rpcCfg = new(types.Rpc)
+	rpcCfg.GrpcBindAddr = "127.0.0.1:8101"
+	rpcCfg.JrpcBindAddr = "127.0.0.1:8200"
+	rpcCfg.Whitlist = []string{"127.0.0.1", "0.0.0.0"}
+	Init(rpcCfg)
+	server := NewJSONRPCServer(&qmocks.Client{})
+	assert.NotNil(t, server)
+
+	api := new(mocks.QueueProtocolAPI)
+	testChain33 := newTestChain33(api)
+	assert.NotNil(t, testChain33)
+	server.jrpc = *testChain33
+	go server.Listen()
+
+	ret := &types.Reply{
+		IsOk: true,
+		Msg:  []byte("123"),
+	}
+	api.On("IsSync").Return(ret, nil)
+	api.On("Close").Return()
+
+	time.Sleep(100)
+	jsonClient, err := NewJSONClient("http://" + rpcCfg.JrpcBindAddr + "/root")
+	assert.Nil(t, err)
+	assert.NotNil(t, jsonClient)
+	var result = ""
+	err = jsonClient.Call("Chain33.Version", nil, &result)
+	assert.NotNil(t, err)
+	assert.Empty(t, result)
+
+	jsonClient, err = NewJSONClient("http://" + rpcCfg.JrpcBindAddr)
+	assert.Nil(t, err)
+	assert.NotNil(t, jsonClient)
+
+	err = jsonClient.Call("Chain33.Version", nil, &result)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, result)
+
+	var isSnyc bool
+	err = jsonClient.Call("Chain33.IsSync", &types.ReqNil{}, &isSnyc)
+	assert.Nil(t, err)
+	assert.Equal(t, ret.GetIsOk(), isSnyc)
+
+	server.Close()
+	mock.AssertExpectationsForObjects(t, api)
 }
