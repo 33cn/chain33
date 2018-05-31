1c0047779e18b60a3f728d05ab1c7f632f6f12c2
wjx <13770935170@163.com>
2018-05-31 10:39:18 +0800

    add crypto tool

--- authority/mspmgr/configbuilder.go
+++ authority/mspmgr/configbuilder.go
@@ -24,9 +24,10 @@ import (
 	"path/filepath"
 
 	"os"
-	"gitlab.33.cn/chain33/chain33/authority/cryptosuite"
-	"gitlab.33.cn/chain33/chain33/authority/bccsp/factory"
+
 	"gitlab.33.cn/chain33/chain33/authority/bccsp"
+	"gitlab.33.cn/chain33/chain33/authority/bccsp/factory"
+	"gitlab.33.cn/chain33/chain33/authority/cryptosuite"
 )
 
 type OrganizationalUnitIdentifiersConfiguration struct {
@@ -96,12 +97,12 @@ func getPemMaterialFromDir(dir string) ([][]byte, error) {
 }
 
 const (
-	cacerts              = "cacerts"
-	admincerts           = "admincerts"
-	signcerts            = "signcerts"
-	keystore             = "keystore"
-	intermediatecerts    = "intermediatecerts"
-	crlsfolder           = "crls"
+	cacerts           = "cacerts"
+	admincerts        = "admincerts"
+	signcerts         = "signcerts"
+	keystore          = "keystore"
+	intermediatecerts = "intermediatecerts"
+	crlsfolder        = "crls"
 )
 
 func SetupBCCSPKeystoreConfig(bccspConfig *factory.FactoryOpts, conf *cryptosuite.CryptoConfig, keystoreDir string) *factory.FactoryOpts {
@@ -152,9 +153,9 @@ func getMspConfig(dir string, signcert []byte, conf *cryptosuite.CryptoConfig) (
 	}
 
 	admincert, err := getPemMaterialFromDir(admincertDir)
-	if err != nil || len(admincert) == 0 {
-		return nil, fmt.Errorf("Could not load a valid admin certificate from directory %s, err %s", admincertDir, err)
-	}
+	//if err != nil || len(admincert) == 0 {
+	//	return nil, fmt.Errorf("Could not load a valid admin certificate from directory %s, err %s", admincertDir, err)
+	//}
 
 	intermediatecerts, err := getPemMaterialFromDir(intermediatecertsDir)
 	if os.IsNotExist(err) {
@@ -172,14 +173,14 @@ func getMspConfig(dir string, signcert []byte, conf *cryptosuite.CryptoConfig) (
 
 	// Set MspCryptoConfig
 	var hashFunction string
-	tmpValue:=conf.SecurityLevel()
+	tmpValue := conf.SecurityLevel()
 	switch tmpValue {
 	case 256:
 		hashFunction = bccsp.SHA256
 	case 384:
 		hashFunction = bccsp.SHA384
 	default:
-		mspLogger.Error(fmt.Sprintf("Invalid security level value:%d",tmpValue))
+		mspLogger.Error(fmt.Sprintf("Invalid security level value:%d", tmpValue))
 	}
 
 	crypto := &MSPCryptoConfig{
@@ -192,9 +193,9 @@ func getMspConfig(dir string, signcert []byte, conf *cryptosuite.CryptoConfig) (
 		Admins:            admincert,
 		RootCerts:         cacerts,
 		IntermediateCerts: intermediatecerts,
-		SigningCert:   signcert,
-		RevocationList:                crls,
-		CryptoConfig:                  crypto,
+		SigningCert:       signcert,
+		RevocationList:    crls,
+		CryptoConfig:      crypto,
 	}
 
 	return mspconf, nil
---
+++ authority/tools/cryptogen/msp/generator.go
@@ -0,0 +1,154 @@
+/*
+Copyright IBM Corp. 2017 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package msp
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"os"
+	"path/filepath"
+
+	"encoding/hex"
+
+	"gitlab.33.cn/chain33/chain33/authority/bccsp"
+	"gitlab.33.cn/chain33/chain33/authority/bccsp/factory"
+	"gitlab.33.cn/chain33/chain33/authority/tools/cryptogen/ca"
+	"gitlab.33.cn/chain33/chain33/authority/tools/cryptogen/csp"
+)
+
+func GenerateLocalMSP(baseDir, name string, signCA *ca.CA) error {
+
+	// create folder structure
+
+	err := createFolderStructure(baseDir, true)
+	if err != nil {
+		return err
+	}
+
+	/*
+		Create the MSP identity artifacts
+	*/
+	// get keystore path
+	keystore := filepath.Join(baseDir, "keystore")
+
+	// generate private key
+	priv, _, err := csp.GeneratePrivateKey(keystore)
+	if err != nil {
+		return err
+	}
+
+	// get public key
+	ecPubKey, err := csp.GetECPublicKey(priv)
+	if err != nil {
+		return err
+	}
+	// generate X509 certificate using signing CA
+	cert, err := signCA.SignCertificate(filepath.Join(baseDir, "signcerts"),
+		name, []string{}, ecPubKey, x509.KeyUsageDigitalSignature, []x509.ExtKeyUsage{})
+	if err != nil || cert == nil {
+		return err
+	}
+
+	// write artifacts to MSP folders
+
+	// the signing CA certificate goes into cacerts
+	err = x509Export(filepath.Join(baseDir, "cacerts", x509Filename(signCA.Name)), signCA.SignCert)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GenerateVerifyingMSP(baseDir string, signCA *ca.CA) error {
+
+	// create folder structure and write artifacts to proper locations
+	err := createFolderStructure(baseDir, false)
+	if err == nil {
+		// the signing CA certificate goes into cacerts
+		err = x509Export(filepath.Join(baseDir, "cacerts", x509Filename(signCA.Name)), signCA.SignCert)
+		if err != nil {
+			return err
+		}
+	}
+
+	// create a throwaway cert to act as an admin cert
+	// NOTE: the admincerts folder is going to be
+	// cleared up anyway by copyAdminCert, but
+	// we leave a valid admin for now for the sake
+	// of unit tests
+	factory.InitFactories(nil)
+	bcsp := factory.GetDefault()
+	priv, err := bcsp.KeyGen(&bccsp.ECDSAP256KeyGenOpts{Temporary: true})
+	ecPubKey, err := csp.GetECPublicKey(priv)
+	if err != nil {
+		return err
+	}
+	_, err = signCA.SignCertificate(filepath.Join(baseDir, "admincerts"), signCA.Name,
+		[]string{""}, ecPubKey, x509.KeyUsageDigitalSignature, []x509.ExtKeyUsage{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func createFolderStructure(rootDir string, local bool) error {
+
+	var folders []string
+	// create admincerts, cacerts, keystore and signcerts folders
+	folders = []string{
+		filepath.Join(rootDir, "cacerts"),
+	}
+	if local {
+		folders = append(folders, filepath.Join(rootDir, "keystore"),
+			filepath.Join(rootDir, "signcerts"))
+	}
+
+	for _, folder := range folders {
+		err := os.MkdirAll(folder, 0755)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func x509Filename(name string) string {
+	return name + "-cert.pem"
+}
+
+func x509Export(path string, cert *x509.Certificate) error {
+	return pemExport(path, "CERTIFICATE", cert.Raw)
+}
+
+func keyExport(keystore, output string, key bccsp.Key) error {
+	id := hex.EncodeToString(key.SKI())
+
+	return os.Rename(filepath.Join(keystore, id+"_sk"), output)
+}
+
+func pemExport(path, pemType string, bytes []byte) error {
+	//write pem out to file
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return pem.Encode(file, &pem.Block{Type: pemType, Bytes: bytes})
+}
