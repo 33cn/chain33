b227c4949218e832006a0c412beefc491289d499
Litian <lit@disanbo.com>
2018-05-31 11:21:14 +0800

    Delete close.sh

--- build/close.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PID=`ps -ef|grep 'dlv --headless=true'|grep -v grep|awk '{print $2}'`
-kill -9 $PID
-PID=`ps -ef|grep 'chain33 -f test.chain33.toml'|grep -v grep|awk '{print $2}'`
-kill -9 $PID
