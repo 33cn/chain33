e48437af12a109c608fff69e2a7dbe1aae57f809
林敬 <linj@disanbo.com>
2018-05-31 16:27:37 +0800

    import code for get miner info
    Former-commit-id: 4e61df00e9ce3ca4c2f6d2a35ac51409e1c6f1c8

---
+++ cmd/miner_accounts/accounts/block.go
@@ -0,0 +1,136 @@
+package accounts
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	chain33rpc "gitlab.33.cn/chain33/chain33/rpc"
+	"gitlab.33.cn/chain33/chain33/types"
+
+)
+
+// ts/height -> blockHeader
+type chain33 struct {
+	lastHeader * chain33rpc.Header
+	// height -> ts -> header
+	Headers map[int64]* chain33rpc.Header
+	Height2Ts map[int64]int64
+	// new only cache ticket for miner
+	accountCache map[int64]*types.Accounts
+}
+
+func (b chain33) findBlock(ts int64) (int64, *chain33rpc.Header) {
+	if ts > b.lastHeader.BlockTime {
+		ts = b.lastHeader.BlockTime
+		return ts, b.lastHeader
+	}
+	// 到底怎么样才能快速根据时间找到block
+	//  1. 一般情况下， 几秒内会有块， 所以直接根据 ts ， 进行前后搜索
+	for delta := int64(1); delta < int64(100); delta ++ {
+		if _, ok := b.Headers[ts-delta]; ok {
+			return ts-delta, b.Headers[ts-delta]
+		}
+	}
+
+	return 0, nil
+}
+func (b chain33) getBalance(addrs []string, exec string, timeNear int64) (int64, []*chain33rpc.Account, error) {
+	rpcCli, err := chain33rpc.NewJSONClient("http://127.0.0.1:8801")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return timeNear, nil, err
+	}
+	realTs, header := b.findBlock(timeNear)
+	if realTs == 0 || header == nil {
+		return timeNear, nil, types.ErrNotFound
+	}
+
+	// TODO cache result
+	acc, err := getBalanceAt(rpcCli, addrs, exec, header.StateHash)
+	return realTs, acc, err
+}
+
+func (b chain33) addBlock(h *chain33rpc.Header) error {
+	b.Headers[h.BlockTime] = h
+	b.Height2Ts[h.Height] = h.BlockTime
+	if h.Height > b.lastHeader.Height {
+		cache.lastHeader = h
+	}
+	// TODO check fork
+	return nil
+}
+
+var cache = chain33{
+	lastHeader:&chain33rpc.Header{Height:0},
+	Headers: map[int64]* chain33rpc.Header{},
+	Height2Ts: map[int64]int64{},
+	accountCache : map[int64]*types.Accounts{},
+}
+
+func getLastHeader(cli *chain33rpc.JSONClient) (*chain33rpc.Header, error) {
+	method := "Chain33.GetLastHeader"
+	var res chain33rpc.Header
+	err := cli.Call(method, nil, &res)
+	return &res, err
+}
+
+func getHeaders(cli *chain33rpc.JSONClient, start, end int64) (*chain33rpc.Headers, error) {
+	method := "Chain33.GetHeaders"
+	params := &types.ReqBlocks{Start: start, End: end, IsDetail: false}
+	var res chain33rpc.Headers
+	err := cli.Call(method, params, &res)
+	return &res, err
+}
+
+func getBalanceAt(cli *chain33rpc.JSONClient, addrs []string, exec , stateHash string) ([]*chain33rpc.Account, error) {
+	method := "Chain33.GetBalance"
+	params := &types.ReqBalance{Addresses: addrs, Execer: exec, StateHash: stateHash}
+	var res []*chain33rpc.Account
+	err := cli.Call(method, params, &res)
+	return res, err
+}
+
+func syncHeaders() {
+	rpcCli, err := chain33rpc.NewJSONClient("http://127.0.0.1:8801")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	last, err := getLastHeader(rpcCli)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	curHeight := cache.lastHeader.Height
+	lastHeight := last.Height
+
+	for curHeight < lastHeight {
+		hs, err := getHeaders(rpcCli, curHeight, curHeight + 100)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+		for _, h := range hs.Items {
+			if h.Height > cache.lastHeader.Height {
+				curHeight = h.Height
+			}
+			cache.addBlock(h)
+		}
+		fmt.Fprintln(os.Stderr, err, cache.lastHeader.Height)
+	}
+
+	fmt.Fprintln(os.Stderr, err, cache.lastHeader.Height)
+}
+
+func SyncBlock() {
+	syncHeaders()
+
+	for true {
+		timeout := time.NewTicker(time.Minute)
+		select {
+		case <-timeout.C:
+			syncHeaders()
+		}
+	}
+}
---
+++ cmd/miner_accounts/accounts/show.go
@@ -0,0 +1,142 @@
+package accounts
+
+import (
+
+	"time"
+
+	l "github.com/inconshreveable/log15"
+
+	"gitlab.33.cn/chain33/chain33/types"
+
+	//"encoding/json"
+	//"io/ioutil"
+	"strconv"
+	"fmt"
+	"gitlab.33.cn/chain33/chain33/rpc"
+)
+
+var log = l.New("module", "accounts")
+
+type ShowMinerAccount struct {
+	DataDir string
+}
+
+func (*ShowMinerAccount) Echo(in *string, out *interface{}) error {
+	if in == nil {
+		return types.ErrInputPara
+	}
+	*out = *in
+	return nil
+}
+
+type TimeAt struct {
+	// YYYY-mm-dd-HH
+	TimeAt string `json:"timeAt"`
+}
+
+func (show *ShowMinerAccount) Get(in *TimeAt, out *interface{}) error {
+	if in == nil {
+		log.Error("show", "in", "nil")
+		return types.ErrInputPara
+	}
+	if len(in.TimeAt) == 0 {
+		log.Error("show", "in.TimeAt", "empty")
+		return types.ErrInputPara
+	}
+	tm, err := time.Parse("2006-01-02-15", in.TimeAt)
+	if err != nil {
+		log.Error("show", "in.TimeAt Parse", err)
+		return types.ErrInputPara
+	}
+	seconds := tm.Unix()
+	log.Info("show", "utc", seconds)
+
+	addrs := []string{"1FB8L3DykVF7Y78bRfUrRcMZwesKue7CyR"} // TODO config or request
+	realTs, curAcc, err := cache.getBalance(addrs, "ticket", seconds)
+	if err != nil {
+		return nil
+	}
+	lastReadTs, lastAcc, err := cache.getBalance(addrs, "ticket", realTs-3600)
+	if err != nil {
+		return nil
+	}
+	fmt.Print(curAcc, lastAcc)
+	miner := calcResult(curAcc, lastAcc)
+	miner.Seconds = realTs - lastReadTs
+	*out = &miner
+
+	return nil
+}
+
+func calcResult(acc1, acc2 []*rpc.Account) *MinerAccounts {
+	type minerAt struct {
+		addr string
+		curAcc *rpc.Account
+		lastAcc *rpc.Account
+	}
+	miners := map[string]*minerAt{}
+	for _, a := range acc1 {
+		miners[a.Addr] = &minerAt{a.Addr, a, nil}
+	}
+	for _, a := range acc2 {
+		if _, ok := miners[a.Addr]; ok {
+			miners[a.Addr].lastAcc = a
+		}
+	}
+	miner := MinerAccounts{}
+	for k, v := range miners {
+		if v.lastAcc != nil && v.curAcc != nil {
+			total := v.curAcc.Balance + v.curAcc.Frozen
+			increase := total - v.lastAcc.Balance - v.lastAcc.Frozen
+			m :=  &MinerAccount{
+				Addr: k,
+				Total: strconv.FormatFloat(float64(total)/float64(types.Coin), 'f', 4, 64),
+				Increase: strconv.FormatFloat(float64(increase)/float64(types.Coin), 'f', 4, 64),
+			}
+			miner.MinerAccounts = append(miner.MinerAccounts, m)
+		}
+	}
+	return &miner
+
+}
+
+/*
+func readJson(jsonFile string) (*Accounts, error) {
+	d1, err := ioutil.ReadFile(jsonFile)
+	if err != nil {
+		log.Error("show", "read json", jsonFile, "err", err)
+		return nil, err
+	}
+	var acc Accounts
+	err = json.Unmarshal([]byte(d1), &acc)
+	if err != nil {
+		log.Error("show", "read json", jsonFile, "err", err)
+		return nil, err
+	}
+	return &acc, nil
+}
+
+func parseAccounts(acc *Accounts) (*map[string]float64, error) {
+	result := map[string]float64{}
+	for _, a := range acc.Accounts {
+		f1, e1 := strconv.ParseFloat(a.Balance, 64)
+		f2, e2 := strconv.ParseFloat(a.Frozen, 64)
+		if e1 != nil || e2 != nil {
+			log.Error("show", "account2  len", e1, "account  len", e2)
+			return nil, types.ErrNotFound
+		}
+		result[a.Addr] = f1 + f2
+	}
+	return &result, nil
+}
+
+func getAccountDetail(jsonFile string) (*map[string]float64, error) {
+	acc, err := readJson(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+	return parseAccounts(acc)
+}
+
+*/
+
