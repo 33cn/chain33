4e19fdba0f8804fe12ce942402ea2607ee3bd948
pengjun <pengj@disanbo.com>
2018-06-06 18:27:05 +0800

    authority-management:删除无效代码
    Former-commit-id: 7502ad7ada8dde260fd0a83ab70608d2f25002c8

--- authority/bccsp/factory/swfactory.go
+++ authority/bccsp/factory/swfactory.go
@@ -22,34 +22,29 @@ import (
 	"gitlab.33.cn/chain33/chain33/authority/bccsp/sw"
 )
 
-const (
-	// SoftwareBasedFactoryName is the name of the factory of the software-based BCCSP implementation
-	SoftwareBasedFactoryName = "SW"
-)
+type FactoryOpts struct {
+	SecLevel   int    `mapstructure:"security" json:"security" yaml:"Security"`
+	HashFamily string `mapstructure:"hash" json:"hash" yaml:"Hash"`
+}
 
 // SWFactory is the factory of the software-based BCCSP.
 type SWFactory struct{}
 
-// Name returns the name of this factory
-func (f *SWFactory) Name() string {
-	return SoftwareBasedFactoryName
-}
-
 // Get returns an instance of BCCSP using Opts.
 func (f *SWFactory) Get(config *FactoryOpts) (bccsp.BCCSP, error) {
 	// Validate arguments
-	if config == nil || config.SwOpts == nil {
+	if config == nil {
 		return nil, errors.New("Invalid config. It must not be nil.")
 	}
 
-	swOpts := config.SwOpts
-
-	return sw.New(swOpts.SecLevel, swOpts.HashFamily)
+	return sw.New(config.SecLevel, config.HashFamily)
 }
 
-// SwOpts contains options for the SWFactory
-type SwOpts struct {
-	// Default algorithms when not specified (Deprecated?)
-	SecLevel   int    `mapstructure:"security" json:"security" yaml:"Security"`
-	HashFamily string `mapstructure:"hash" json:"hash" yaml:"Hash"`
+// GetDefaultOpts offers a default implementation for Opts
+// returns a new instance every time
+func GetDefaultOpts() *FactoryOpts {
+	return &FactoryOpts{
+		HashFamily: "SHA2",
+		SecLevel:   256,
+	}
 }
