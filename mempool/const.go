eb93f5c00eb70dc79201270fe0982af14d4d3ea8
任硕 <rens@disanbo.com>
2018-06-11 18:55:47 +0800

    时间单位秒

--- mempool/const.go
+++ mempool/const.go
@@ -7,14 +7,15 @@ import (
 )
 
 var (
-	mlog                         = log.New("module", "mempool")
-	poolCacheSize          int64 = 10240  // mempool容量
-	channelSize            int64 = 1024   // channel缓存大小
-	mempoolExpiredInterval int64 = 600000 // mempool内交易过期时间，10分钟
-	mempoolReSendInterval  int64 = 60000  // mempool内交易重发时间，1分钟
-	mempoolAddedTxSize           = 102400 // 已添加过的交易缓存大小
-	maxTxNumPerAccount     int64 = 100    // TODO 每个账户在mempool中最大交易数量，10
-	processNum             int
+	mlog                           = log.New("module", "mempool")
+	poolCacheSize            int64 = 10240  // mempool容量
+	channelSize              int64 = 1024   // channel缓存大小
+	mempoolExpiredInterval   int64 = 600000 // mempool内交易过期时间，10分钟
+	mempoolReSendInterval    int64 = 60000  // mempool内交易重发时间，1分钟
+	mempoolDupResendInterval int64 = 120    // mempool重复交易可再次发送间隔，120秒
+	mempoolAddedTxSize             = 102400 // 已添加过的交易缓存大小
+	maxTxNumPerAccount       int64 = 100    // TODO 每个账户在mempool中最大交易数量，10
+	processNum               int
 )
 
 // TODO
