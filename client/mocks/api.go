8e87220caa72d34987e987bc7afb83d21d942a56
gitlab <gitlab@hzfzm.cn>
2018-06-13 16:34:37 +0800

    auto ci

--- client/mocks/api.go
+++ client/mocks/api.go
@@ -9,6 +9,29 @@ type QueueProtocolAPI struct {
 	mock.Mock
 }
 
+// BindMiner provides a mock function with given fields: param
+func (_m *QueueProtocolAPI) BindMiner(param *types.ReqBindMiner) (*types.ReplyBindMiner, error) {
+	ret := _m.Called(param)
+
+	var r0 *types.ReplyBindMiner
+	if rf, ok := ret.Get(0).(func(*types.ReqBindMiner) *types.ReplyBindMiner); ok {
+		r0 = rf(param)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*types.ReplyBindMiner)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(*types.ReqBindMiner) error); ok {
+		r1 = rf(param)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // Close provides a mock function with given fields:
 func (_m *QueueProtocolAPI) Close() {
 	_m.Called()
@@ -30,6 +53,29 @@ func (_m *QueueProtocolAPI) CloseTickets() (*types.ReplyHashes, error) {
 	var r1 error
 	if rf, ok := ret.Get(1).(func() error); ok {
 		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// DecodeRawTransaction provides a mock function with given fields: param
+func (_m *QueueProtocolAPI) DecodeRawTransaction(param *types.ReqDecodeRawTransaction) (*types.Transaction, error) {
+	ret := _m.Called(param)
+
+	var r0 *types.Transaction
+	if rf, ok := ret.Get(0).(func(*types.ReqDecodeRawTransaction) *types.Transaction); ok {
+		r0 = rf(param)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*types.Transaction)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(*types.ReqDecodeRawTransaction) error); ok {
+		r1 = rf(param)
 	} else {
 		r1 = ret.Error(1)
 	}
