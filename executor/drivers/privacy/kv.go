ea24bc6a85380b9d4b246b1b5493c3682400cd7a
hzj <hzj@disanbo.com>
2018-06-15 11:43:25 +0800

    fix bug of utxo's pubkey check for mixing
    Former-commit-id: 85824d014a19b0beb57530a397a44e94c8780057
    Former-commit-id: 2c86af0095dd4ff4f4ae96422b7a276fccfedab4

--- executor/drivers/privacy/kv.go
+++ executor/drivers/privacy/kv.go
@@ -36,12 +36,12 @@ func CalcPrivacyUTXOkeyHeightStr(token string, amount, height int64, txhash stri
 }
 
 func CalcPrivacyUTXOkeyHeightPrefix(token string, amount int64) (key []byte) {
-	return []byte(fmt.Sprintf(PrivacyUTXOKEYPrefix+"-%s-%d", token, amount))
+	return []byte(fmt.Sprintf(PrivacyUTXOKEYPrefix+"-%s-%d-", token, amount))
 }
 
 //设置当前系统存在的token的amount的类型，如存在1,3,5,100...等等的类型,
 func CalcprivacyKeyTokenAmountType(token string) (key []byte) {
-	return []byte(fmt.Sprintf(PrivacyAmountTypePrefix+"-%s", token))
+	return []byte(fmt.Sprintf(PrivacyAmountTypePrefix+"-%s-", token))
 }
 
 func CalcprivacyKeyTokenTypes() (key []byte) {
