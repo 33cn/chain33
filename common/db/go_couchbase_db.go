f31fa25cb9f3284d4f63e437bc872ed30d0aed51
lilinleeli1234 <932323316@qq.com>
2018-06-22 17:01:31 +0800

    update couchbase

--- common/db/go_couchbase_db.go
+++ common/db/go_couchbase_db.go
@@ -5,15 +5,20 @@ import (
 	"encoding/hex"
 
 	log "github.com/inconshreveable/log15"
-	"github.com/syndtr/goleveldb/leveldb"
+	// "github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/errors"
+	// "github.com/syndtr/goleveldb/leveldb/opt"
+	// gcb "github.com/go-couchbase"
+	"fmt"
+
+	"github.com/couchbase/gocb"
 	"github.com/syndtr/goleveldb/leveldb/iterator"
-	"github.com/syndtr/goleveldb/leveldb/opt"
-	"github.com/syndtr/goleveldb/leveldb/util"
-	"gopkg.in/couchbase/gocb.v1"
 )
 
-var clog = log.New("module", "db.gocouchbase")
+var (
+	clog           = log.New("module", "db.gocouchbase")
+	BATCH_MAP_SIZE = 10240
+)
 
 func init() {
 	dbCreator := func(name string, dir string, cache int) (DB, error) {
@@ -25,32 +30,47 @@ func init() {
 
 type GoCouchBase struct {
 	TransactionDB
-	db      *leveldb.DB
-	cluster *gocb.Cluster
-	bucket  *gocb.Bucket
-	items   []gocb.BulkOp
+	// db      *leveldb.DB
+	bucket *gocb.Bucket
+	items  []gocb.BulkOp
+	// batch  map[string][]byte
 }
 
 func NewGoCouchBase(name string, dir string, cache int) (*GoCouchBase, error) {
-	cl, err := gocb.Connect("couchbase://localhost")
+	//cl, err := gcb.Connect("http://Administrator:test123@127.0.0.1:8091")
+	//if err != nil {
+	//	clog.Error("Connect err", "error", err)
+	//}
+	//
+	//if err != nil {
+	//	clog.Error("Auth err", "error", err)
+	//}
+	//pool, err := cl.GetPool("default")
+	//if err != nil {
+	//	clog.Error("Get Pool err", "error", err)
+	//}
+	//
+	//bucket, err := pool.GetBucket("default")
+	//if err != nil {
+	//	clog.Error("Get bucket err", "error", err)
+	//}
+	//
+	//batch := make(map[string][]byte, BATCH_MAP_SIZE)
+	//
+	//database := &GoCouchBase{bucket: bucket, batch: batch}
+
+	cluster, err := gocb.Connect("couchbase://localhost")
 	if err != nil {
 		clog.Error("Connect err", "error", err)
-	}
-	err = cl.Authenticate(gocb.PasswordAuthenticator{
-		Username: "Administrator",
-		Password: "test123",
-	})
-
-	if err != nil {
-		clog.Error("Auth err", "error", err)
-	}
-	bucket, err := cl.OpenBucket("default", "")
-	if err != nil {
-		clog.Error("Open bucket err", "error", err)
+		return nil, err
 	}
 
-	database := &GoCouchBase{cluster: cl, bucket: bucket, items: nil}
-	return database, nil
+	bucket, err := cluster.OpenBucket("default", "")
+	if err != nil {
+		fmt.Println("Get Pool err", "error", err)
+	}
+
+	return &GoCouchBase{bucket: bucket, items: nil}, nil
 }
 
 func (db *GoCouchBase) Get(key []byte) (val []byte, err error) {
@@ -72,7 +92,7 @@ func (db *GoCouchBase) Set(key []byte, value []byte) error {
 }
 
 func (db *GoCouchBase) SetSync(key []byte, value []byte) error {
-	err := db.db.Put(key, value, &opt.WriteOptions{Sync: true})
+	err := db.Set(key, value)
 	if err != nil {
 		clog.Error("SetSync", "error", err)
 		return err
@@ -81,16 +101,17 @@ func (db *GoCouchBase) SetSync(key []byte, value []byte) error {
 }
 
 func (db *GoCouchBase) Delete(key []byte) error {
-	err := db.db.Delete(key, nil)
-	if err != nil {
-		clog.Error("Delete", "error", err)
-		return err
+	for _, op := range db.items {
+		k := op.(*gocb.UpsertOp).Key
+		if k == hex.EncodeToString(key) {
+			op = nil
+		}
 	}
 	return nil
 }
 
 func (db *GoCouchBase) DeleteSync(key []byte) error {
-	err := db.db.Delete(key, &opt.WriteOptions{Sync: true})
+	err := db.Delete(key)
 	if err != nil {
 		clog.Error("DeleteSync", "error", err)
 		return err
@@ -98,120 +119,112 @@ func (db *GoCouchBase) DeleteSync(key []byte) error {
 	return nil
 }
 
-func (db *GoCouchBase) DB() *leveldb.DB {
-	return db.db
-}
-
 func (db *GoCouchBase) Close() {
-	db.db.Close()
+	db.bucket.Close()
 }
 
 func (db *GoCouchBase) Print() {
-	str, _ := db.db.GetProperty("leveldb.stats")
-	clog.Info("Print", "stats", str)
 
-	iter := db.db.NewIterator(nil, nil)
-	for iter.Next() {
-		key := iter.Key()
-		value := iter.Value()
-		//fmt.Printf("[%X]:\t[%X]\n", key, value)
-		clog.Info("Print", "key", string(key), "value", string(value))
-	}
 }
 
 func (db *GoCouchBase) Stats() map[string]string {
-	keys := []string{
-		"leveldb.num-files-at-level{n}",
-		"leveldb.stats",
-		"leveldb.sstables",
-		"leveldb.blockpool",
-		"leveldb.cachedblock",
-		"leveldb.openedtables",
-		"leveldb.alivesnaps",
-		"leveldb.aliveiters",
-	}
-
 	stats := make(map[string]string)
-	for _, key := range keys {
-		str, err := db.db.GetProperty(key)
-		if err == nil {
-			stats[key] = str
-		}
-	}
 	return stats
 }
 
 func (db *GoCouchBase) Iterator(prefix []byte, reserve bool) Iterator {
-	r := &util.Range{prefix, bytesPrefix(prefix)}
-	it := db.db.NewIterator(r, nil)
-	return &GoCouchBaseIt{it, reserve, prefix}
+	//r := &util.Range{prefix, bytesPrefix(prefix)}
+	//var ks []string
+	//ks = append(ks, hex.EncodeToString(r.Start))
+	//ks = append(ks, hex.EncodeToString(r.Limit))
+	//val, _ := db.bucket.(ks)
+	//var keys []string
+	//for k, _ := range val {
+	//	keys = append(keys, k)
+	//}
+	return &GoCouchBaseIt{}
 }
 
 type GoCouchBaseIt struct {
 	iterator.Iterator
-	reserve bool
+	mVal    map[string][]byte
 	prefix  []byte
+	reserve bool
+	keys    []string
+	index   int64
 }
 
 func (dbit *GoCouchBaseIt) Close() {
-	dbit.Iterator.Release()
+	dbit.mVal = nil
+	dbit.prefix = nil
+	dbit.reserve = false
+	dbit.index = 0
 }
 
 func (dbit *GoCouchBaseIt) Next() bool {
 	if dbit.reserve {
-		return dbit.Iterator.Prev() && dbit.Valid()
+		dbit.index--
+	} else {
+		dbit.index++
 	}
-	return dbit.Iterator.Next() && dbit.Valid()
+	if dbit.mVal[dbit.keys[dbit.index]] != nil {
+		return true
+	} else {
+		return false
+	}
 }
 
 func (dbit *GoCouchBaseIt) Rewind() bool {
 	if dbit.reserve {
-		return dbit.Iterator.Last() && dbit.Valid()
+		dbit.index = int64(len(dbit.keys))
+	} else {
+		dbit.index = 0
 	}
-	return dbit.Iterator.First() && dbit.Valid()
+
+	return dbit.Valid()
 }
 
 func (dbit *GoCouchBaseIt) Value() []byte {
-	return dbit.Iterator.Value()
+	return dbit.mVal[dbit.keys[dbit.index]]
 }
 
 func (dbit *GoCouchBaseIt) ValueCopy() []byte {
-	v := dbit.Iterator.Value()
+	v := dbit.mVal[dbit.keys[dbit.index]]
 	value := make([]byte, len(v))
 	copy(value, v)
 	return value
 }
 
 func (dbit *GoCouchBaseIt) Valid() bool {
-	return dbit.Iterator.Valid() && bytes.HasPrefix(dbit.Key(), dbit.prefix)
+	val, _ := hex.DecodeString(dbit.keys[dbit.index])
+	return dbit.mVal[dbit.keys[dbit.index]] != nil && bytes.Contains(val, dbit.prefix)
 }
 
 type GoCouchBaseBatch struct {
-	bucket *gocb.Bucket
-	items  []gocb.BulkOp
+	bucket   *gocb.Bucket
+	batchMap []gocb.BulkOp
 }
 
 func (db *GoCouchBase) NewBatch(sync bool) Batch {
 	bucket := db.bucket
-	items := db.items
-	return &GoCouchBaseBatch{bucket, items}
+	batch := db.items
+	return &GoCouchBaseBatch{bucket, batch}
 }
 
 func (mBatch *GoCouchBaseBatch) Set(key, value []byte) {
-	mBatch.items = append(mBatch.items, &gocb.UpsertOp{Key: hex.EncodeToString(key), Value: value})
+	mBatch.batchMap = append(mBatch.batchMap, &gocb.UpsertOp{Key: hex.EncodeToString(key), Value: value})
 }
 
 func (mBatch *GoCouchBaseBatch) Delete(key []byte) {
-	for i := 0; i < len(mBatch.items); i++ {
-		k := mBatch.items[i].(*gocb.UpsertOp).Key
+	for _, op := range mBatch.batchMap {
+		k := op.(*gocb.UpsertOp).Key
 		if k == hex.EncodeToString(key) {
-			mBatch.items[i].(*gocb.UpsertOp).Key = ""
-			mBatch.items[i].(*gocb.UpsertOp).Value = nil
+			op = nil
 		}
 	}
 }
 
 func (mBatch *GoCouchBaseBatch) Write() error {
-	err := mBatch.bucket.Do(mBatch.items)
-	return err
+	mBatch.bucket.Do(mBatch.batchMap)
+	return nil
 }
