ff557150e03bd8c5d7cdcb1b833e1b1442765597
林敬 <linj@disanbo.com>
2018-06-22 14:22:09 +0800

    none action name
    Former-commit-id: 47d83730d03e22e9ff30d83004fe0534068d5b42
    Former-commit-id: 3051e08dc6d7983ae27655e8252f0a5cf23b6d94

---
+++ types/executor/none/none.go
@@ -0,0 +1,76 @@
+package none
+
+
+
+import (
+	"gitlab.33.cn/chain33/chain33/types"
+	"encoding/json"
+	"time"
+	log "github.com/inconshreveable/log15"
+	"math/rand"
+)
+
+const name = "none"
+
+var tlog = log.New("module", name)
+
+func init() {
+	// init executor type
+	types.RegistorExecutor(name, &NoneType{})
+
+	// init log
+	types.RegistorLog(types.TyLogDeposit, &CoinsDepositLog{})
+
+	// init query rpc
+	types.RegistorRpcType("q2", &CoinsGetTxsByAddr{})
+}
+
+
+
+type NoneType struct {
+}
+
+func (none NoneType) ActionName(tx *types.Transaction) string {
+	return "none"
+}
+
+// TODO 暂时不修改实现， 先完成结构的重构
+func (none NoneType) NewTx(action string, message json.RawMessage) (*types.Transaction, error) {
+	var tx *types.Transaction
+	return tx, nil
+}
+
+type CoinsDepositLog struct {
+}
+
+func (l CoinsDepositLog) Name() string {
+	return "LogDeposit"
+}
+
+func (l CoinsDepositLog) Decode(msg []byte) (interface{}, error){
+	var logTmp types.ReceiptAccountTransfer
+	err := types.Decode(msg, &logTmp)
+	if err != nil {
+		return nil, err
+	}
+	return logTmp, err
+}
+
+type CoinsGetTxsByAddr struct {
+}
+
+func (t *CoinsGetTxsByAddr) Input(message json.RawMessage) ([]byte, error) {
+	var req types.ReqAddr
+	err := json.Unmarshal(message, &req)
+	if err != nil {
+		return nil, err
+	}
+	return types.Encode(&req), nil
+}
+
+func (t *CoinsGetTxsByAddr) Output(reply interface{}) (interface{}, error) {
+	return reply, nil
+}
+
+
+
