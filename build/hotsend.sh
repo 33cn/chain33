feb435029830eeea338ec20139a8048831b8a0c5
king wang <kingwang@kingdeMacBook-Pro.local>
2018-06-22 22:16:04 +0800

    del game
    Former-commit-id: fd0dc20292c4a888a8445df584655e3c5badca5d
    Former-commit-id: 1ac118c5f9ded41a81844846674e016c06554477

--- build/hotsend.sh
+++ build/hotsend.sh
@@ -1,4 +1,4 @@
-withdraw="`./chain33-cli bty withdraw -n "withdraw" -a 8000000 -e "ticket"`"
-send="`./chain33-cli bty transfer -n "" -a 8000000 -t 1FCX9XJTZXvZteagTrefJEBPZMt8BFmdoi`"
-echo "cli wallet sign -d $withdraw -a 1FB8L3DykVF7Y78bRfUrRcMZwesKue7CyR -e 1h > d:/a.txt"
-echo "cli wallet sign -d $send -a 1FB8L3DykVF7Y78bRfUrRcMZwesKue7CyR -e 1h >> d:/a.txt"
+withdraw="`./chain33-cli bty withdraw -n "withdraw" -a 1400000 -e "ticket"`"
+send="`./chain33-cli bty transfer -n "" -a 1400000 -t 1FCX9XJTZXvZteagTrefJEBPZMt8BFmdoi`"
+echo "cli wallet sign -d $withdraw -a 1KNGHukhbBnbWWnMYxu1C7YMoCj45Z3amm -e 1h > d:/a.txt"
+echo "cli wallet sign -d $send -a 1KNGHukhbBnbWWnMYxu1C7YMoCj45Z3amm -e 1h >> d:/a.txt"
