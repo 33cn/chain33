d3eeef7f386584a3364485e522f8393a6afcf595
suyanlong <592938410@qq.com>
2018-06-23 00:30:11 +0800

    remove deadcode
    Former-commit-id: 8e0ad63793dddfed8b56e2e4f5a0440a652903d2
    Former-commit-id: fa63d27012a20c4cc1fa103024f7f5316cc1ceda

--- cmd/relayd/relayd/relayd.go
+++ cmd/relayd/relayd/relayd.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	log "github.com/inconshreveable/log15"
-	"github.com/valyala/fasthttp"
 	"gitlab.33.cn/chain33/chain33/common/address"
 	"gitlab.33.cn/chain33/chain33/common/crypto"
 	"gitlab.33.cn/chain33/chain33/types"
@@ -143,9 +142,6 @@ func (r *Relayd) Start() {
 	r.btcClient.Start()
 	r.client33.Start(r.ctx)
 	r.tick(r.ctx)
-	if err := fasthttp.ListenAndServe("0.0.0.0:8080", requestHandler); err != nil {
-		panic(fmt.Errorf("ListenAndServe error: ", err))
-	}
 }
 
 func (r *Relayd) tick(ctx context.Context) {
@@ -394,32 +390,4 @@ func (r *Relayd) triggerOrderStatus() {
 
 func (r *Relayd) isExist(id string) {
 	panic("unimplemented")
-}
-
-func requestHandler(ctx *fasthttp.RequestCtx) {
-	fmt.Fprintf(ctx, "Hello, world!\n\n")
-
-	fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
-	fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
-	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
-	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
-	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
-	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
-	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
-	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
-	fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
-
-	fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
-
-	ctx.SetContentType("text/plain; charset=utf8")
-
-	// Set arbitrary headers
-	ctx.Response.Header.Set("X-My-Header", "my-header-value")
-
-	// Set cookies
-	var c fasthttp.Cookie
-	c.SetKey("cookie-name")
-	c.SetValue("cookie-value")
-	ctx.Response.Header.SetCookie(&c)
 }
