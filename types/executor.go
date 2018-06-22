a1b1568cacd36424ce4db3e2d4ce5fcb51acce5d
林敬 <linj@disanbo.com>
2018-06-22 09:06:07 +0800

    split file: executor type part to new file executor.go

---
+++ types/executor.go
@@ -0,0 +1,51 @@
+package types
+
+import "encoding/json"
+
+
+type ExecutorType interface {
+	// name 是 executor name 构造时用
+	//func Name()
+	ActionName(transaction *Transaction) string
+	NewTx(action string, message json.RawMessage) (*Transaction, error)
+}
+
+type LogType interface {
+	Name()string
+	Decode([]byte) (interface{}, error)
+}
+
+var executorMap = map[string]ExecutorType{}
+var receiptLogMap = map[int64]LogType{}
+
+func RegistorExecutor(funcName string, util ExecutorType) {
+	//tlog.Debug("rpc", "t", funcName, "t", util)
+	if _, exist := executorMap[funcName]; exist {
+		panic("DupExecutorType")
+	} else {
+		executorMap[funcName] = util
+	}
+}
+
+func LoadExecutor(exec string) ExecutorType {
+	if exec, exist := executorMap[exec]; exist {
+		return exec
+	}
+	return nil
+}
+
+func RegistorLog(funcName int64, util LogType) {
+	//tlog.Debug("rpc", "t", funcName, "t", util)
+	if _, exist := receiptLogMap[funcName]; exist {
+		panic("DupLogType")
+	} else {
+		receiptLogMap[funcName] = util
+	}
+}
+
+func LoadLog(ty int64) LogType {
+	if log, exist := receiptLogMap[ty]; exist {
+		return log
+	}
+	return nil
+}
