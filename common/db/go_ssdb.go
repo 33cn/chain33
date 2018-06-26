4e6195db1e29ae8a127ffe9ccb360f28468da77f
litian33 <lit@disanbo.com>
2018-06-26 17:48:56 +0800

    SSDB代码测试基本通过

--- common/db/go_ssdb.go
+++ common/db/go_ssdb.go
@@ -5,11 +5,7 @@ import (
 	log "github.com/inconshreveable/log15"
 	"github.com/syndtr/goleveldb/leveldb/util"
 
-	"encoding/hex"
 	"fmt"
-	"github.com/pkg/errors"
-	"github.com/seefan/gossdb"
-	"github.com/seefan/gossdb/ssdb"
 	"gitlab.33.cn/chain33/chain33/types"
 	"strconv"
 	"strings"
@@ -17,7 +13,6 @@ import (
 )
 
 var dlog = log.New("module", "db.ssdb")
-var SsdbPoolError = errors.New("get client from pool error")
 var benchmark = &SsdbBench{}
 
 func init() {
@@ -46,8 +41,7 @@ type SsdbNode struct {
 }
 type GoSSDB struct {
 	TransactionDB
-	//pool  *gossdb.Connectors
-	pool  *gossdb.Client
+	pool  *SDBPool
 	nodes []*SsdbNode
 }
 
@@ -69,11 +63,11 @@ func (bench *SsdbBench) String() string {
 }
 
 func printSsdbBenchmark() {
+	tick := time.Tick(time.Minute)
 	for {
-		time.Sleep(time.Minute)
+		<-tick
 		dlog.Info(benchmark.String())
 	}
-
 }
 
 // url pattern: ip:port,ip:port
@@ -101,90 +95,45 @@ func parseSsdbNode(url string) (nodes []*SsdbNode) {
 }
 
 func NewGoSSDB(name string, dir string, cache int) (*GoSSDB, error) {
+
 	database := &GoSSDB{}
 	database.nodes = parseSsdbNode(dir)
+
 	if database.nodes == nil {
 		dlog.Error("no valid ssdb instance exists, exit!")
 		return nil, types.ErrDataBaseDamage
 	}
-
 	var err error
-	//node := database.nodes[0]
-	//pool, err := gossdb.NewPool(&conf.Config{
-	//	Host:         node.ip,
-	//	Port:         node.port,
-	//	RetryEnabled: true,
-	//})
-	//if err != nil {
-	//	dlog.Error("create ssdb pool error, exit!", "error", err)
-	//	return nil, types.ErrDataBaseDamage
-	//}
-	if err = ssdb.Start(); err != nil {
-		dlog.Error("start ssdb error, exit!", "error", err)
-		return nil, types.ErrDataBaseDamage
-	}
-
-	database.pool, err = ssdb.Client()
+	database.pool, err = NewSDBPool(database.nodes)
 	if err != nil {
-		dlog.Error("get ssdb client error, exit!", "error", err)
+		dlog.Error("connect to ssdb error!", "ssdb", database.nodes[0])
 		return nil, types.ErrDataBaseDamage
 	}
 	return database, nil
 }
 
-func (db *GoSSDB) newClient() *gossdb.Client {
-	//client, err := db.pool.NewClient()
-	//if err != nil {
-	//	dlog.Error("can not get connection from pool", "error", err)
-	//	return nil
-	//}
-	//return client
-
-	return db.pool
-}
-
 func (db *GoSSDB) Get(key []byte) ([]byte, error) {
 	start := time.Now()
-	client := db.newClient()
-	if client == nil {
-		return nil, SsdbPoolError
-	}
-	defer client.Close()
 
-	value, err := client.Get(string(key))
+	value, err := db.pool.get().Get(string(key))
 	if err != nil {
-		dlog.Error("Get value error", "error", err, "key", key, "keyhex", hex.EncodeToString(key), "keystr", string(key))
+		//dlog.Error("Get value error", "error", err, "key", key, "keyhex", hex.EncodeToString(key), "keystr", string(key))
 		return nil, err
 	}
-	if value == "" {
+	if value == nil {
 		return nil, ErrNotFoundInDb
 	}
 
-	// 空值特殊处理
-	binVal := value.Bytes()
-	if bytes.Equal(binVal, types.EmptyValue) {
-		return []byte{}, nil
-	}
-
 	benchmark.read(1, time.Since(start))
-	return binVal, nil
+	return value.Bytes(), nil
 }
 
 func (db *GoSSDB) Set(key []byte, value []byte) error {
 	start := time.Now()
-	if len(value) == 0 {
-		value = types.EmptyValue
-	}
 
-	client := db.newClient()
-	if client == nil {
-		return SsdbPoolError
-	}
-	defer client.Close()
-
-	err := client.Set(string(key), value)
+	err := db.pool.get().Set(string(key), value)
 	if err != nil {
-		llog.Error("Set", "error", err)
+		dlog.Error("Set", "error", err)
 		return err
 	}
 	benchmark.write(1, time.Since(start))
@@ -197,14 +146,10 @@ func (db *GoSSDB) SetSync(key []byte, value []byte) error {
 
 func (db *GoSSDB) Delete(key []byte) error {
 	start := time.Now()
-	client := db.newClient()
-	if client == nil {
-		return SsdbPoolError
-	}
-	defer client.Close()
-	err := client.Del(string(key))
+
+	err := db.pool.get().Del(string(key))
 	if err != nil {
-		llog.Error("Delete", "error", err)
+		dlog.Error("Delete", "error", err)
 		return err
 	}
 	benchmark.write(1, time.Since(start))
@@ -216,7 +161,7 @@ func (db *GoSSDB) DeleteSync(key []byte) error {
 }
 
 func (db *GoSSDB) Close() {
-	db.pool.Close()
+	db.pool.close()
 }
 
 func (db *GoSSDB) Print() {
@@ -229,49 +174,118 @@ func (db *GoSSDB) Stats() map[string]string {
 
 func (db *GoSSDB) Iterator(prefix []byte, reverse bool) Iterator {
 	start := time.Now()
-	client := db.newClient()
-	if client == nil {
-		return newSSDBIt(prefix, []string{}, reverse, db)
-	}
-	defer client.Close()
 
 	var (
-		keys []string
-		err  error
+		keys  []string
+		err   error
+		begin string
+		end   string
 	)
 
 	limit := util.BytesPrefix(prefix)
 	if reverse {
-		keys, err = client.Rkeys(string(limit.Limit), string(prefix), -1)
+		begin = string(limit.Limit)
+		end = string(prefix)
+		keys, err = db.pool.get().Rkeys(begin, end, IteratorPageSize)
 	} else {
-		keys, err = client.Rkeys(string(prefix), string(limit.Limit), -1)
+		begin = string(prefix)
+		end = string(limit.Limit)
+		keys, err = db.pool.get().Keys(begin, end, IteratorPageSize)
 	}
 
-	it := newSSDBIt(prefix, []string{}, reverse, db)
-	if err != nil || len(keys) == 0 {
+	it := newSSDBIt(begin, end, prefix, []string{}, reverse, db)
+	if err != nil {
 		dlog.Error("get iterator error", "error", err, "keys", keys)
 		return it
 	}
-	it.keys = keys
+	if len(keys) > 0 {
+		it.keys = keys
+
+		// 如果返回的数据大小刚好满足分页，则假设下一页还有数据
+		if len(it.keys) == IteratorPageSize {
+			it.nextPage = true
+			it.tmpEnd = it.keys[IteratorPageSize-1]
+		}
+	}
 
 	benchmark.read(len(keys), time.Since(start))
 	return it
 }
 
+// 因为ssdb不支持原生的迭代器模式，需要一次获取KEY；
+// 为了防止匹配的KEY范围过大，这里需要进行分页，每次只取1024条KEY；
+// Next方法自动进行跨页取数据
 type ssDBIt struct {
 	db      *GoSSDB
 	keys    []string
 	index   int
 	reverse bool
-	prefix  []byte
+	// 所有KEY的前缀
+	prefix []byte
+	// 迭代开始位置
+	begin string
+	// 迭代结束位置
+	end string
+
+	// 需要分页的情况下，下一页的开始位置
+	//  是否有下一页数据
+	tmpEnd   string
+	nextPage bool
+
+	// 当前所属的页数（从0开始）
+	pageNo int
 }
 
-func newSSDBIt(prefix []byte, keys []string, reverse bool, db *GoSSDB) *ssDBIt {
-	return &ssDBIt{index: -1, keys: keys, reverse: reverse, prefix: prefix, db: db}
+func newSSDBIt(begin, end string, prefix []byte, keys []string, reverse bool, db *GoSSDB) *ssDBIt {
+	return &ssDBIt{index: -1, keys: keys, reverse: reverse, db: db, begin: begin, end: end, prefix: prefix}
 }
 
 func (dbit *ssDBIt) Close() {
 	dbit.keys = []string{}
+}
+
+// 获取下一页的数据
+func (dbit *ssDBIt) cacheNextPage(begin string) bool {
+	if dbit.initKeys(begin, dbit.end) {
+		dbit.index = 0
+		dbit.pageNo++
+		return true
+	} else {
+		return false
+	}
+}
+
+func (dbit *ssDBIt) initKeys(begin, end string) bool {
+	var (
+		keys []string
+		err  error
+	)
+	if dbit.reverse {
+		keys, err = dbit.db.pool.get().Rkeys(begin, end, IteratorPageSize)
+	} else {
+		keys, err = dbit.db.pool.get().Keys(begin, end, IteratorPageSize)
+	}
+	if err != nil {
+		dlog.Error("get iterator next page error", "error", err, "begin", begin, "end", dbit.end, "reverse", dbit.reverse)
+		return false
+	}
+
+	if len(keys) > 0 {
+		// 这里只改变keys，不改变index
+		dbit.keys = keys
+
+		// 如果返回的数据大小刚好满足分页，则假设下一页还有数据
+		if len(keys) == IteratorPageSize {
+			dbit.nextPage = true
+			dbit.tmpEnd = dbit.keys[IteratorPageSize-1]
+		} else {
+			dbit.nextPage = false
+		}
+		return true
+	} else {
+		return false
+	}
+
 }
 
 func (dbit *ssDBIt) Next() bool {
@@ -279,38 +293,64 @@ func (dbit *ssDBIt) Next() bool {
 		dbit.index++
 		return true
 	} else {
+		// 如果有下一页数据，则自动抓取
+		if dbit.nextPage {
+			return dbit.cacheNextPage(dbit.tmpEnd)
+		}
 		return false
 	}
 }
 
-func (dbit *ssDBIt) Seek(key []byte) bool {
-	keyStr := string(key)
-	pos := 0
+func (dbit *ssDBIt) findInPage(key string) int {
+	pos := -1
 	for i, v := range dbit.keys {
 		if i < dbit.index {
 			continue
 		}
-		if strings.Compare(keyStr, v) < 0 {
+		if strings.Compare(key, v) < 0 {
 			continue
 		} else {
 			pos = i
 			break
 		}
 	}
+	return pos
+}
 
-	tmp := dbit.index
-	dbit.index = pos
-	if dbit.Valid() {
-		return true
-	} else {
-		dbit.index = tmp
-		return false
+func (dbit *ssDBIt) Seek(key []byte) bool {
+	keyStr := string(key)
+	pos := -1
+	pos = dbit.findInPage(keyStr)
+
+	// 如果第一页已经找到，不会走入此逻辑
+	for pos == -1 && dbit.nextPage {
+		if dbit.cacheNextPage(dbit.tmpEnd) {
+			pos = dbit.findInPage(keyStr)
+		} else {
+			break
+		}
 	}
+
+	dbit.index = pos
+	return dbit.Valid()
 }
 
 func (dbit *ssDBIt) Rewind() bool {
-	dbit.index = 0
-	return true
+	// 目前代码的Rewind调用都是在第一页，正常情况下走不到else分支；
+	// 但为了代码健壮性考虑，这里增加对else分支的处理
+	if dbit.pageNo == 0 {
+		dbit.index = 0
+		return true
+	}
+
+	// 当数据取到第N页的情况时，Rewind需要返回到第一页第一条
+	if dbit.initKeys(dbit.begin, dbit.end) {
+		dbit.index = 0
+		dbit.pageNo = 0
+		return true
+	} else {
+		return false
+	}
 }
 
 func (dbit *ssDBIt) Key() []byte {
@@ -325,7 +365,7 @@ func (dbit *ssDBIt) Value() []byte {
 	value, err := dbit.db.Get([]byte(key))
 
 	if err != nil {
-		dlog.Error("get iterator value error", "key", key)
+		dlog.Error("get iterator value error", "key", key, "error", err)
 		return nil
 	}
 	return value
@@ -344,6 +384,9 @@ func (dbit *ssDBIt) ValueCopy() []byte {
 
 func (dbit *ssDBIt) Valid() bool {
 	start := time.Now()
+	if dbit.index < 0 {
+		return false
+	}
 	if len(dbit.keys) <= dbit.index {
 		return false
 	}
@@ -354,18 +397,15 @@ func (dbit *ssDBIt) Valid() bool {
 
 type ssDBBatch struct {
 	db       *GoSSDB
-	batchset map[string]interface{}
+	batchset map[string][]byte
 	batchdel []string
 }
 
 func (db *GoSSDB) NewBatch(sync bool) Batch {
-	return &ssDBBatch{db: db, batchset: make(map[string]interface{})}
+	return &ssDBBatch{db: db, batchset: make(map[string][]byte)}
 }
 
 func (db *ssDBBatch) Set(key, value []byte) {
-	if len(value) == 0 {
-		value = types.EmptyValue
-	}
 	db.batchset[string(key)] = value
 }
 
@@ -380,14 +420,9 @@ func (db *ssDBBatch) Delete(key []byte) {
 // 这样即使中间执行出错，也不会导致删除结果未写入的情况（值已经被置空）；
 func (db *ssDBBatch) Write() error {
 	start := time.Now()
-	client := db.db.newClient()
-	if client == nil {
-		return SsdbPoolError
-	}
-	defer client.Close()
 
 	if len(db.batchset) > 0 {
-		err := client.MultiSet(db.batchset)
+		err := db.db.pool.get().MultiSet(db.batchset)
 		if err != nil {
 			dlog.Error("Write (multi_set)", "error", err)
 			return err
@@ -395,13 +430,13 @@ func (db *ssDBBatch) Write() error {
 	}
 
 	if len(db.batchdel) > 0 {
-		err := client.MultiDel(db.batchdel...)
+		err := db.db.pool.get().MultiDel(db.batchdel...)
 		if err != nil {
 			dlog.Error("Write (multi_del)", "error", err)
 			return err
 		}
 	}
 
-	benchmark.read(len(db.batchset)+len(db.batchdel), time.Since(start))
+	benchmark.write(len(db.batchset)+len(db.batchdel), time.Since(start))
 	return nil
 }
