951f7ca8919fe6e006bdc0f5f575ddc57aea3a1f
pengjun <pengj@disanbo.com>
2018-06-27 14:44:52 +0800

    authority-management:删除无效代码，证书校验测试通过

--- authority/utils/io.go
+++ authority/utils/io.go
@@ -1,23 +1,3 @@
-/*
-Copyright IBM Corp. 2017 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-/*
-Notice: This file has been modified for Hyperledger Fabric SDK Go usage.
-Please review third_party pinning scripts and patches for more details.
-*/
-
 package utils
 
 import (
@@ -28,7 +8,6 @@ import (
 	"io/ioutil"
 )
 
-// DirMissingOrEmpty checks is a directory is missing or empty
 func DirMissingOrEmpty(path string) (bool, error) {
 	dirExists, err := DirExists(path)
 	if err != nil {
@@ -48,7 +27,6 @@ func DirMissingOrEmpty(path string) (bool, error) {
 	return false, nil
 }
 
-// DirExists checks if a directory exists
 func DirExists(path string) (bool, error) {
 	_, err := os.Stat(path)
 	if err == nil {
@@ -60,7 +38,6 @@ func DirExists(path string) (bool, error) {
 	return false, err
 }
 
-// DirEmpty checks if a directory is empty
 func DirEmpty(path string) (bool, error) {
 	f, err := os.Open(path)
 	if err != nil {
@@ -91,7 +68,7 @@ func ReadPemFile(file string) ([]byte, error) {
 	}
 
 	b, _ := pem.Decode(bytes)
-	if b == nil { // TODO: also check that the type is what we expect (cert vs key..)
+	if b == nil {
 		return nil, fmt.Errorf("No pem content for file %s", file)
 	}
 
