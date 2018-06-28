69e202e2bdddd6a18a397df3de22a5c559c66b4e
王志文 <wzw@disanbo.com>
2018-06-28 10:27:37 +0800

    fixbug loop back address and ipv6 addr

---
+++ types/time.go
@@ -0,0 +1,28 @@
+package types
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+var deltaTime int64
+var timeCalibration bool
+
+func SetTimeCalibration(openTimeCalibration bool) {
+	timeCalibration = openTimeCalibration
+}
+
+//realtime - localtime
+//超过60s 不做修正
+//为了系统的安全，我们只做小范围时间错误的修复
+func SetTimeDelta(dt int64) {
+	if dt > 60*int64(time.Second) || dt < -60*int64(time.Second) {
+		dt = 0
+	}
+	atomic.StoreInt64(&deltaTime, dt)
+}
+
+func GetRealTime() time.Time {
+	dt := time.Duration(atomic.LoadInt64(&deltaTime))
+	return time.Now().Add(dt)
+}
