d3ffa514c7be99b0e8c98099bacc63233e6f819d
Shuo Ren <rens@disanbo.com>
2018-07-03 17:50:52 +0800

    delete rollback tx from addedtxs
    Former-commit-id: 7dfc105d90a8ffbe6e8bccb72dbbf8d793a676c1
    Former-commit-id: e9ae59a54b0f0c5d509b54e88d3a492c22270463

--- mempool/const.go
+++ mempool/const.go
@@ -7,11 +7,11 @@ import (
 )
 
 var (
-	mlog                         = log.New("module", "mempool")
-	poolCacheSize          int64 = 10240 // mempool容量
-	channelSize            int64 = 1024  // channel缓存大小
-	mempoolExpiredInterval int64 = 600   // mempool内交易过期时间，10分钟
-	// mempoolReSendInterval    int64 = 60     // mempool内交易重发时间，1分钟
+	mlog                           = log.New("module", "mempool")
+	poolCacheSize            int64 = 10240  // mempool容量
+	channelSize              int64 = 1024   // channel缓存大小
+	mempoolExpiredInterval   int64 = 600    // mempool内交易过期时间，10分钟
+	mempoolReSendInterval    int64 = 60     // mempool内交易重发时间，1分钟
 	mempoolDupResendInterval int64 = 120    // mempool重复交易可再次发送间隔，120秒
 	mempoolAddedTxSize             = 102400 // 已添加过的交易缓存大小
 	maxTxNumPerAccount       int64 = 100    // TODO 每个账户在mempool中最大交易数量，10
