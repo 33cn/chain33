fc11df094beda4bf4cd891a7b3fa2084ad6ba727
Shuo Ren <rens@disanbo.com>
2018-07-03 11:45:21 +0800

    remove retrysend
    Former-commit-id: 69661c5404945e3bca5fe1d2eadd4c53709c029a

--- mempool/mempool.go
+++ mempool/mempool.go
@@ -249,30 +249,30 @@ func (mem *Mempool) GetLatestTx() []*types.Transaction {
 }
 
 // Mempool.ReTrySend每隔两分钟运行一次ReTry
-func (mem *Mempool) ReTrySend() {
-	for {
-		time.Sleep(time.Minute * 2)
-		mem.ReTry()
-	}
-}
+//func (mem *Mempool) ReTrySend() {
+//	for {
+//		time.Sleep(time.Minute * 2)
+//		mem.ReTry()
+//	}
+//}
 
 // Mempool.ReTry检查Mempool，将未过期的交易重发送给P2P
-func (mem *Mempool) ReTry() {
-	var result []*types.Transaction
-	mem.proxyMtx.Lock()
-	for _, v := range mem.cache.txMap {
-		if types.Now().Unix()-v.Value.(*Item).enterTime >= mempoolReSendInterval {
-			result = append(result, v.Value.(*Item).value)
-		}
-	}
-	mem.proxyMtx.Unlock()
-	if len(result) > 0 {
-		mlog.Debug("retry send tx...")
-	}
-	for _, tx := range result {
-		mem.SendTxToP2P(tx)
-	}
-}
+//func (mem *Mempool) ReTry() {
+//	var result []*types.Transaction
+//	mem.proxyMtx.Lock()
+//	for _, v := range mem.cache.txMap {
+//		if types.Now().Unix()-v.Value.(*Item).enterTime >= mempoolReSendInterval {
+//			result = append(result, v.Value.(*Item).value)
+//		}
+//	}
+//	mem.proxyMtx.Unlock()
+//	if len(result) > 0 {
+//		mlog.Debug("retry send tx...")
+//	}
+//	for _, tx := range result {
+//		mem.SendTxToP2P(tx)
+//	}
+//}
 
 // Mempool.RemoveBlockedTxs每隔1分钟清理一次已打包的交易
 func (mem *Mempool) RemoveBlockedTxs() {
@@ -317,6 +317,7 @@ func (mem *Mempool) RemoveBlockedTxs() {
 		for _, t := range dupTxs {
 			txValue, exists := mem.cache.txMap[string(t)]
 			if exists {
+				mem.addedTxs.Add(string(t), nil)
 				mem.cache.Remove(txValue.Value.(*Item).value.Hash())
 			}
 		}
@@ -487,7 +488,7 @@ func (mem *Mempool) SetQueueClient(client queue.Client) {
 
 	go mem.pollLastHeader()
 	go mem.getSync()
-	go mem.ReTrySend()
+	//	go mem.ReTrySend()
 	// 从badChan读取坏消息，并回复错误信息
 	go func() {
 		for m := range mem.badChan {
