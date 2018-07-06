0b99e7b31388e7f45973d6011b1b2076c923994c
king wang <kingwang@kingdeMacBook-Pro.local>
2018-07-06 20:18:49 +0800

    wait listen is ok

--- rpc/server_test.go
+++ rpc/server_test.go
@@ -46,16 +46,19 @@ func TestJSONClient_Call(t *testing.T) {
 	testChain33 := newTestChain33(api)
 	assert.NotNil(t, testChain33)
 	server.jrpc = *testChain33
-	go server.Listen()
-
+	done := make(chan struct{}, 1)
+	go func () {
+		done <- struct{}{}
+		server.Listen()
+	()
+	<-done
+	time.Sleep(time.Millisecond)
 	ret := &types.Reply{
 		IsOk: true,
 		Msg:  []byte("123"),
 	}
 	api.On("IsSync").Return(ret, nil)
 	api.On("Close").Return()
-
-	time.Sleep(100)
 	jsonClient, err := NewJSONClient("http://" + rpcCfg.JrpcBindAddr + "/root")
 	assert.Nil(t, err)
 	assert.NotNil(t, jsonClient)
