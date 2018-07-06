aaccec38b63884fd891df2bbcddf15a9d2b0cf9a
liuyuhang <liuyuhang203@sina.com>
2018-07-06 19:24:17 +0800

    Modify a bug
    Former-commit-id: 58ac2e28db846fe58d5d1f58551256531b2c385a

--- build/coins-fork-test.sh
+++ build/coins-fork-test.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-coinsTxFee1=0
-coinsTxFee2=0
 coinsTxHashs1=("")
-coinsTxHashs2=("")
+#coinsTxHashs2=("")
 coinsgStr=""
 coinsRepeatTx=1 #重复发送交易次数
-coinsTotalAmount1="100.0000"
-coinsTotalAmount2="100.0000"
-defaultTxFee="0.001"
+coinsTotalAmount1="0"
+coinsTxAmount="5" #每次发送交易数
+#defaultTxFee="0.001"
 coinsAddr1=""
-#coinsAddr2=""
 coinsTxSign=("")
 
 function initCoinsAccount() {
@@ -21,38 +18,13 @@ function initCoinsAccount() {
     coinsAddr1=$coinsgStr
 
     sleep 1
-    From="12qyocayNF7Lv6C9qW4avxs2E7U41fKSfv"
-    to="14KEKbYtKKQm4wMthSK9J4La4nAiidGozt"
-    note="coinstx"
-    amount=$coinsTotalAmount1
-    sendcoinsTx "${name}" $From $to $note $amount
 
-    sleep 1
-    name="${CLI4}"
-    label="coinstest2"
-    createAccount "${name}" $label
-    #coinsAddr2=$coinsgStr
-
-    sleep 1
-    From="1EDnnePAZN48aC2hiTDzhkczfF39g1pZZX"
-    to="1KcCVZLSQYRUwE5EXTsAoQs9LuJW6xwfQa"
-    note="coinstx"
-    amount=$coinsTotalAmount2
-    sendcoinsTx "${name}" $From $to $note $amount
-
-    #########查询新增账户余额#########
-    sleep 60
     name="${CLI}"
-    fromAdd="14KEKbYtKKQm4wMthSK9J4La4nAiidGozt"
+    fromAdd="12qyocayNF7Lv6C9qW4avxs2E7U41fKSfv"
     showCoinsBalance "${name}" $fromAdd
     coinsTotalAmount1=$coinsgStr
 
-    sleep 1
-    name="${CLI4}"
-    fromAdd="1KcCVZLSQYRUwE5EXTsAoQs9LuJW6xwfQa"
-    showCoinsBalance "${name}" $fromAdd
-    coinsTotalAmount2=$coinsgStr
-
+    shouAccountList "${name}"
 }
 
 function genFirstChainCoinstx() {
@@ -61,10 +33,11 @@ function genFirstChainCoinstx() {
     echo "当前链为：${name}"
 
     for ((i = 0; i < coinsRepeatTx; i++)); do
-        From="14KEKbYtKKQm4wMthSK9J4La4nAiidGozt"
+        From="12qyocayNF7Lv6C9qW4avxs2E7U41fKSfv"
         to=$coinsAddr1
         note="coinstx"
-        amount=1
+        amount=$coinsTxAmount
+
         genCoinsTx "${name}" $From $to $note $amount
         coinsTxSign[$i]="${coinsgStr}"
 
@@ -82,15 +55,14 @@ function genFirstChainCoinstx() {
 
 function genSecondChainCoinstx() {
     echo "====== 发送交易 ======"
-    name=$CLI4
+    name=$CLI
     echo "当前链为：${name}"
+
     #将第一条链产生的签名交易发送出去
     for ((i = 0; i < ${#coinsTxSign[*]}; i++)); do
         #发送交易
         sign="${coinsTxSign[$i]}"
         sendCoinsTxHash "${name}" "${sign}"
-
-        coinsTxHashs2[$i]="${coinsgStr}"
 
         sleep 1
         height=$(${name} block last_header | jq ".height")
@@ -99,79 +71,45 @@ function genSecondChainCoinstx() {
 }
 
 function checkCoinsResult() {
-    name1=$CLI
-    name2=$CLI4
+    name=$CLI
 
     echo "====================检查第一组docker运行结果================="
+
+    totalCoinsTxAmount="0"
 
     for ((i = 0; i < ${#coinsTxHashs1[*]}; i++)); do
         txHash=${coinsTxHashs1[$i]}
         echo $txHash
-        txQuery "${name1}" $txHash
+        txQuery "${name}" $txHash
         result=$?
         if [ $result -eq 0 ]; then
-            coinsTxFee1=$(echo "$coinsTxFee1 + $defaultTxFee" | bc)
+            #coinsTxFee1=$(echo "$coinsTxFee1 + $defaultTxFee" | bc)
+            totalCoinsTxAmount=$(echo "$totalCoinsTxAmount + $coinsTxAmount" | bc)
         fi
         sleep 1
     done
 
-    #    sleep 1
-    #
-    #    fromAdd="14KEKbYtKKQm4wMthSK9J4La4nAiidGozt"
-    #    showcoinsBalance "${name1}" $fromAdd
-    #    value1=$coinsgStr
-    #
-    #    sleep 1
-    #
-    #    fromAdd=$coinsAddr1
-    #    showcoinsBalance "${name1}" $fromAdd
-    #    value2=$coinsgStr
-    #
-    #    printf "中间交易费为%d \n" $coinsTxFee1
-    #
-    #    actTotal=$(echo "$value1 + $value2 + $coinsTxFee1" | bc)
-    #    echo "${name1} 实际金额：$actTotal"
-    #
-    #    if [ "${actTotal}" = $coinsTotalAmount1 ]; then
-    #        echo "${name1} 分叉后检查实际金额符合"
-    #    else
-    #        echo "${name1} 分叉后检查实际金额不符合"
-    #    fi
+    sleep 1
 
-    echo "====================检查第二组docker运行结果================="
-    for ((i = 0; i < ${#coinsTxHashs2[*]}; i++)); do
-        txHash=${coinsTxHashs2[$i]}
-        echo $txHash
-        txQuery "${name2}" $txHash
-        result=$?
-        if [ $result -eq 0 ]; then
-            coinsTxFee2=$(echo "$coinsTxFee2 + $defaultTxFee" | bc)
-        fi
-        sleep 1
-    done
+    fromAdd="12qyocayNF7Lv6C9qW4avxs2E7U41fKSfv"
+    showCoinsBalance "${name}" $fromAdd
+    value1=$coinsgStr
 
-    #    fromAdd="1KcCVZLSQYRUwE5EXTsAoQs9LuJW6xwfQa"
-    #    showcoinsBalance "${name2}" $fromAdd
-    #    value1=$coinsgStr
-    #
-    #    sleep 1
-    #
-    #    fromAdd=$coinsAddr2
-    #    showcoinsBalance "${name2}" $fromAdd
-    #    value2=$coinsgStr
-    #
-    #    printf "中间交易费为%d \n" $coinsTxFee2
-    #
-    #    actTotal=$(echo "$value1 + $value2 +  $coinsTxFee2" | bc)
-    #    echo "${name2} 实际金额：$actTotal"
-    #
-    #    if [ "${actTotal}" = $coinsTotalAmount2 ]; then
-    #        echo "${name2} 分叉后检查实际金额符合"
-    #    else
-    #        echo "${name2} 分叉后检查实际金额不符合"
-    #    fi
-    #
-    #    sleep 1
+    sleep 1
+
+    fromAdd=$coinsAddr1
+    showCoinsBalance "${name}" $fromAdd
+    value2=$coinsgStr
+
+    actTotal=$(echo "$value1 + $value2 " | bc)
+    echo "${name} 实际金额：$actTotal"
+
+    #if [ `echo "${actTotal} > ${coinsTotalAmount1}" | bc` -eq 1 ]; then
+    if [ "$(echo "${actTotal} > ${coinsTotalAmount1}" | bc)" -eq 1 ]; then
+        echo "${name} 出现双花"
+    else
+        echo "${name} 未出现双花"
+    fi
 }
 
 function sendcoinsTx() {
@@ -201,11 +139,12 @@ function genCoinsTx() {
     toAdd=$3
     note=$4
     amount=$5
+    expire="600s"
 
     #组合形式交易
     hash=$(${name} bty transfer -t "${toAdd}" -a "${amount}" -n "${note}" | tr '\r' ' ')
     echo "${hash}"
-    sign=$(${name} wallet sign -a "${fromAdd}" -d "${hash}" | tr '\r' ' ')
+    sign=$(${name} wallet sign -a "${fromAdd}" -d "${hash}" -e "${expire}" | tr '\r' ' ')
     echo "sign: $sign"
     coinsgStr=$sign
 
@@ -235,4 +174,10 @@ function createAccount() {
     result=$($name account create -l "${label}" | jq -r ".acc.addr")
     printf 'New account addr %s \n' "${result}"
     coinsgStr=$result
+}
+
+function shouAccountList() {
+    name=$1
+    printf '==========shouAccountList name=%s ==========\n' "${name}"
+    $name account list
 }
