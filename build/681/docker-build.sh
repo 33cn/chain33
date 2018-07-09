20c9de4f4111ae39493cbc64401f8428c252cfa1
suyanlong <592938410@qq.com>
2018-07-09 20:31:42 +0800

    Remove unused shell
    Former-commit-id: 4f0ff34637c24612296992c63bafcc4a2e4aae01
    Former-commit-id: a5af17b8cd938899790c4bbc86937c705a9016a5

--- build/681/docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# https://hub.docker.com/r/suyanlong/golang-dev/
-# https://github.com/suyanlong/golang-dev
-# sudo docker pull suyanlong/golang-dev:latest
-
-sudo docker run -it -p 8801:8801 -p 8802:8802 -p 6060:6060 -p 50051:50051 -l linux-chain33-build \
-    -v "$GOPATH"/src/gitlab.33.cn/chain33/chain33:/go/src/gitlab.33.cn/chain33/chain33 \
-    -w /go/src/gitlab.33.cn/chain33/chain33 suyanlong/golang-dev:latest
