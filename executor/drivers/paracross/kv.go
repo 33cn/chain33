4f9803c1291fa133f60565602d0298bca46e76d7
林敬 <linj@disanbo.com>
2018-07-20 16:52:18 +0800

    update gen key func
    Former-commit-id: 0488041237345e67c50037d628330cf47082c345
    Former-commit-id: c0a2f5e2258e3f9822771e6d33424f77e2e7f825

--- executor/drivers/paracross/kv.go
+++ executor/drivers/paracross/kv.go
@@ -40,12 +40,16 @@ func calcLocalTxKey(title string, height int64, addr string) []byte {
 	return []byte(fmt.Sprintf(localTx+"%s-%012-%s", title, height, addr))
 }
 
-func calcLocalTitle(title string) []byte {
+func calcLocalTitleKey(title string) []byte {
 	return []byte(fmt.Sprintf(localTitle+"%s", title))
 }
 
-func calcLocalTitleHeight(title string, height int64) []byte {
+func calcLocalTitleHeightKey(title string, height int64) []byte {
 	return []byte(fmt.Sprintf(localTitle+"%s-%012d", title, height))
+}
+
+func calcLocalTitlePrefix() []byte {
+	return  []byte(localTitle)
 }
 
 
