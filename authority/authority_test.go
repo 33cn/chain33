ef46e8b20c3267e6facd638cb4b921e6583eea91
gitlab <gitlab@hzfzm.cn>
2018-07-23 10:37:42 +0800

    auto ci
    Former-commit-id: 6e4e97a90b2eb813cb406f774e89672b94b6b038
    Former-commit-id: f5b5c6858f6041f0bc14d9f362ee70ae62f9fbab

--- authority/authority_test.go
+++ authority/authority_test.go
@@ -5,12 +5,12 @@ import (
 	"testing"
 	"time"
 
+	"gitlab.33.cn/chain33/chain33/authority/utils"
 	"gitlab.33.cn/chain33/chain33/common/config"
 	"gitlab.33.cn/chain33/chain33/common/crypto"
 	"gitlab.33.cn/chain33/chain33/common/merkle"
 	"gitlab.33.cn/chain33/chain33/executor/drivers"
 	"gitlab.33.cn/chain33/chain33/types"
-	"gitlab.33.cn/chain33/chain33/authority/utils"
 )
 
 var (
