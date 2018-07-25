b7616d148ca2fa9f8c8919d34a6580c4f43a1984
gitlab <gitlab@hzfzm.cn>
2018-07-25 16:58:36 +0800

    auto ci

--- authority/core/gmimpl.go
+++ authority/core/gmimpl.go
@@ -12,8 +12,8 @@ import (
 	"time"
 
 	"github.com/tjfoc/gmsm/sm2"
-	sm2_util "gitlab.33.cn/chain33/chain33/common/crypto/sm2"
 	"gitlab.33.cn/chain33/chain33/authority/utils"
+	sm2_util "gitlab.33.cn/chain33/chain33/common/crypto/sm2"
 	"gitlab.33.cn/chain33/chain33/types"
 )
 
