ba6dd15208f89cf6ecd8059494a5ac6c5b514bdd
林敬 <linj@disanbo.com>
2018-07-27 11:03:13 +0800

    init

--- executor/executor.go
+++ executor/executor.go
@@ -22,6 +22,7 @@ import (
 	"gitlab.33.cn/chain33/chain33/executor/drivers/manage"
 	"gitlab.33.cn/chain33/chain33/executor/drivers/none"
 	"gitlab.33.cn/chain33/chain33/executor/drivers/norm"
+	"gitlab.33.cn/chain33/chain33/executor/drivers/paracross"
 	"gitlab.33.cn/chain33/chain33/executor/drivers/relay"
 	"gitlab.33.cn/chain33/chain33/executor/drivers/retrieve"
 	"gitlab.33.cn/chain33/chain33/executor/drivers/ticket"
@@ -70,6 +71,7 @@ func execInit() {
 	manage.Init()
 	none.Init()
 	norm.Init()
+	paracross.Init()
 	retrieve.Init()
 	ticket.Init()
 	token.Init()
