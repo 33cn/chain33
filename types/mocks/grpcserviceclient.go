dff79288e4aa2270392beb71eb5468c7cde48cea
madengji <mdj@disanbo.com>
2018-08-14 10:27:54 +0800

    fmt
    Former-commit-id: 1196867987221ec37c208db5ed7c57795d8700f2

--- types/mocks/grpcserviceclient.go
+++ types/mocks/grpcserviceclient.go
@@ -191,6 +191,36 @@ func (_m *GrpcserviceClient) DumpPrivkey(ctx context.Context, in *types.ReqStr, 
 	return r0, r1
 }
 
+// EnablePrivacy provides a mock function with given fields: ctx, in, opts
+func (_m *GrpcserviceClient) EnablePrivacy(ctx context.Context, in *types.ReqEnablePrivacy, opts ...grpc.CallOption) (*types.RepEnablePrivacy, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *types.RepEnablePrivacy
+	if rf, ok := ret.Get(0).(func(context.Context, *types.ReqEnablePrivacy, ...grpc.CallOption) *types.RepEnablePrivacy); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*types.RepEnablePrivacy)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *types.ReqEnablePrivacy, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GenSeed provides a mock function with given fields: ctx, in, opts
 func (_m *GrpcserviceClient) GenSeed(ctx context.Context, in *types.GenSeedLang, opts ...grpc.CallOption) (*types.ReplySeed, error) {
 	_va := make([]interface{}, len(opts))
