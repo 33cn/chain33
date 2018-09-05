8ed13cb4757fc72122cfe70485d98ee3b2e01732
sanghg <shg@disanbo.com>
2018-09-05 14:39:34 +0800

    重构执行器相关的代码
    Former-commit-id: 4c263a5d30c3a7ec65dd66c3151d93c4fb2973b5

---
+++ pluginmanager/plugins/game/types/const.go
@@ -0,0 +1,19 @@
+package types
+
+//game action ty
+const (
+	GameActionCreate = iota + 1
+	GameActionMatch
+	GameActionCancel
+	GameActionClose
+)
+
+const (
+	PackageName		= "gitlab.33.cn/game"
+	GameX           = "game"
+
+)
+
+var (
+	ExecerGame       = []byte(GameX)
+)
