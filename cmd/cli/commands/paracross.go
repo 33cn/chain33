6888c73679f62f5d96217399a5c8a2a4d6e41d74
林敬 <linj@disanbo.com>
2018-09-06 17:26:15 +0800

    add cmd for paracross
    Former-commit-id: 63fa595b0690e8ef6bf1dc8d8e1781862295d33b
    Former-commit-id: a36e51afb78ff132d39f1616eb050958ba019cf8

---
+++ cmd/cli/commands/paracross.go
@@ -0,0 +1,128 @@
+package commands
+
+import (
+	"encoding/hex"
+	"fmt"
+	"math"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"gitlab.33.cn/chain33/chain33/types"
+	pt "gitlab.33.cn/chain33/chain33/types/executor/paracross"
+)
+
+func ParcCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "para",
+		Short: "Construct para transactions",
+		Args:  cobra.MinimumNArgs(1),
+	}
+	cmd.AddCommand(
+		CreateRawAssetTransferCmd(),
+		CreateRawAssetWithdrawCmd(),
+	)
+	return cmd
+}
+
+// create raw transfer tx
+func CreateRawAssetTransferCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "transfer",
+		Short: "Create a asset transfer transaction",
+		Run:   createAssetTransfer,
+	}
+	addCreateAssetTransferFlags(cmd)
+	return cmd
+}
+
+func addCreateAssetTransferFlags(cmd *cobra.Command) {
+	cmd.Flags().StringP("to", "t", "", "receiver account address")
+	cmd.MarkFlagRequired("to")
+
+	cmd.Flags().Float64P("amount", "a", 0, "transaction amount")
+	cmd.MarkFlagRequired("amount")
+
+	cmd.Flags().StringP("note", "n", "", "transaction note info")
+
+	cmd.Flags().StringP("title", "", "", "the title of para chain, like `p.user.guodun.`")
+	cmd.MarkFlagRequired("title")
+}
+
+func createAssetTransfer(cmd *cobra.Command, args []string) {
+	txHex, err := createAssetTx(cmd, true)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	fmt.Println(txHex)
+}
+
+// create raw withdraw tx
+func CreateRawAssetWithdrawCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "withdraw",
+		Short: "Create a asset withdraw transaction",
+		Run:   createAssetWithdraw,
+	}
+	addCreateAssetWithdrawFlags(cmd)
+	return cmd
+}
+
+func addCreateAssetWithdrawFlags(cmd *cobra.Command) {
+	cmd.Flags().Float64P("amount", "a", 0, "withdraw amount")
+	cmd.MarkFlagRequired("amount")
+
+	cmd.Flags().StringP("note", "n", "", "transaction note info")
+
+	cmd.Flags().StringP("title", "", "", "the title of para chain, like `p.user.guodun.`")
+	cmd.MarkFlagRequired("title")
+
+	cmd.Flags().StringP("to", "t", "", "receiver account address")
+	cmd.MarkFlagRequired("to")
+}
+
+func createAssetWithdraw(cmd *cobra.Command, args []string) {
+	txHex, err := createAssetTx(cmd, true)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	fmt.Println(txHex)
+}
+
+func createAssetTx(cmd *cobra.Command, isWithdraw bool) (string, error) {
+	amount, _ := cmd.Flags().GetFloat64("amount")
+	if amount < 0 {
+		return "", types.ErrAmount
+	}
+	amountInt64 := int64(math.Trunc((amount+0.0000001)*1e4)) * 1e4
+
+	toAddr, _ := cmd.Flags().GetString("to")
+	note, _ := cmd.Flags().GetString("note")
+
+	title, _ := cmd.Flags().GetString("title")
+	if strings.HasPrefix(title, "p.user.") {
+		fmt.Fprintln(os.Stderr, "title is not right, title format like `p.user.guodun.`")
+		return "", types.ErrInputPara
+	}
+	execName := title + types.ParaX
+
+	param := types.CreateTx{
+		To:          toAddr,
+		Amount:      amountInt64,
+		Fee:         0,
+		Note:        note,
+		IsWithdraw:  isWithdraw,
+		IsToken:     false,
+		TokenSymbol: "",
+		ExecName:    execName,
+	}
+	tx, err := pt.CreateRawTransferTx(&param)
+	if err != nil {
+		return "", err
+	}
+
+	txHex := types.Encode(tx)
+	return hex.EncodeToString(txHex), nil
+}
