ea834f21d2855409f16dc3c0f8fa89690d75634b
kingwang <kingwang@kingwangdeiMac.local>
2018-09-06 15:09:09 +0800

    move mavl and mpt to plugin module
    Former-commit-id: 45bd4f0d397484c6ec0f313035d655de3455bb31

--- plugin/store/mpt/mpt.go
+++ plugin/store/mpt/mpt.go
@@ -5,7 +5,7 @@ import (
 	log "github.com/inconshreveable/log15"
 	"gitlab.33.cn/chain33/chain33/common"
 	clog "gitlab.33.cn/chain33/chain33/common/log"
-	"gitlab.33.cn/chain33/chain33/common/mpt"
+	mpt "gitlab.33.cn/chain33/chain33/plugin/store/mpt/db"
 	"gitlab.33.cn/chain33/chain33/queue"
 	drivers "gitlab.33.cn/chain33/chain33/system/store"
 	"gitlab.33.cn/chain33/chain33/types"
