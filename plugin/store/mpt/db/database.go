a664e9d93389e7a392288cc9d9ffe862fcc98383
kingwang <kingwang@kingwangdeiMac.local>
2018-09-06 15:09:09 +0800

    move mavl and mpt to plugin module
    Former-commit-id: 45bd4f0d397484c6ec0f313035d655de3455bb31
    Former-commit-id: ea834f21d2855409f16dc3c0f8fa89690d75634b

--- plugin/store/mpt/db/database.go
+++ plugin/store/mpt/db/database.go
@@ -24,7 +24,7 @@ import (
 
 	"gitlab.33.cn/chain33/chain33/common"
 	dbm "gitlab.33.cn/chain33/chain33/common/db"
-	"gitlab.33.cn/chain33/chain33/common/mpt/rlp"
+	"gitlab.33.cn/chain33/chain33/plugin/store/mpt/db/rlp"
 )
 
 // secureKeyPrefix is the database key prefix used to store trie node preimages.
