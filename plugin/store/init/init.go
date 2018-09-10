a9ce18e8091fab5bfd7a76349e6817e25a0084dc
张振华 <zzh@33.cn>
2018-09-10 18:02:46 +0800

    mvcc
    Former-commit-id: 9fc32afc67fdc8bbdce52f057d6810f0e258632b
    Former-commit-id: 2878dcb4b1fbb6400864f4e0a14b84a5f5c3954a

--- plugin/store/init/init.go
+++ plugin/store/init/init.go
@@ -3,4 +3,5 @@ package plugin
 import (
 	_ "gitlab.33.cn/chain33/chain33/plugin/store/kvdb"
 	_ "gitlab.33.cn/chain33/chain33/plugin/store/mpt"
+	_ "gitlab.33.cn/chain33/chain33/plugin/store/kvmvcc"
 )
