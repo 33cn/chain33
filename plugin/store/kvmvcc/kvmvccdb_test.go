8207bb4f5c66721526e3bcaebc44cba7ded81d83
张振华 <zzh@33.cn>
2018-09-13 09:08:52 +0800

    change ut code
    Former-commit-id: 9ad06bed9f84c5790227cb5a24ae47d8314038e3
    Former-commit-id: e848f4e40ac7371473598219317295c787bbeb84

--- plugin/store/kvmvcc/kvmvccdb_test.go
+++ plugin/store/kvmvcc/kvmvccdb_test.go
@@ -10,11 +10,11 @@ import (
 	"fmt"
 )
 
-var store_cfg0 = &types.Store{"kvmvcc", "leveldb", "kvdb_test0", 100}
-var store_cfg1 = &types.Store{"kvmvcc", "leveldb", "kvdb_test1", 100}
-var store_cfg2 = &types.Store{"kvmvcc", "leveldb", "kvdb_test2", 100}
-var store_cfg3 = &types.Store{"kvmvcc", "leveldb", "kvdb_test3", 100}
-var store_cfg4 = &types.Store{"kvmvcc", "leveldb", "kvdb_test4", 100}
+var store_cfg0 = &types.Store{"kvmvcc", "leveldb", "/tmp/kvdb_test0", 100}
+var store_cfg1 = &types.Store{"kvmvcc", "leveldb", "/tmp/kvdb_test1", 100}
+var store_cfg2 = &types.Store{"kvmvcc", "leveldb", "/tmp/kvdb_test2", 100}
+var store_cfg3 = &types.Store{"kvmvcc", "leveldb", "/tmp/kvdb_test3", 100}
+var store_cfg4 = &types.Store{"kvmvcc", "leveldb", "/tmp/kvdb_test4", 100}
 
 func TestKvmvccdbNewClose(t *testing.T) {
 	os.RemoveAll(store_cfg0.DbPath)
