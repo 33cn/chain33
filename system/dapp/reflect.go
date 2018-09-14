26fd406fafd17e73543860d611fbcaa6dea2059a
kingwang <kingwang@kingwangdeiMac.local>
2018-09-14 14:52:04 +0800

    fixbug test

--- system/dapp/reflect.go
+++ system/dapp/reflect.go
@@ -3,36 +3,7 @@ package dapp
 import (
 	"reflect"
 	"strings"
-	"sync"
-	"unicode"
-	"unicode/utf8"
 )
-
-var typeOfError = reflect.TypeOf((*error)(nil)).Elem()
-
-type methodType struct {
-	sync.Mutex // protects counters
-	method     reflect.Method
-	ArgType    reflect.Type
-	ReplyType  reflect.Type
-	numCalls   uint
-}
-
-// Is this an exported - upper case - name?
-func isExported(name string) bool {
-	rune, _ := utf8.DecodeRuneInString(name)
-	return unicode.IsUpper(rune)
-}
-
-// Is this type exported or a builtin?
-func isExportedOrBuiltinType(t reflect.Type) bool {
-	for t.Kind() == reflect.Ptr {
-		t = t.Elem()
-	}
-	// PkgPath will be non-empty even for an exported type,
-	// so we need to check the type name as well.
-	return isExported(t.Name()) || t.PkgPath() == ""
-}
 
 func buildFuncList(funclist []interface{}) map[string]bool {
 	list := make(map[string]bool)
