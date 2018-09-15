b24d53820eb7206d03643015a1374affffb31458
madengji <mdj@disanbo.com>
2018-09-15 19:42:45 +0800

    fix para.usr.p.paracross is not Allowd in main

--- executor/executor.go
+++ executor/executor.go
@@ -244,7 +244,7 @@ func isAllowExec(key, txexecer []byte, tx *types.Transaction, height int64) bool
 	if ok {
 		if execaddr == drivers.ExecAddress(string(tx.Execer)) {
 			return true
-		} else if !types.IsPara() && types.IsParaCrossTransferTx(txexecer, tx) && execaddr == drivers.ExecAddress(string(tx.Execer)) {
+		} else if !types.IsPara() && types.IsParaCrossTransferTx(txexecer, tx) {
 			// 跨链交易需要在主链和平行链都执行， 现在 txexecer 设置为 $(title) + types.ParaX
 			return true
 		}
