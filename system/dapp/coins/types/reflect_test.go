0ea47990c4eca77c4ce6c0705bb4bc1f62647962
king wang <kingwang@kingdeMacBook-Pro.local>
2018-09-18 14:24:17 +0800

    reflect executor rpc
    Former-commit-id: 7773fa930fc9c8a31f4c497cbbf3e1c6f9d6d550
    Former-commit-id: 221b46af3d6d663a1fc95c726486925382463f04

--- system/dapp/coins/types/reflect_test.go
+++ system/dapp/coins/types/reflect_test.go
@@ -9,8 +9,8 @@ import (
 
 func TestMethodCall(t *testing.T) {
 	action := &CoinsAction{Value: &CoinsAction_Transfer{Transfer: &types.AssetsTransfer{}}}
-	funclist := ListMethod(action)
-	name, ty, v := GetActionValue(action, funclist)
+	funclist := types.ListMethod(action)
+	name, ty, v := types.GetActionValue(action, funclist)
 	assert.Equal(t, int32(0), ty)
 	assert.Equal(t, "Transfer", name)
 	assert.Equal(t, &types.AssetsTransfer{}, v.Interface())
@@ -18,7 +18,7 @@ func TestMethodCall(t *testing.T) {
 
 func TestListMethod(t *testing.T) {
 	action := &CoinsAction{Value: &CoinsAction_Transfer{Transfer: &types.AssetsTransfer{}}}
-	funclist := ListMethod(action)
+	funclist := types.ListMethod(action)
 	excpect := []string{"GetWithdraw", "GetGenesis", "GetTransfer", "GetTransferToExec", "GetValue"}
 	for _, v := range excpect {
 		if _, ok := funclist[v]; !ok {
@@ -27,11 +27,19 @@ func TestListMethod(t *testing.T) {
 	}
 }
 
+func TestListType(t *testing.T) {
+	excpect := []string{"Value_Withdraw", "Withdraw", "Value_Transfer", "Value_Genesis", "Value_TransferToExec"}
+	for _, v := range excpect {
+		if _, ok := NewType().GetValueTypeMap()[v]; !ok {
+			t.Error(v + " is not in list")
+		}
+	}
+}
 func BenchmarkGetActionValue(b *testing.B) {
 	action := &CoinsAction{Value: &CoinsAction_Transfer{Transfer: &types.AssetsTransfer{}}}
-	funclist := ListMethod(action)
+	funclist := types.ListMethod(action)
 	for i := 0; i < b.N; i++ {
-		_, _, v := GetActionValue(action, funclist)
+		_, _, v := types.GetActionValue(action, funclist)
 		assert.NotNil(b, v)
 	}
 }
--- types/executor.go
+++ types/executor.go
@@ -3,8 +3,13 @@ package types
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"reflect"
+	"strings"
+	"time"
 	"unicode"
+
+	"gitlab.33.cn/chain33/chain33/types"
 )
 
 type ExecutorType interface {
@@ -124,8 +129,10 @@ type ExecType interface {
 	GetLogMap() map[int64]reflect.Type
 	//actionType -> name map
 	GetTypeMap() map[string]int32
+	GetValueTypeMap() map[string]reflect.Type
 	//action function list map
 	GetFuncMap() map[string]reflect.Method
+	GetRPCFuncMap() map[string]reflect.Method
 }
 
 type ExecTypeGet interface {
@@ -133,7 +140,49 @@ type ExecTypeGet interface {
 }
 
 type ExecTypeBase struct {
-	child ExecType
+	child               ExecType
+	childValue          reflect.Value
+	actionFunList       map[string]reflect.Method
+	actionListValueType map[string]reflect.Type
+	rpclist             map[string]reflect.Method
+}
+
+func (base *ExecTypeBase) SetChild(child ExecType) {
+	base.child = child
+	base.childValue = reflect.ValueOf(child)
+	base.rpclist = ListMethod(child)
+	action := child.GetPayload()
+	base.actionFunList = ListMethod(action)
+	retval := base.actionFunList["XXX_OneofFuncs"].Func.Call([]reflect.Value{reflect.ValueOf(action)})
+	if len(retval) != 4 {
+		panic("err XXX_OneofFuncs")
+	}
+	list := ListType(retval[3].Interface().([]interface{}))
+	base.actionListValueType = make(map[string]reflect.Type)
+	for k, v := range list {
+		data := strings.Split(k, "_")
+		if len(data) != 2 {
+			panic("name create " + k)
+		}
+		base.actionListValueType["Value_"+data[1]] = v
+		field := v.Field(0)
+		base.actionListValueType[field.Name] = field.Type
+	}
+	//check type map is all in value type list
+	typelist := base.child.GetTypeMap()
+	for k := range typelist {
+		if _, ok := base.actionListValueType[k]; !ok {
+			panic("value type not found " + k)
+		}
+	}
+}
+
+func (base *ExecTypeBase) GetRPCFuncMap() map[string]reflect.Method {
+	return base.rpclist
+}
+
+func (base *ExecTypeBase) GetValueTypeMap() map[string]reflect.Type {
+	return base.actionListValueType
 }
 
 //用户看到的ToAddr
@@ -190,8 +239,8 @@ func (base *ExecTypeBase) GetViewFromToAddr(tx *Transaction) (string, string) {
 	return tx.From(), tx.To
 }
 
-func (base *ExecTypeBase) SetChild(child ExecType) {
-	base.child = child
+func (base *ExecTypeBase) GetFuncMap() map[string]reflect.Method {
+	return base.actionFunList
 }
 
 func (base *ExecTypeBase) DecodePayload(tx *Transaction) (interface{}, error) {
@@ -247,4 +296,117 @@ func lowcaseFirst(v string) string {
 		return string(change)
 	}
 	return v
+}
+
+func (base *ExecTypeBase) callRPC(method reflect.Method, action string, msg json.RawMessage) (tx *Transaction, err error) {
+	valueret := method.Func.Call([]reflect.Value{base.childValue, reflect.ValueOf(action), reflect.ValueOf(msg)})
+	if len(valueret) != 2 {
+		return nil, types.ErrMethodNotFound
+	}
+	r1 := valueret[0].Interface()
+	if r1 != nil {
+		if r, ok := r1.(*types.Transaction); ok {
+			tx = r
+		} else {
+			return nil, types.ErrMethodReturnType
+		}
+	}
+	//参数2
+	r2 := valueret[1].Interface()
+	err = nil
+	if r2 != nil {
+		if r, ok := r2.(error); ok {
+			err = r
+		} else {
+			return nil, types.ErrMethodReturnType
+		}
+	}
+	if tx == nil && err == nil {
+		return nil, types.ErrActionNotSupport
+	}
+	return tx, err
+}
+
+func (base *ExecTypeBase) createRPC(action string, message json.RawMessage) (*Transaction, error) {
+	err := json.Unmarshal(message, &param)
+	if err != nil {
+		tlog.Error("CreateTx", "Error", err)
+		return nil, ErrInputPara
+	}
+	if param.ExecName != "" && !IsAllowExecName([]byte(param.ExecName), []byte(param.ExecName)) {
+		tlog.Error("CreateTx", "Error", ErrExecNameNotMatch)
+		return nil, ErrExecNameNotMatch
+	}
+
+	//to地址要么是普通用户地址，要么就是执行器地址，不能为空
+	if param.To == "" {
+		return nil, ErrAddrNotExist
+	}
+
+	var tx *Transaction
+	if param.Amount < 0 {
+		return nil, ErrAmount
+	}
+	if param.IsToken {
+		return nil, ErrNotSupport
+	} else {
+		tx = base.CreateCoinsTransfer(&param)
+	}
+
+	tx.Fee, err = tx.GetRealFee(MinFee)
+	if err != nil {
+		return nil, err
+	}
+
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	tx.Nonce = random.Int63()
+
+	return tx, nil
+
+}
+
+func (base *ExecTypeBase) CreateTx(action string, message json.RawMessage) (*Transaction, error) {
+	//先判断 FuncList 中有没有符合要求的函数 RPC_{action}
+	funclist := base.GetRPCFuncMap()
+	if method, ok := funclist["RPC_"+action]; ok {
+		return base.callRPC(method, action, msg)
+	}
+	typemap := base.child.GetTypeMap()
+	if _, ok := typemap[action]; ok {
+		return base.createRPC(action, msg)
+	}
+	return nil, types.ErrActionNotSupport
+}
+
+func (base *ExecTypeBase) CreateCoinsTransfer(param *CreateTx) *Transaction {
+	return nil
+	/*
+		transfer := base.child.GetPayload()
+		to := ""
+		if IsPara() {
+			to = param.GetTo()
+		}
+			if !param.IsWithdraw {
+				if param.ExecName != "" {
+					v := &cty.CoinsAction_TransferToExec{TransferToExec: &AssetsTransferToExec{
+						Amount: param.Amount, Note: param.GetNote(), ExecName: param.GetExecName(), To: to}}
+					transfer.Value = v
+					transfer.Ty = cty.CoinsActionTransferToExec
+				} else {
+					v := &cty.CoinsAction_Transfer{Transfer: &AssetsTransfer{
+						Amount: param.Amount, Note: param.GetNote(), To: to}}
+					transfer.Value = v
+					transfer.Ty = cty.CoinsActionTransfer
+				}
+			} else {
+				v := &cty.CoinsAction_Withdraw{Withdraw: &AssetsWithdraw{
+					Amount: param.Amount, Note: param.GetNote(), ExecName: param.GetExecName(), To: to}}
+				transfer.Value = v
+				transfer.Ty = cty.CoinsActionWithdraw
+			}
+			if IsPara() {
+				return &Transaction{Execer: []byte(nameX), Payload: Encode(transfer), To: address.ExecAddress(nameX)}
+			}
+			return &Transaction{Execer: []byte(nameX), Payload: Encode(transfer), To: param.GetTo()}
+	*/
 }
