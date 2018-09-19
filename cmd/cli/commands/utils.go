563e7c2622b2cdfe3cb991b3b6eb3cc4bef94a8b
gitlab <gitlab@hzfzm.cn>
2018-09-19 21:11:08 +0800

    auto ci
    Former-commit-id: 327577866c56ab9b5199b25b8c669631985ab7bd
    Former-commit-id: 3f3d256d68e4b9e2fd47e0d29ff7c91409b8d050

--- cmd/cli/commands/utils.go
+++ cmd/cli/commands/utils.go
@@ -14,9 +14,9 @@ import (
 	"github.com/spf13/cobra"
 	"gitlab.33.cn/chain33/chain33/common"
 	"gitlab.33.cn/chain33/chain33/common/address"
+	pt "gitlab.33.cn/chain33/chain33/plugin/dapp/paracross/types"
 	jsonrpc "gitlab.33.cn/chain33/chain33/rpc"
 	cty "gitlab.33.cn/chain33/chain33/system/dapp/coins/types"
-	pt "gitlab.33.cn/chain33/chain33/plugin/dapp/paracross/types"
 	"gitlab.33.cn/chain33/chain33/types"
 )
 
@@ -292,7 +292,7 @@ func decodeLog(rlog jsonrpc.ReceiptDataResult) *ReceiptData {
 			types.TyLogBlackwhiteCreate, types.TyLogBlackwhiteShow, types.TyLogBlackwhitePlay,
 			types.TyLogBlackwhiteTimeout, types.TyLogBlackwhiteDone, types.TyLogBlackwhiteLoopInfo,
 			types.TyLogLotteryCreate, types.TyLogLotteryBuy, types.TyLogLotteryDraw, types.TyLogLotteryClose,
-			pt.TyLogParacrossMiner,pt.TyLogParaAssetTransfer,pt.TyLogParaAssetWithdraw,pt.TyLogParacrossCommit:
+			pt.TyLogParacrossMiner, pt.TyLogParaAssetTransfer, pt.TyLogParaAssetWithdraw, pt.TyLogParacrossCommit:
 
 			rl.Log = l.Log
 		//case 2, 3, 5, 11:
