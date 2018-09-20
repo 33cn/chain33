b9e35cbeed8f4c6e52096ffbabad791f2231ba6e
sanghg <shg@disanbo.com>
2018-09-20 09:39:11 +0800

    重构game执行器
    1.重构Exec()函数实现
    
    
    Former-commit-id: 2bf0e9f39f8934d0d5823c713191f513e3eb87b6

--- plugin/dapp/game/executor/exec.go
+++ plugin/dapp/game/executor/exec.go
@@ -1,12 +1,26 @@
 package executor
 
-
 import (
-	"gitlab.33.cn/chain33/chain33/types"
 	gt "gitlab.33.cn/chain33/chain33/plugin/dapp/game/types"
+	"gitlab.33.cn/chain33/chain33/types"
 )
 
+func (g *Game) Exec_Create(payload *gt.GameCreate, tx *types.Transaction, index int) (*types.Receipt, error) {
+	action := NewAction(g, tx, index)
+	return action.GameCreate(payload)
+}
 
-func (c *Game) Exec_Create(payload *gt.GameCreate, tx *types.Transaction, index int) (*types.Receipt, error) {
-	return nil, nil
+func (g *Game) Exec_Cancel(payload *gt.GameCancel, tx *types.Transaction, index int) (*types.Receipt, error) {
+	action := NewAction(g, tx, index)
+	return action.GameCancel(payload)
+}
+
+func (g *Game) Exec_Close(payload *gt.GameClose, tx *types.Transaction, index int) (*types.Receipt, error) {
+	action := NewAction(g, tx, index)
+	return action.GameClose(payload)
+}
+
+func (g *Game) Exec_Match(payload *gt.GameMatch, tx *types.Transaction, index int) (*types.Receipt, error) {
+	action := NewAction(g, tx, index)
+	return action.GameMatch(payload)
 }
