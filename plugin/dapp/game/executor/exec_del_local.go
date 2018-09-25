7ad3b6eea28305404091495c0b026e5e241f9496
sanghg <shg@disanbo.com>
2018-09-25 14:17:46 +0800

    Merge remote-tracking branch 'remotes/origin/develop' into CHAIN33-494-game-bugfix
    # Conflicts:
    #	plugin/dapp/game/executor/game.go
    
    
    Former-commit-id: 1326f3d909796c407518c0fbd451f4997ebd8603
    Former-commit-id: 9069fd8dfaf8b2299a05c40f36833557293a672c

--- plugin/dapp/game/executor/exec_del_local.go
+++ plugin/dapp/game/executor/exec_del_local.go
@@ -1,1 +1,41 @@
 package executor
+
+import (
+	gt "gitlab.33.cn/chain33/chain33/plugin/dapp/game/types"
+	"gitlab.33.cn/chain33/chain33/types"
+)
+
+func (g *Game) execDelLocal(receiptData *types.ReceiptData) (*types.LocalDBSet, error) {
+	dbSet := &types.LocalDBSet{}
+	if receiptData.GetTy() != types.ExecOk {
+		return dbSet, nil
+	}
+	for _, log := range receiptData.Logs {
+		switch log.GetTy() {
+		case types.TyLogCreateGame, types.TyLogMatchGame, types.TyLogCloseGame, types.TyLogCancleGame:
+			receiptGame := &gt.ReceiptGame{}
+			if err := types.Decode(log.Log, receiptGame); err != nil {
+				return nil, err
+			}
+			kv := g.rollbackIndex(receiptGame)
+			dbSet.KV = append(dbSet.KV, kv...)
+		}
+	}
+	return dbSet, nil
+}
+
+func (g *Game) ExecDelLocal_Create(payload *gt.GameCreate, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
+	return g.execDelLocal(receiptData)
+}
+
+func (g *Game) ExecDelLocal_Cancel(payload *gt.GameCancel, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
+	return g.execDelLocal(receiptData)
+}
+
+func (g *Game) ExecDelLocal_Close(payload *gt.GameClose, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
+	return g.execDelLocal(receiptData)
+}
+
+func (g *Game) ExecDelLocal_Match(payload *gt.GameMatch, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
+	return g.execDelLocal(receiptData)
+}
