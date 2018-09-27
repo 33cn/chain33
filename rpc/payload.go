087d3a80c577388cefc797cf6cd539aaf8657bbe
sanghg <shg@disanbo.com>
2018-09-27 13:51:26 +0800

    按照新版本的框架要求，重构lottery执行器模块
    Former-commit-id: 3b782a370fe5a89398adf6a77b8d770a000e363e

--- rpc/payload.go
+++ rpc/payload.go
@@ -137,26 +137,26 @@ func relayPayloadType(funcname string) (proto.Message, error) {
 	return req, nil
 }
 
-func lotteryPayloadType(funcname string) (proto.Message, error) {
-	var req proto.Message
-	switch funcname {
-	case "GetLotteryNormalInfo":
-		req = &types.ReqLotteryInfo{}
-	case "GetLotteryCurrentInfo":
-		req = &types.ReqLotteryInfo{}
-	case "GetLotteryHistoryLuckyNumber":
-		req = &types.ReqLotteryInfo{}
-	case "GetLotteryRoundLuckyNumber":
-		req = &types.ReqLotteryLuckyInfo{}
-	case "GetLotteryHistoryBuyInfo":
-		req = &types.ReqLotteryBuyHistory{}
-	case "GetLotteryBuyRoundInfo":
-		req = &types.ReqLotteryBuyInfo{}
-	default:
-		return nil, types.ErrInputPara
-	}
-	return req, nil
-}
+//func lotteryPayloadType(funcname string) (proto.Message, error) {
+//	var req proto.Message
+//	switch funcname {
+//	case "GetLotteryNormalInfo":
+//		req = &types.ReqLotteryInfo{}
+//	case "GetLotteryCurrentInfo":
+//		req = &types.ReqLotteryInfo{}
+//	case "GetLotteryHistoryLuckyNumber":
+//		req = &types.ReqLotteryInfo{}
+//	case "GetLotteryRoundLuckyNumber":
+//		req = &types.ReqLotteryLuckyInfo{}
+//	case "GetLotteryHistoryBuyInfo":
+//		req = &types.ReqLotteryBuyHistory{}
+//	case "GetLotteryBuyRoundInfo":
+//		req = &types.ReqLotteryBuyInfo{}
+//	default:
+//		return nil, types.ErrInputPara
+//	}
+//	return req, nil
+//}
 
 func payloadType(execer, funcname string) (proto.Message, error) {
 	switch execer {
@@ -178,8 +178,8 @@ func payloadType(execer, funcname string) (proto.Message, error) {
 		return privacyPayloadType(funcname)
 	case types.ExecName(types.RelayX):
 		return relayPayloadType(funcname)
-	case types.ExecName(types.LotteryX):
-		return lotteryPayloadType(funcname)
+		//case types.ExecName(types.LotteryX):
+		//	return lotteryPayloadType(funcname)
 	}
 	return nil, types.ErrInputPara
 }
