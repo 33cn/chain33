845c3b2f68803fcfac83a9e1e98d02a6948e0273
sanghg <shg@disanbo.com>
2018-09-27 18:13:21 +0800

    重构hashlock模块的Exec()\ExecLocal()\ExecDelLocal()\Query()模块的代码

--- plugin/dapp/hashlock/executor/exec_local.go
+++ plugin/dapp/hashlock/executor/exec_local.go
@@ -1,1 +1,18 @@
 package executor
+
+import (
+	pty "gitlab.33.cn/chain33/chain33/plugin/dapp/hashlock/types"
+	"gitlab.33.cn/chain33/chain33/types"
+)
+
+func (h *Hashlock) ExecLocal_Hlock(transfer *pty.HashlockLock, tx *types.Transaction, receipt *types.ReceiptData, index int) (*types.LocalDBSet, error) {
+	return nil, nil
+}
+
+func (h *Hashlock) ExecLocal_Hsend(transfer *pty.HashlockSend, tx *types.Transaction, receipt *types.ReceiptData, index int) (*types.LocalDBSet, error) {
+	return nil, nil
+}
+
+func (h *Hashlock) ExecLocal_Hunlock(transfer *pty.HashlockUnlock, tx *types.Transaction, receipt *types.ReceiptData, index int) (*types.LocalDBSet, error) {
+	return nil, nil
+}
