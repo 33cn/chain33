884ea64e002a296495bea3750d0855c572b36734
king wang <kingwang@kingdeMacBook-Pro.local>
2018-09-27 22:36:52 +0800

    重构黑白配的log
    Former-commit-id: 56e773fc0b53bb10b0a5c299a8935b362a5a0713
    Former-commit-id: 63ba910a632255a0b86a8af04f24c8b259f8b0cb

--- plugin/dapp/blackwhite/types/const.go
+++ plugin/dapp/blackwhite/types/const.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"reflect"
+
 	"github.com/inconshreveable/log15"
 	"gitlab.33.cn/chain33/chain33/types"
 )
@@ -12,6 +14,16 @@ const (
 	BlackwhiteStatusShow
 	BlackwhiteStatusTimeout
 	BlackwhiteStatusDone
+)
+
+const (
+	// log for blackwhite game
+	TyLogBlackwhiteCreate   = 750
+	TyLogBlackwhitePlay     = 751
+	TyLogBlackwhiteShow     = 752
+	TyLogBlackwhiteTimeout  = 753
+	TyLogBlackwhiteDone     = 754
+	TyLogBlackwhiteLoopInfo = 755
 )
 
 const (
@@ -32,6 +44,15 @@ var (
 		"Play":        BlackwhiteActionPlay,
 		"Show":        BlackwhiteActionShow,
 		"TimeoutDone": BlackwhiteActionTimeoutDone,
+	}
+
+	logInfo = map[int64]*types.LogInfo{
+		TyLogBlackwhiteCreate:   &types.LogInfo{reflect.TypeOf(ReceiptBlackwhite{}), "LogBlackwhiteCreate"},
+		TyLogBlackwhitePlay:     &types.LogInfo{reflect.TypeOf(ReceiptBlackwhite{}), "LogBlackwhitePlay"},
+		TyLogBlackwhiteShow:     &types.LogInfo{reflect.TypeOf(ReceiptBlackwhite{}), "LogBlackwhiteShow"},
+		TyLogBlackwhiteTimeout:  &types.LogInfo{reflect.TypeOf(ReceiptBlackwhite{}), "LogBlackwhiteTimeout"},
+		TyLogBlackwhiteDone:     &types.LogInfo{reflect.TypeOf(ReceiptBlackwhite{}), "LogBlackwhiteDone"},
+		TyLogBlackwhiteLoopInfo: &types.LogInfo{reflect.TypeOf(ReplyLoopResults{}), "LogBlackwhiteLoopInfo"},
 	}
 )
 
