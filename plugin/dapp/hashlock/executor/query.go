f6102bdf1ebf81e9f604d938c863ff8693d0e3c3
sanghg <shg@disanbo.com>
2018-10-08 10:18:59 +0800

    完成对hashlock执行器模块的重构
    Former-commit-id: d291706a5d6b5d4c7a815510ef578eb78430b7f8

--- plugin/dapp/hashlock/executor/query.go
+++ plugin/dapp/hashlock/executor/query.go
@@ -3,5 +3,7 @@ package executor
 import "gitlab.33.cn/chain33/chain33/types"
 
 func (h *Hashlock) Query_GetHashlocKById(in []byte) (types.Message, error) {
-	return nil, nil
+	differTime := types.Now().UnixNano()/1e9 - h.GetBlockTime()
+	clog.Error("Query action")
+	return h.GetTxsByHashlockID(in, differTime)
 }
