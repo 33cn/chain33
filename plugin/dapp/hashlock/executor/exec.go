0761952930e1b8629d6f40f36ec92a0db9c8fed8
sanghg <shg@disanbo.com>
2018-10-08 15:08:01 +0800

    修复hashlock的执行错误问题
    Former-commit-id: ae0ff5c5ecb8d964cace625525b9a1e7a2462da1
    Former-commit-id: 66ebfe54400b58d8f8f013f258a81d3271ff2aa8

--- plugin/dapp/hashlock/executor/exec.go
+++ plugin/dapp/hashlock/executor/exec.go
@@ -34,16 +34,16 @@ func (h *Hashlock) Exec_Hlock(hlock *pty.HashlockLock, tx *types.Transaction, in
 	return actiondb.Hashlocklock(hlock)
 }
 
-func (h *Hashlock) Exec_Hsend(transfer *pty.HashlockUnlock, tx *types.Transaction, index int) (*types.Receipt, error) {
+func (h *Hashlock) Exec_Hsend(transfer *pty.HashlockSend, tx *types.Transaction, index int) (*types.Receipt, error) {
 	//unlock 有两个条件： 1. 时间已经过期 2. 密码是对的，返回原来的账户
 	clog.Debug("hashlockunlock action")
 	actiondb := NewAction(h, tx, drivers.ExecAddress(string(tx.Execer)))
-	return actiondb.Hashlockunlock(transfer)
+	return actiondb.Hashlocksend(transfer)
 }
 
-func (h *Hashlock) Exec_Hunlock(transfer *pty.HashlockSend, tx *types.Transaction, index int) (*types.Receipt, error) {
+func (h *Hashlock) Exec_Hunlock(transfer *pty.HashlockUnlock, tx *types.Transaction, index int) (*types.Receipt, error) {
 	//send 有两个条件：1. 时间没有过期 2. 密码是对的，币转移到 ToAddress
 	clog.Debug("hashlocksend action")
 	actiondb := NewAction(h, tx, drivers.ExecAddress(string(tx.Execer)))
-	return actiondb.Hashlocksend(transfer)
+	return actiondb.Hashlockunlock(transfer)
 }
