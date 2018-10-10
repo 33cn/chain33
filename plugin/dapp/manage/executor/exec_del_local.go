bcceb0fb25150d38d80e5baa688daa15abc6ef14
sanghg <shg@disanbo.com>
2018-10-10 14:38:51 +0800

    重构晚manage，添加对manage的测试用例
    Former-commit-id: e6bc1719951668db962ca16b050b448a52e59c49

---
+++ plugin/dapp/manage/executor/exec_del_local.go
@@ -0,0 +1,28 @@
+package executor
+
+import (
+	pty "gitlab.33.cn/chain33/chain33/plugin/dapp/manage/types"
+	"gitlab.33.cn/chain33/chain33/types"
+)
+
+func (c *Manage) ExecDelLocal_Modify(transfer *types.ModifyConfig, tx *types.Transaction, receipt *types.ReceiptData, index int) (*types.LocalDBSet, error) {
+	set := &types.LocalDBSet{}
+	if receipt.GetTy() != types.ExecOk {
+		return set, nil
+	}
+
+	for i := 0; i < len(receipt.Logs); i++ {
+		item := receipt.Logs[i]
+		if item.Ty == pty.ManageActionModifyConfig {
+			var receipt types.ReceiptConfig
+			err := types.Decode(item.Log, &receipt)
+			if err != nil {
+				panic(err) //数据错误了，已经被修改了
+			}
+			key := receipt.Current.Key
+			set.KV = append(set.KV, &types.KeyValue{Key: []byte(key), Value: types.Encode(receipt.Prev)})
+			clog.Debug("ExecDelLocal to savelogs", "config ", key, "value", receipt.Prev)
+		}
+	}
+	return set, nil
+}
