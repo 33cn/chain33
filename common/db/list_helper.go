282bdf6c01340f0049bed1d606c9be592dc0588e
张振华 <zzh@33.cn>
2018-10-11 17:28:39 +0800

    code
    Former-commit-id: 6a3f0595a4aa2a4d71fd64b77d1231b70b2d3864
    Former-commit-id: d371e3d63c1342b1e6c2719965f9846e13a58cf3

--- common/db/list_helper.go
+++ common/db/list_helper.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 
 	log "github.com/inconshreveable/log15"
+	"fmt"
 )
 
 type ListHelper struct {
@@ -107,7 +108,7 @@ func (db *ListHelper) IteratorScanFromFirst(prefix []byte, count int32) (values 
 			values = nil
 			return
 		}
-		// blog.Debug("PrefixScan", "key", string(item.Key()), "value", value)
+		 blog.Debug("PrefixScan", "key", string(it.Key()), "value", value)
 		values = append(values, value)
 		i++
 		if i == count {
@@ -151,6 +152,95 @@ func (db *ListHelper) PrefixCount(prefix []byte) (count int64) {
 		}
 
 		count++
+	}
+	return
+}
+
+func (db *ListHelper) ListExcept(expectPrefix []byte, exceptPrefix []byte, count int32, direction int32) (values [][]byte) {
+	var reserse = false
+	if direction == 0 {
+		reserse = true
+	}
+	it := db.db.Iterator(expectPrefix, reserse)
+	defer it.Close()
+
+	var i int32
+	for it.Rewind(); it.Valid(); it.Next() {
+		value := it.ValueCopy()
+		if it.Error() != nil {
+			listlog.Error("PrefixScan it.Value()", "error", it.Error())
+			values = nil
+			fmt.Println("nil")
+			return
+		}
+
+		if bytes.Compare(it.Key(), exceptPrefix) >= 0 {
+			fmt.Println(string(it.Key()), "bigger than ", string(exceptPrefix))
+			continue
+		}else{
+			fmt.Println(string(it.Key()), "less than ", string(exceptPrefix))
+		}
+		// blog.Debug("PrefixScan", "key", string(item.Key()), "value", value)
+		values = append(values, value)
+		i++
+		if i == count {
+			break
+		}
+	}
+	return
+}
+
+func (db *ListHelper) ListExceptAndExcute(expectPrefix []byte, exceptPrefix []byte, count int32, direction int32, fn func(key, value []byte) bool) {
+	var reserse = false
+	if direction == 0 {
+		reserse = true
+	}
+
+	//针对expectPrefix为".-mvcc-.l.mavl-coins-bty-06htvcBNSEA7fZhAdLJphDwQRQJaHpy001"的情况，需要做特殊处理:
+	//先截取最后一个'-'及之前的部分作为前缀扫描，再判断每一个元素的key值是否大于等于expectPrefix，如果是，则进行处理；否则，记录是之前已经处理过的，直接跳过。
+	var it Iterator
+	startKeyFlag := false
+	lastIndexOfDelimiter := bytes.LastIndex(expectPrefix, []byte("-"))
+	if lastIndexOfDelimiter + 1 < len(expectPrefix) {
+		it = db.db.Iterator(expectPrefix[0:lastIndexOfDelimiter + 1], reserse)
+		startKeyFlag = true
+	} else {
+		it = db.db.Iterator(expectPrefix, reserse)
+	}
+
+	defer it.Close()
+
+	var i int32
+
+	for it.Rewind(); it.Valid(); it.Next() {
+		value := it.ValueCopy()
+		if it.Error() != nil {
+			listlog.Error("PrefixScan it.Value()", "error", it.Error())
+			return
+		}
+
+		if bytes.Compare(it.Key(), exceptPrefix) >= 0 {
+			fmt.Println(string(it.Key()), "bigger than ", string(exceptPrefix), " ,so exclude it.")
+			continue
+		}else{
+			fmt.Println(string(it.Key()), "less than ", string(exceptPrefix))
+		}
+
+		if startKeyFlag && bytes.Compare(it.Key(), expectPrefix) < 0 {
+			fmt.Println(string(it.Key()), "less than ", string(expectPrefix), " ,so exclude it.")
+			continue
+		}else{
+			fmt.Println(string(it.Key()), "not less than ", string(expectPrefix))
+		}
+		key := make([]byte, len(it.Key()))
+		copy(key, it.Key())
+		if fn(key, value) {
+			break
+		}
+		i++
+		if i == count {
+			break
+		}
 	}
 	return
 }
