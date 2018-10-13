3e2f1271d7a68f37907882a4c2bd71b4e4fe22f0
wjx <13770935170@163.com>
2018-10-13 10:46:05 +0800

    fix refactor bug

--- plugin/dapp/retrieve/types/const.go
+++ plugin/dapp/retrieve/types/const.go
@@ -4,11 +4,12 @@ import "gitlab.33.cn/chain33/chain33/types"
 
 //retrieve
 const (
-	RetrievePreapre= iota + 1
+	RetrieveBackup = iota + 1
+	RetrievePreapre
 	RetrievePerform
-	RetrieveBackup
 	RetrieveCancel
 )
+
 // retrieve op
 const (
 	RetrieveActionPrepare = 1
@@ -16,16 +17,17 @@ const (
 	RetrieveActionBackup  = 3
 	RetrieveActionCancel  = 4
 )
+
 var (
-	JRPCName        = "Retrieve"
+	JRPCName       = "Retrieve"
 	RetrieveX      = "retrieve"
 	ExecerRetrieve = []byte(RetrieveX)
 
-	actionName       = map[string]int32{
-		"Prepare":  RetrieveActionPrepare,
-		"Perform":  RetrieveActionPerform,
-		"Backup":   RetrieveActionBackup,
-		"Cancel":   RetrieveActionCancel,
+	actionName = map[string]int32{
+		"Prepare": RetrieveActionPrepare,
+		"Perform": RetrieveActionPerform,
+		"Backup":  RetrieveActionBackup,
+		"Cancel":  RetrieveActionCancel,
 	}
 )
 
