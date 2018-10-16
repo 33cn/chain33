c2c8e68b5f4eafdd02acef23da8bbb932025f7ff
pengjun <pengj@disanbo.com>
2018-10-16 10:10:07 +0800

    CHAIN33-512:fix linter error
    Former-commit-id: 4965bb5a7d2665db63578c1ccf523833652b9c6d
    Former-commit-id: b6a47519dcb4746f5480bd9d4b350ccc55351b0e

--- plugin/dapp/cert/executor/exec_local.go
+++ plugin/dapp/cert/executor/exec_local.go
@@ -8,24 +8,12 @@ import (
 	"gitlab.33.cn/chain33/chain33/types"
 )
 
-func (c *Cert) execLocal(tx *types.Transaction, receipt *types.ReceiptData, index int) (*types.LocalDBSet, error) {
+func (c *Cert) ExecLocal_New(payload *ct.CertNew, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
 	if !authority.IsAuthEnable {
 		clog.Error("Authority is not available. Please check the authority config or authority initialize error logs.")
 		return nil, types.ErrInitializeAuthority
 	}
-
-	set, err := c.DriverBase.ExecLocal(tx, receipt, index)
-	if err != nil {
-		return nil, err
-	}
-	return set, nil
-}
-
-func (c *Cert) ExecLocal_New(payload *ct.CertNew, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	set, err := c.execLocal(tx, receiptData, index)
-	if err != nil {
-		return nil, err
-	}
+	var set types.LocalDBSet
 
 	historityCertdata := &types.HistoryCertStore{}
 	authority.Author.HistoryCertCache.CurHeight = c.GetHeight()
@@ -39,14 +27,15 @@ func (c *Cert) ExecLocal_New(payload *ct.CertNew, tx *types.Transaction, receipt
 	noneCertdata.CurHeigth = 0
 	set.KV = append(set.KV, &types.KeyValue{[]byte("cert_0"), types.Encode(noneCertdata)})
 
-	return set, nil
+	return &set, nil
 }
 
 func (c *Cert) ExecLocal_Update(payload *ct.CertUpdate, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	set, err := c.execLocal(tx, receiptData, index)
-	if err != nil {
-		return nil, err
+	if !authority.IsAuthEnable {
+		clog.Error("Authority is not available. Please check the authority config or authority initialize error logs.")
+		return nil, types.ErrInitializeAuthority
 	}
+	var set types.LocalDBSet
 
 	// 写入上一纪录的next-height
 	key := []byte(fmt.Sprintf("cert_%d", authority.Author.HistoryCertCache.CurHeight))
@@ -61,9 +50,15 @@ func (c *Cert) ExecLocal_Update(payload *ct.CertUpdate, tx *types.Transaction, r
 	authority.Author.HistoryCertCache.ToHistoryCertStore(historityCertdata)
 	setKey := fmt.Sprintf("cert_%d", c.GetHeight())
 	set.KV = append(set.KV, &types.KeyValue{[]byte(setKey), types.Encode(historityCertdata)})
-	return set, nil
+	return &set, nil
 }
 
 func (c *Cert) ExecLocal_Normal(payload *ct.CertNormal, tx *types.Transaction, receiptData *types.ReceiptData, index int) (*types.LocalDBSet, error) {
-	return c.execLocal(tx, receiptData, index)
+	if !authority.IsAuthEnable {
+		clog.Error("Authority is not available. Please check the authority config or authority initialize error logs.")
+		return nil, types.ErrInitializeAuthority
+	}
+	var set types.LocalDBSet
+
+	return &set, nil
 }
