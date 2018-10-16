d07953c09e6ddac5ba0c96efc1cdd213014e953a
king wang <kingwang@kingdeMacBook-Pro.local>
2018-10-16 23:01:41 +0800

    rpc test pass
    Former-commit-id: a4011fce5b14417811fb9077ed7025a70dee1f49
    Former-commit-id: a91c2e6f86f8a4b626b2e877892dd95bd7466a5c

--- plugin/dapp/privacy/rpc/rpc_test.go
+++ plugin/dapp/privacy/rpc/rpc_test.go
@@ -1,4 +1,45 @@
 package rpc
 
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"gitlab.33.cn/chain33/chain33/client/mocks"
+	pty "gitlab.33.cn/chain33/chain33/plugin/dapp/privacy/types"
+	rpctypes "gitlab.33.cn/chain33/chain33/rpc/types"
+)
+
 //todo: privacy rpc test
 //后面可以用我们的mock node 来做测试
+func newTestChannelClient() *channelClient {
+	api := &mocks.QueueProtocolAPI{}
+	return &channelClient{
+		ChannelClient: rpctypes.ChannelClient{QueueProtocolAPI: api},
+	}
+}
+
+func newTestJrpcClient() *Jrpc {
+	return &Jrpc{cli: newTestChannelClient()}
+}
+
+func newTestJrpcClient2(api *mocks.QueueProtocolAPI) *Jrpc {
+	return &Jrpc{cli: &channelClient{
+		ChannelClient: rpctypes.ChannelClient{QueueProtocolAPI: api},
+	}}
+}
+
+func TestChain33_PrivacyTxList(t *testing.T) {
+	api := new(mocks.QueueProtocolAPI)
+	testChain33 := newTestJrpcClient2(api)
+	actual := &pty.ReqPrivacyTransactionList{}
+	api.On("ExecWalletFunc", "privacy", "PrivacyTransactionList", actual).Return(nil, errors.New("error value"))
+	var testResult interface{}
+	err := testChain33.PrivacyTxList(actual, &testResult)
+	t.Log(err)
+	assert.Equal(t, nil, testResult)
+	assert.NotNil(t, err)
+
+	mock.AssertExpectationsForObjects(t, api)
+}
