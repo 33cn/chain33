8db239136a729fdb0869e327eff0f514a3017d8b
林敬 <linj@disanbo.com>
2018-10-17 17:07:30 +0800

    impl paracross rpc query

--- plugin/dapp/paracross/rpc/rpc.go
+++ plugin/dapp/paracross/rpc/rpc.go
@@ -5,7 +5,6 @@ import (
 	pt "gitlab.33.cn/chain33/chain33/plugin/dapp/paracross/types"
 )
 
-
 func (c *channelClient) GetHeight(req *types.ReqString) (*pt.ParacrossStatus, error) {
 	in := &types.Query{
 		Execer: []byte(pt.GetExecName()),
@@ -21,6 +20,7 @@ func (c *channelClient) GetHeight(req *types.ReqString) (*pt.ParacrossStatus, er
 	}
 	return nil, types.ErrDecode
 }
+
 func (c *Jrpc) GetHeight(req *types.ReqString, result *interface{}) error {
 	if req == nil {
 		return types.ErrInvalidParam
@@ -30,16 +30,75 @@ func (c *Jrpc) GetHeight(req *types.ReqString, result *interface{}) error {
 	return err
 }
 
-// TODO
-func (c *Jrpc) ListTitles() {
-
+func (c *channelClient) ListTitles(req *types.ReqNil) (*pt.RespParacrossTitles, error) {
+	in := &types.Query{
+		Execer: []byte(pt.GetExecName()),
+		FuncName: "ListTitles",
+		Payload: nil,
+	}
+	data, err := c.Query(in)
+	if err != nil {
+		return nil, err
+	}
+	if resp, ok := data.(*pt.RespParacrossTitles); ok {
+		return resp, nil
+	}
+	return nil, types.ErrDecode
 }
 
-func (c *Jrpc) GetTitleHeight() {
-
+func (c *Jrpc) ListTitles(req *types.ReqNil, result *interface{}) error {
+	data, err := c.cli.ListTitles(req)
+	*result = data
+	return err
 }
 
-func (c *Jrpc) GetAssetTxResult() {
+func (c *channelClient) GetTitleHeight(req *pt.ReqParacrossTitleHeight) (*pt.RespParacrossTitles, error) {
+	in := &types.Query{
+		Execer: []byte(pt.GetExecName()),
+		FuncName: "GetTitleHeight",
+		Payload: nil,
+	}
+	data, err := c.Query(in)
+	if err != nil {
+		return nil, err
+	}
+	if resp, ok := data.(*pt.RespParacrossTitles); ok {
+		return resp, nil
+	}
+	return nil, types.ErrDecode
+}
 
+func (c *Jrpc) GetTitleHeight(req *pt.ReqParacrossTitleHeight, result *interface{}) error {
+	if req == nil {
+		return types.ErrInvalidParam
+	}
+	data, err := c.cli.GetTitleHeight(req)
+	*result = data
+	return err
+}
+
+func (c *channelClient) GetAssetTxResult(req *types.ReqHash) (*pt.ParacrossAsset, error) {
+	in := &types.Query{
+		Execer: []byte(pt.GetExecName()),
+		FuncName: "GetAssetTxResult",
+		Payload: nil,
+	}
+	data, err := c.Query(in)
+	if err != nil {
+		return nil, err
+	}
+	if resp, ok := data.(*pt.ParacrossAsset); ok {
+		return resp, nil
+	}
+	return nil, types.ErrDecode
+}
+
+func (c *Jrpc) GetAssetTxResult(req *types.ReqHash, result *interface{}) error {
+	if req == nil {
+		return types.ErrInvalidParam
+	}
+	data, err := c.cli.GetAssetTxResult(req)
+	*result = data
+	return err
 }
 
