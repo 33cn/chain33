6e5a1e73680febb2b2bab53d8f684fde8e6e6142
sanghg <shg@disanbo.com>
2018-10-17 11:46:55 +0800

    继续完善执行器项目工程创建向导
    Former-commit-id: 55cfeca8f49d4aca73e89febda4ce87663961b24
    Former-commit-id: bab60a38d57f924ec6ae4e33699661cd38586731

--- cmd/exec_creator_guider/tasks/check_file_existed_task.go
+++ cmd/exec_creator_guider/tasks/check_file_existed_task.go
@@ -1,5 +1,9 @@
 package tasks
 
+import (
+	"gitlab.33.cn/chain33/chain33/util"
+)
+
 // CheckFileExistedTask 检测文件是否存在
 type CheckFileExistedTask struct {
 	TaskBase
@@ -8,6 +12,9 @@ type CheckFileExistedTask struct {
 
 func (this *CheckFileExistedTask) Execute() error {
 	mlog.Info("Execute file existed task.", "file", this.FileName)
-
+	_, err := util.CheckFileExists(this.FileName)
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- cmd/exec_creator_guider/tasks/comile_protofile_task.go
+++ cmd/exec_creator_guider/tasks/comile_protofile_task.go
@@ -1,5 +1,12 @@
 package tasks
 
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // CompileProtoFileTask 将用户编写的proto文件通过工具编译成pb.go
 type CompileProtoFileTask struct {
 	TaskBase
@@ -8,5 +15,19 @@ type CompileProtoFileTask struct {
 
 func (this *CompileProtoFileTask) Execute() error {
 	mlog.Info("Execute compile protobuf file task.", "file", this.FileName)
-	return nil
+	cmd := exec.Command("protoc", "--proto_path=.", "--go_out=.", this.FileName)
+	var outBuf bytes.Buffer
+	var errBuf bytes.Buffer
+	cmd.Stdout = &outBuf
+	cmd.Stderr = &errBuf
+	err := cmd.Run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	if errBuf.String() != "" {
+		mlog.Error("CompileProtoFileTask Command error", "error", errBuf.String())
+	} else {
+		mlog.Info("CompileProtoFileTask success")
+	}
+	return err
 }
