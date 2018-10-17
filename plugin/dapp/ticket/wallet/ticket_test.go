330d19ed586e3a868dcb852384f172b26cc32449
king wang <kingwang@kingdeMacBook-Pro.local>
2018-10-17 14:35:43 +0800

    ticket wallet build pass
    Former-commit-id: c744079a3f5ee9be45418d8d9d4ef99ec36eda86

--- plugin/dapp/ticket/wallet/ticket_test.go
+++ plugin/dapp/ticket/wallet/ticket_test.go
@@ -1,9 +1,7 @@
-package wallet
+package wallet_test
 
 import (
-	"fmt"
 	"math/rand"
-	"sync"
 	"testing"
 	"time"
 
@@ -13,18 +11,18 @@ import (
 	"gitlab.33.cn/chain33/chain33/common"
 	"gitlab.33.cn/chain33/chain33/common/config"
 	"gitlab.33.cn/chain33/chain33/common/crypto"
-	"gitlab.33.cn/chain33/chain33/common/db"
 	"gitlab.33.cn/chain33/chain33/common/log"
 	"gitlab.33.cn/chain33/chain33/consensus"
 	"gitlab.33.cn/chain33/chain33/executor"
 	"gitlab.33.cn/chain33/chain33/mempool"
 	"gitlab.33.cn/chain33/chain33/p2p"
+	_ "gitlab.33.cn/chain33/chain33/plugin"
 	tickettypes "gitlab.33.cn/chain33/chain33/plugin/dapp/ticket/types"
 	"gitlab.33.cn/chain33/chain33/queue"
 	"gitlab.33.cn/chain33/chain33/store"
 	_ "gitlab.33.cn/chain33/chain33/system"
 	"gitlab.33.cn/chain33/chain33/types"
-	wcom "gitlab.33.cn/chain33/chain33/wallet/common"
+	"gitlab.33.cn/chain33/chain33/wallet"
 )
 
 var (
@@ -45,228 +43,6 @@ func init() {
 	for _, key := range strPrivKeys {
 		privKeys = append(privKeys, getprivkey(key))
 	}
-}
-
-type walletMock struct {
-	client   queue.Client
-	api      client.QueueProtocolAPI
-	db       db.DB
-	reporter wcom.MineStatusReport
-	cfg      *types.Wallet
-	wg       sync.WaitGroup
-	done     chan struct{}
-	mtx      sync.Mutex
-
-	walletStatus       bool
-	walletAccountStore []*types.WalletAccountStore
-}
-
-func (mock *walletMock) init(cfg *types.Wallet) {
-	mock.cfg = cfg
-	mock.done = make(chan struct{})
-	mock.db = db.NewDB("wallet", cfg.Driver, cfg.DbPath, cfg.DbCache)
-
-	wcom.RegisterMsgFunc(types.EventWalletImportprivkey, mock.onWalletImportprivkey)
-	wcom.RegisterMsgFunc(types.EventAddBlock, mock.onAddBlock)
-	wcom.RegisterMsgFunc(types.EventGetWalletStatus, mock.onGetWalletStatus)
-	wcom.RegisterMsgFunc(types.EventWalletGetTickets, mock.onWalletGetTickets)
-}
-
-func (mock *walletMock) processMessage() {
-	defer mock.wg.Done()
-	for msg := range mock.client.Recv() {
-		funcExisted, topic, retty, reply, err := wcom.ProcessFuncMap(&msg)
-		if funcExisted {
-			if err != nil {
-				msg.Reply(mock.api.NewMessage(topic, retty, err))
-			} else {
-				msg.Reply(mock.api.NewMessage(topic, retty, reply))
-			}
-		} else {
-			fmt.Println("msg ty ", msg.Ty)
-		}
-	}
-}
-
-func (mock *walletMock) onWalletImportprivkey(msg *queue.Message) (string, int64, interface{}, error) {
-	topic := "rpc"
-	retty := int64(types.EventWalletImportprivkey)
-
-	return topic, retty, &types.WalletAccount{
-		Label: "no award",
-		Acc: &types.Account{
-			Currency: 1000000000,
-			Balance:  1000000000,
-		},
-	}, nil
-}
-
-func (mock *walletMock) onAddBlock(msg *queue.Message) (string, int64, interface{}, error) {
-	topic := "rpc"
-	retty := int64(types.EventAddBlock)
-
-	return topic, retty, nil, nil
-}
-
-func (mock *walletMock) onGetWalletStatus(msg *queue.Message) (string, int64, interface{}, error) {
-	topic := "rpc"
-	retty := int64(types.EventGetWalletStatus)
-
-	return topic, retty, &types.WalletStatus{
-		IsWalletLock: false,
-		IsAutoMining: true,
-		IsHasSeed:    true,
-		IsTicketLock: false,
-	}, nil
-}
-
-func (mock *walletMock) getTicketList() *tickettypes.ReplyTicketList {
-	reqaddr := &tickettypes.TicketList{"12qyocayNF7Lv6C9qW4avxs2E7U41fKSfv", 1}
-	var req types.Query
-	req.Execer = []byte("ticket")
-	req.FuncName = "TicketList"
-	req.Payload = types.Encode(reqaddr)
-	msg, _ := mock.api.Query(&req)
-	return msg.(*tickettypes.ReplyTicketList)
-}
-
-func (mock *walletMock) onWalletGetTickets(msg *queue.Message) (string, int64, interface{}, error) {
-	topic := "rpc"
-	retty := int64(types.EventWalletTickets)
-	var privKeysdata [][]byte
-	for _, privkey := range privKeys {
-		privKeysdata = append(privKeysdata, privkey.Bytes())
-	}
-	ticklist := mock.getTicketList()
-	ticklist.Tickets = ticklist.Tickets[0:5000]
-	tks := &tickettypes.ReplyWalletTickets{ticklist.Tickets, privKeysdata}
-	return topic, retty, tks, nil
-}
-
-func (mock *walletMock) SetQueueClient(cli queue.Client) {
-	mock.client = cli
-	mock.client.Sub("wallet")
-	mock.api, _ = client.New(cli, nil)
-
-	mock.wg.Add(1)
-	go mock.processMessage()
-}
-
-func (mock *walletMock) Close() {
-
-}
-
-func (mock *walletMock) RegisterMineStatusReporter(reporter wcom.MineStatusReport) error {
-	mock.reporter = reporter
-	return nil
-}
-
-func (mock *walletMock) GetAPI() client.QueueProtocolAPI {
-	panic("implement me")
-}
-
-func (mock *walletMock) GetMutex() *sync.Mutex {
-	return &mock.mtx
-}
-
-func (mock *walletMock) GetDBStore() db.DB {
-	return mock.db
-}
-
-func (mock *walletMock) GetSignType() int {
-	panic("implement me")
-}
-
-func (mock *walletMock) GetPassword() string {
-	panic("implement me")
-}
-
-func (mock *walletMock) GetBlockHeight() int64 {
-	panic("implement me")
-}
-
-func (mock *walletMock) GetRandom() *rand.Rand {
-	panic("implement me")
-}
-
-func (mock *walletMock) GetWalletDone() chan struct{} {
-	return mock.done
-}
-
-func (mock *walletMock) GetLastHeader() *types.Header {
-	panic("implement me")
-}
-
-func (mock *walletMock) GetTxDetailByHashs(ReqHashes *types.ReqHashes) {
-	panic("implement me")
-}
-
-func (mock *walletMock) GetWaitGroup() *sync.WaitGroup {
-	return &mock.wg
-}
-
-func (mock *walletMock) GetAllPrivKeys() ([]crypto.PrivKey, error) {
-	panic("implement me")
-}
-
-func (mock *walletMock) GetWalletAccounts() ([]*types.WalletAccountStore, error) {
-	return mock.walletAccountStore, nil
-}
-
-func (mock *walletMock) GetPrivKeyByAddr(addr string) (crypto.PrivKey, error) {
-	panic("implement me")
-}
-
-func (mock *walletMock) GetConfig() *types.Wallet {
-	return mock.cfg
-}
-
-func (mock *walletMock) GetBalance(addr string, execer string) (*types.Account, error) {
-	panic("implement me")
-}
-
-func (mock *walletMock) IsWalletLocked() bool {
-	panic("implement me")
-}
-
-func (mock *walletMock) IsClose() bool {
-	panic("implement me")
-}
-
-func (mock *walletMock) IsCaughtUp() bool {
-	panic("implement me")
-}
-
-func (mock *walletMock) GetRescanFlag() int32 {
-	panic("implement me")
-}
-
-func (mock *walletMock) SetRescanFlag(flag int32) {
-	panic("implement me")
-}
-
-func (mock *walletMock) CheckWalletStatus() (bool, error) {
-	return mock.walletStatus, nil
-}
-
-func (mock *walletMock) Nonce() int64 {
-	panic("implement me")
-}
-
-func (mock *walletMock) WaitTx(hash []byte) *types.TransactionDetail {
-	panic("implement me")
-}
-
-func (mock *walletMock) WaitTxs(hashes [][]byte) (ret []*types.TransactionDetail) {
-	panic("implement me")
-}
-
-func (mock *walletMock) SendTransaction(payload types.Message, execer []byte, priv crypto.PrivKey, to string) (hash []byte, err error) {
-	panic("implement me")
-}
-
-func (mock *walletMock) SendToAddress(priv crypto.PrivKey, addrto string, amount int64, note string, Istoken bool, tokenSymbol string) (*types.ReplyHash, error) {
-	panic("implement me")
 }
 
 func getprivkey(key string) crypto.PrivKey {
@@ -301,7 +77,7 @@ type chain33Mock struct {
 
 func (mock *chain33Mock) start() {
 	q := queue.New("channel")
-	cfg := config.InitCfg("../../../cmd/chain33/chain33.test.toml")
+	cfg := config.InitCfg("testdata/chain33.test.toml")
 	cfg.Title = "local"
 	cfg.TestNet = true
 	cfg.Consensus.Name = "ticket"
@@ -325,14 +101,10 @@ func (mock *chain33Mock) start() {
 	mock.network.SetQueueClient(q.Client())
 	mock.api, _ = client.New(q.Client(), nil)
 	cli := q.Client()
-	wallet := &walletMock{
-		client: cli,
-		api:    mock.api,
-	}
+
 	mock.client = cli
-	mock.wallet = wallet
+	mock.wallet = wallet.New(cfg.Wallet)
 	mock.wallet.SetQueueClient(cli)
-	wallet.init(cfg.Wallet)
 }
 
 func (mock *chain33Mock) stop() {
@@ -359,11 +131,7 @@ func (mock *chain33Mock) waitHeight(height int64, t *testing.T) {
 }
 
 func (mock *chain33Mock) getTicketList(addr string, t *testing.T) *tickettypes.ReplyTicketList {
-	var req types.Query
-	req.Execer = types.ExecerTicket
-	req.FuncName = "TicketList"
-	req.Payload = types.Encode(&tickettypes.TicketList{addr, 1})
-	msg, err := mock.api.Query(&req)
+	msg, err := mock.api.Query(types.TicketX, "TicketList", &tickettypes.TicketList{addr, 1})
 	require.Equal(t, err, nil)
 	return msg.(*tickettypes.ReplyTicketList)
 }
