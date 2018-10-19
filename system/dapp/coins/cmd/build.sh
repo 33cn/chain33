6bea128b538e5fbffa72add3ff9ee1af52606085
king wang <kingwang@kingdeMacBook-Pro.local>
2018-10-19 21:58:02 +0800

    change shell scriot
    Former-commit-id: 55cec29a3f9d42f6c28522b95dff56e8823e6214

--- system/dapp/coins/cmd/build.sh
+++ system/dapp/coins/cmd/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && exit && pwd)"
 OUT_DIR=$1
 echo "-----only for test begin-----"
 echo $DIR
