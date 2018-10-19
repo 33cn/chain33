2a79f25f2c321fe73f73cfb126ab22dd54014739
sanghg <shg@disanbo.com>
2018-10-19 18:12:18 +0800

    1. 增加对于type的输出 2. 增加格式化命令 3. 增加模板信息

--- cmd/exec_creator_guider/tasks/create_dappsource_task.go
+++ cmd/exec_creator_guider/tasks/create_dappsource_task.go
@@ -2,12 +2,11 @@ package tasks
 
 import (
 	"fmt"
-	"regexp"
-	"strings"
-	"unicode"
-
 	"github.com/pkg/errors"
 	"gitlab.33.cn/chain33/chain33/authority/utils"
+	"gitlab.33.cn/chain33/chain33/util"
+	"regexp"
+	"strings"
 )
 
 type actionInfoItem struct {
@@ -23,8 +22,12 @@ type CreateDappSourceTask struct {
 	ProjectName        string
 	ClsName            string // 生成源码的类名
 	ActionName         string // 生成源码的Action类名
+	TypeName           string
+	ExecuteName        string
 	ProtoFile          string // 推导的原始proto文件
 	ExecHeaderTempFile string
+	TypeTempFile       string
+	TypeOutputFile     string
 
 	actionInfos           []*actionInfoItem // Action中的成员变量名称PB格式
 	execHeaderTempContent string
@@ -45,6 +48,9 @@ func (this *CreateDappSourceTask) Execute() error {
 		return err
 	}
 	if err := this.createExecDelLocalFile(); err != nil {
+		return err
+	}
+	if err := this.createTypeExecuteFile(); err != nil {
 		return err
 	}
 	return nil
@@ -93,10 +99,8 @@ func (this *CreateDappSourceTask) readActionMemberNames() error {
 	for _, member := range members {
 		memberType := strings.Replace(member[1], " ", "", -1)
 		memberName := strings.Replace(member[2], " ", "", -1)
-		tmp := []rune(memberName)
 		// 根据proto生成pb.go的规则，成员变量首字母必须大写
-		tmp[0] = unicode.ToUpper(tmp[0])
-		memberName = string(tmp)
+		memberName, _ = util.MakeStringToUpper(memberName, 0, 1)
 		this.actionInfos = append(this.actionInfos, &actionInfoItem{
 			memberName: memberName,
 			memberType: memberType,
@@ -162,5 +166,95 @@ func (this *CreateDappSourceTask) createExecDelLocalFile() error {
 		mlog.Error(fmt.Sprintf("Write to file %s failed. error %q", fileName, err))
 		return err
 	}
+	return nil
+}
+
+// 组成规则是 TyLog+ActionName + ActionMemberName
+func (this *CreateDappSourceTask) buildActionLogTypeText() (text string, err error) {
+	items := fmt.Sprintf("TyLog%sUnknown = iota\r\n", this.ExecuteName)
+	for _, info := range this.actionInfos {
+		items += fmt.Sprintf("TyLog%s%s\r\n", this.ExecuteName, info.memberName)
+	}
+	text = fmt.Sprintf("const (\r\n%s)\r\n", items)
+	return
+}
+
+// 组成规则是 ActionName + ActionMemberName
+func (this *CreateDappSourceTask) buildActionIDText() (text string, err error) {
+	var items string
+	for index, info := range this.actionInfos {
+		items += fmt.Sprintf("%sAction%s = %d\r\n", this.ExecuteName, info.memberName, index)
+	}
+	text = fmt.Sprintf("const (\r\n%s)\r\n", items)
+	return
+}
+
+// 返回 map[int64]*types.LogInfo
+func (this *CreateDappSourceTask) buildLogMapText() (text string, err error) {
+	var items string
+	for _, info := range this.actionInfos {
+		items += fmt.Sprintf("\"%s\": %sAction%s,\r\n", info.memberName, this.ExecuteName, info.memberName)
+	}
+	text = fmt.Sprintf("map[string]int32{\r\n%s}", items)
+	return
+}
+
+// 返回 map[string]*types.LogInfo
+func (this *CreateDappSourceTask) buidTypeMapText() (text string, err error) {
+	text = fmt.Sprintf("map[int64]*types.LogInfo{\r\n}")
+	return
+}
+
+/**
+createTypeExecuteFile 根据自己的需求，创建一个types中与执行器同名的Type对照关系
+需要处理的内容：
+1. 定义TyLogXXXX的常量，规则是 TyLog + 变量名称
+2. 定义类型常量，规则是 ActionName + 变量名称
+3. 实现GetLogMap()
+4. 实现GetTypeMap()
+*/
+func (this *CreateDappSourceTask) createTypeExecuteFile() error {
+	logText, err := this.buildActionLogTypeText() // ${TYLOGACTIONTYPE}
+	if err != nil {
+		return err
+	}
+	actionIDText, err := this.buildActionIDText() // ${ACTIONIDTEXT}
+	if err != nil {
+		return err
+	}
+	logMapText, err := this.buildLogMapText() // ${LOGMAPTEXT}
+	if err != nil {
+		return err
+	}
+	typeMapText, err := this.buidTypeMapText() // ${TYPEMAPTEXT}
+	if err != nil {
+		return err
+	}
+
+	replacePairs := []struct {
+		src string
+		dst string
+	}{
+		{src: "${TYLOGACTIONTYPE}", dst: logText},
+		{src: "${ACTIONIDTEXT}", dst: actionIDText},
+		{src: "${LOGMAPTEXT}", dst: logMapText},
+		{src: "${TYPEMAPTEXT}", dst: typeMapText},
+		{src: "${TYPENAME}", dst: this.TypeName},
+		{src: "${EXECUTENAME}", dst: this.ExecuteName},
+		{src: "${ACTIONNAME}", dst: this.ActionName},
+	}
+	bcontent, err := utils.ReadFile(this.TypeTempFile)
+	if err != nil {
+		return err
+	}
+	content := string(bcontent)
+	for _, pair := range replacePairs {
+		content = strings.Replace(content, pair.src, pair.dst, -1)
+	}
+	fileName := fmt.Sprintf("%s%s.go", this.TypeOutputFile, this.ClsName)
+	utils.DeleteFile(fileName)
+	_, err = utils.WriteStringToFile(fileName, content)
+	return err
+
 	return nil
 }
