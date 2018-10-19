4f6a2d9576bce6b1d8228881a16ef24051a06d6e
king wang <kingwang@kingdeMacBook-Pro.local>
2018-10-19 22:21:39 +0800

    fix linter

--- plugin/dapp/relay/cmd/build.sh
+++ plugin/dapp/relay/cmd/build.sh
@@ -2,6 +2,7 @@
 
 OUT_DIR=$1
 SRC_RELAYD=./relayd
+FLAG=$2
 
-go build -i ${2} -v -o "${OUT_DIR}/relayd" "${SRC_RELAYD}"
-cp ${SRC_RELAYD}/relayd.toml "${OUT_DIR}"
+go build -i ${FLAG} -v -o "${OUT_DIR}/relayd" "${SRC_RELAYD}"
+cp "${SRC_RELAYD}/relayd.toml" "${OUT_DIR}"
