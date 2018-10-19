8eb955e5aaeb9b6fa1dcd10bff9dad44aba3d4c5
gitlab <gitlab@hzfzm.cn>
2018-10-19 18:13:56 +0800

    auto ci
    Former-commit-id: 5eca9033ed0832de15e04f4d7a302416f68d7bd2

--- util/util.go
+++ util/util.go
@@ -2,9 +2,10 @@ package util
 
 import (
 	"fmt"
-	"github.com/pkg/errors"
 	"strings"
 	"unicode"
+
+	"github.com/pkg/errors"
 )
 
 func GetRealExecName(paraName string, name string) string {
@@ -17,12 +18,12 @@ func GetRealExecName(paraName string, name string) string {
 // MakeStringToUpper 将给定的in字符串从pos开始一共count个转换为大写字母
 func MakeStringToUpper(in string, pos, count int) (out string, err error) {
 	l := len(in)
-	if pos<0 || pos >= l || (pos+count)>=l {
+	if pos < 0 || pos >= l || (pos+count) >= l {
 		err = errors.New(fmt.Sprintf("Invalid params. in=%s pos=%d count=%d", in, pos, count))
 		return
 	}
 	tmp := []rune(in)
-	for n:=pos; n<pos+count; n++ {
+	for n := pos; n < pos+count; n++ {
 		tmp[n] = unicode.ToUpper(tmp[n])
 	}
 	out = string(tmp)
@@ -32,12 +33,12 @@ func MakeStringToUpper(in string, pos, count int) (out string, err error) {
 // MakeStringToLower 将给定的in字符串从pos开始一共count个转换为小写字母
 func MakeStringToLower(in string, pos, count int) (out string, err error) {
 	l := len(in)
-	if pos<0 || pos >= l || (pos+count)>=l {
+	if pos < 0 || pos >= l || (pos+count) >= l {
 		err = errors.New(fmt.Sprintf("Invalid params. in=%s pos=%d count=%d", in, pos, count))
 		return
 	}
 	tmp := []rune(in)
-	for n:=pos; n<pos+count; n++ {
+	for n := pos; n < pos+count; n++ {
 		tmp[n] = unicode.ToLower(tmp[n])
 	}
 	out = string(tmp)
