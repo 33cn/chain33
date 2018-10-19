95ec5265ee57dcc8354c4f05049ec5a6cc45aa43
pengjun <pengj@disanbo.com>
2018-10-19 14:33:19 +0800

    fixbug-CHAIN33-512:fix pokerbull query error

--- plugin/dapp/pokerbull/types/const.go
+++ plugin/dapp/pokerbull/types/const.go
@@ -20,7 +20,7 @@ const (
 //建议用github的组织名称，或者用户名字开头, 再加上自己的插件的名字
 //如果发生重名，可以通过配置文件修改这些名字
 var (
-	JRPCName        = "PokerBull"
+	JRPCName        = "pokerbull"
 	PokerBullX      = "pokerbull"
 	ExecerPokerBull = []byte(PokerBullX)
 )
