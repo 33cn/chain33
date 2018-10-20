0f6f4f9ea6b94beb4ea3efff0d5988c7b1dc48b0
madengji <mdj@disanbo.com>
2018-10-20 16:50:25 +0800

    modify relayd build cmd
    Former-commit-id: 159824044366e9ef929220cba5c3629384697022

--- plugin/dapp/relay/cmd/build.sh
+++ plugin/dapp/relay/cmd/build.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 OUT_DIR=$1
-SRC_RELAYD=./relayd
+SRC_RELAYD=gitlab.33.cn/chain33/chain33/plugin/dapp/relay/cmd/relayd
 FLAG=$2
 
 # shellcheck disable=SC2086
 go build -i ${FLAG} -v -o "${OUT_DIR}/relayd" "${SRC_RELAYD}"
-cp "${SRC_RELAYD}/relayd.toml" "${OUT_DIR}"
+cp "./relayd/relayd.toml" "${OUT_DIR}"
