b952e746c3ae4f6029e70ac6df966f38031c7991
king wang <kingwang@kingdeMacBook-Pro.local>
2018-10-22 01:21:56 +0800

    ticket node
    Former-commit-id: 0553590c4d657fa5c54705126112ee546171fc6f

--- plugin/consensus/ticket/ticket_test.go
+++ plugin/consensus/ticket/ticket_test.go
@@ -4,25 +4,17 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"gitlab.33.cn/chain33/chain33/common/limits"
-	"gitlab.33.cn/chain33/chain33/common/log"
 	_ "gitlab.33.cn/chain33/chain33/plugin/dapp/init"
 	_ "gitlab.33.cn/chain33/chain33/plugin/store/init"
 	_ "gitlab.33.cn/chain33/chain33/system"
 	"gitlab.33.cn/chain33/chain33/util/testnode"
 )
 
-func init() {
-	err := limits.SetLimits()
-	if err != nil {
-		panic(err)
-	}
-	log.SetLogLevel("info")
-}
-
 // 执行： go test -cover
 func TestTicket(t *testing.T) {
-	mock33 := testnode.New("", nil)
+	cfg, sub := testnode.GetDefaultConfig()
+	cfg.Consensus.Name = "ticket"
+	mock33 := testnode.NewWithConfig(cfg, sub, nil)
 	defer mock33.Close()
 	err := mock33.WaitHeight(2)
 	assert.Nil(t, err)
