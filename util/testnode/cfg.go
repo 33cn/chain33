fe9ff51e4cc3cdbea40a0deca04195167a7c3523
king wang <kingwang@kingdeMacBook-Pro.local>
2018-10-22 21:17:57 +0800

    fix test error
    Former-commit-id: 3038844f8d5163e3b31a7b673739e2c5dbfe6eaf
    Former-commit-id: c6d9e1364edc2bfb820f822a4750a633ee622a32

--- util/testnode/cfg.go
+++ util/testnode/cfg.go
@@ -70,11 +70,11 @@ name="solo"
 minerstart=true
 genesisBlockTime=1514533394
 genesis="14KEKbYtKKQm4wMthSK9J4La4nAiidGozt"
+hotkeyAddr="12qyocayNF7Lv6C9qW4avxs2E7U41fKSfv"
 
 [consensus.sub.solo]
 genesis="14KEKbYtKKQm4wMthSK9J4La4nAiidGozt"
 genesisBlockTime=1514533394
-hotkeyAddr="12qyocayNF7Lv6C9qW4avxs2E7U41fKSfv"
 
 [consensus.sub.ticket]
 genesisBlockTime=1514533394
@@ -114,8 +114,8 @@ signType="secp256k1"
 minerwhitelist=["*"]
 
 [exec]
-isFree=false
-minExecFee=100000
+isFree=true
+minExecFee=0
 enableStat=false
 enableMVCC=false
 
