b73c4be60c0959f64cec727682e9edd5d7401121
sanghg <shg@disanbo.com>
2018-10-24 09:53:03 +0800

    移动有关trade执行器模块的测试用例依赖
    Former-commit-id: 2d743457b6719e893c0bc7a5bd8661bbf970384b
    Former-commit-id: c26e649173d1276e38c6ee4cb89532892c073169

--- plugin/dapp/trade/rpc/types.go
+++ plugin/dapp/trade/rpc/types.go
@@ -2,11 +2,12 @@ package rpc
 
 import (
 	ptypes "gitlab.33.cn/chain33/chain33/plugin/dapp/trade/types"
-	"gitlab.33.cn/chain33/chain33/rpc/types"
+	rpctypes "gitlab.33.cn/chain33/chain33/rpc/types"
+	"gitlab.33.cn/chain33/chain33/types"
 )
 
 type channelClient struct {
-	types.ChannelClient
+	rpctypes.ChannelClient
 }
 
 type Jrpc struct {
@@ -17,9 +18,30 @@ type Grpc struct {
 	*channelClient
 }
 
-func Init(name string, s types.RPCServer) {
+func Init(name string, s rpctypes.RPCServer) {
 	cli := &channelClient{}
 	grpc := &Grpc{channelClient: cli}
 	cli.Init(name, s, &Jrpc{cli: cli}, grpc)
 	ptypes.RegisterTradeServer(s.GRPC(), grpc)
+}
+
+func (this *Jrpc) GetLastMemPool(in types.ReqNil, result *interface{}) error {
+	reply, err := this.cli.GetLastMempool()
+	if err != nil {
+		return err
+	}
+
+	{
+		var txlist rpctypes.ReplyTxList
+		txs := reply.GetTxs()
+		for _, tx := range txs {
+			tran, err := rpctypes.DecodeTx(tx)
+			if err != nil {
+				continue
+			}
+			txlist.Txs = append(txlist.Txs, tran)
+		}
+		*result = &txlist
+	}
+	return nil
 }
