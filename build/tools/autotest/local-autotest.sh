e6791b030927f7b468becbde5e8dd51262a0a60a
jiangpeng <jiangp@33.cn>
2018-10-26 15:36:41 +0800

    make linter
    Former-commit-id: f7126d477d04692a8fd609aebfc74af89481bed9
    Former-commit-id: d7798b568144a4d5f59b10f725e5c5b5c2e348da

--- build/tools/autotest/local-autotest.sh
+++ build/tools/autotest/local-autotest.sh
@@ -10,12 +10,10 @@ set -o pipefail
 # sudo apt-get install jq
 # sudo apt-get install shellcheck, in order to static check shell script
 # sudo apt-get install parallel
-# ./run-autotest.sh build
 
 PWD=$(cd "$(dirname "$0")" && pwd)
 export PATH="$PWD:$PATH"
 
-PROJECT_NAME="${1}"
 CLI="./chain33-cli"
 
 sedfix=""
@@ -33,16 +31,24 @@ function init() {
     cp ../../chain33-cli ./
     cp ../../../cmd/chain33/chain33.test.toml ./
 
-
 }
 
 function config_chain33() {
 
+    # shellcheck disable=SC2015
     echo "# config chain33 solo test"
     # update test environment
     sed -i $sedfix 's/^Title.*/Title="local"/g' ${chain33Config}
     # sed -i $sedfix 's/^TestNet=.*/TestNet=true/g' ${chain33Config}
-    grep -q '^TestNet' ${chain33Config} && sed -i $sedfix 's/^TestNet.*/TestNet=true/' ${chain33Config} || sed -i '/^Title/a TestNet=true' ${chain33Config}
+#    grep -q '^TestNet' ${chain33Config} && sed -i $sedfix 's/^TestNet.*/TestNet=true/' ${chain33Config} || sed -i '/^Title/a TestNet=true' ${chain33Config}
+
+    if grep -q '^TestNet' ${chain33Config}; then
+        sed -i $sedfix 's/^TestNet.*/TestNet=true/' ${chain33Config}
+    else
+        sed -i '/^Title/a TestNet=true' ${chain33Config}
+    fi
+
+
 
     #update fee
     sed -i $sedfix 's/Fee=.*/Fee=100000/' ${chain33Config}
@@ -64,17 +70,23 @@ function config_autotest() {
     if [[ $1 == "" ]] || [[ $1 == "all" ]]; then
         cp ${autotestConfig} ${autotestTempConfig}
     else
-         #copy config before [
-        sed -n '/^\[/!p;//q' ${autotestConfig} > ${autotestTempConfig}
+        #copy config before [
+        sed -n '/^\[/!p;//q' ${autotestConfig} >${autotestTempConfig}
 
         #copy specific dapp cofig
 
-        for dapp in "$@"
-        do
-            echo "[[TestCaseFile]]" >> ${autotestTempConfig}
-            echo "contract=\"$dapp\"" >> ${autotestTempConfig}
-            echo "filename=\"$dapp.toml\"" >> ${autotestTempConfig}
-        done
+        for dapp in "$@"; do
+#            echo "[[TestCaseFile]]" >> ${autotestTempConfig}
+#            echo "contract=\"$dapp\"" >> ${autotestTempConfig}
+#            echo "filename=\"$dapp.toml\"" >> ${autotestTempConfig}
+
+            {
+                echo "[[TestCaseFile]]"
+                echo "contract=\"$dapp\""
+                echo "filename=\"$dapp.toml\""
+            } >> ${autotestTempConfig}
+
+       done
     fi
 
     sed -i $sedfix 's/^checkSleepTime.*/checkSleepTime='${chain33BlockTime}'/' ${autotestTempConfig}
@@ -85,7 +97,7 @@ function start_chain33() {
     echo "# start solo chain33, make sure there is no chain33 instance running"
     rm -rf ../autotest/datadir ../autotest/logs
     rm ../autotest/grpc33.log
-    ./chain33 -f chain33.test.toml > /dev/null 2>&1 &
+    ./chain33 -f chain33.test.toml >/dev/null 2>&1 &
 
     local SLEEP=5
     echo "=========== sleep ${SLEEP}s ============="
@@ -93,8 +105,6 @@ function start_chain33() {
 
     # query node run status
     ${CLI} block last_header
-
-
 
     echo "=========== # save seed to wallet ============="
     result=$(${CLI} seed save -p 1314 -s "tortoise main civil member grace happy century convince father cage beach hip maid merry rib" | jq ".isok")
@@ -144,7 +154,6 @@ function start_chain33() {
         exit 1
     fi
 
-
     echo "=========== #transfer to miner addr ============="
     hash=$(${CLI} send coins transfer -a 10000 -n test -t 12qyocayNF7Lv6C9qW4avxs2E7U41fKSfv -k CC38546E9E659D15E6B4893F0AB32A06D103931A8230B0BDE71459D2B27D6944)
 
@@ -154,7 +163,6 @@ function start_chain33() {
         echo "transferTokenAdmin cannot find tx"
         exit 1
     fi
-
 
     echo "=========== #transfer to token amdin ============="
     hash=$(${CLI} send coins transfer -a 10 -n test -t 1Q8hGLfoGe63efeWa8fJ4Pnukhkngt6poK -k CC38546E9E659D15E6B4893F0AB32A06D103931A8230B0BDE71459D2B27D6944)
@@ -170,7 +178,7 @@ function start_chain33() {
     rawData=$(${CLI} config config_tx -k token-blacklist -o add -v BTC)
     signData=$(${CLI} wallet sign -d "${rawData}" -k 0xc34b5d9d44ac7b754806f761d3d4d2c4fe5214f6b074c19f069c4f5c2a29c8cc)
     hash=$(${CLI} wallet send -d "${signData}")
-    
+
     sleep ${chain33BlockTime}
     txs=$(${CLI} tx query_hash -s "${hash}" | jq ".txs")
     if [ "${txs}" == "null" ]; then
@@ -183,14 +191,12 @@ function start_chain33() {
     ${CLI} mempool list
 }
 
-
 function start_autotest() {
 
-    
     echo "=========== #run autotest, make sure saving autotest.log.last file============="
 
     if [ -e autotest.log ]; then
-        cat autotest.log > autotest.log.last
+        cat autotest.log >autotest.log.last
         rm autotest.log
     fi
 
@@ -206,7 +212,7 @@ function stop_chain33() {
 
 function main() {
     echo "==========================================main begin========================================================"
-    config_autotest $@
+    config_autotest "$@"
     init
     config_chain33
     start_chain33
@@ -216,5 +222,4 @@ function main() {
     echo "==========================================main end========================================================="
 }
 
-
-main $@
+main "$@"
