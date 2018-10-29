6465ae8e498577211dc8b029bc10484b02026a87
kingwang <kingwang@kingwangdeiMac.local>
2018-10-29 15:25:36 +0800

    del token name
    Former-commit-id: 2dd44bb76ad388206691538f3c2ee5758c734e63

--- plugin/dapp/trade/executor/kv.go
+++ plugin/dapp/trade/executor/kv.go
@@ -9,18 +9,18 @@ import (
 )
 
 const (
-	sellOrderSHTAS = "LODB-trade-token-sellorder-shtas:"
-	sellOrderASTS  = "LODB-trade-token-sellorder-asts:"
-	sellOrderATSS  = "LODB-trade-token-sellorder-atss:"
-	sellOrderTSPAS = "LODB-trade-oken-sellorder-tspas:"
-	buyOrderSHTAS  = "LODB-trade-token-buyorder-shtas:"
-	buyOrderASTS   = "LODB-trade-token-buyorder-asts:"
-	buyOrderATSS   = "LODB-trade-token-buyorder-atss:"
-	buyOrderTSPAS  = "LODB-trade-token-buyorder-tspas:"
+	sellOrderSHTAS = "LODB-trade-sellorder-shtas:"
+	sellOrderASTS  = "LODB-trade-sellorder-asts:"
+	sellOrderATSS  = "LODB-trade-sellorder-atss:"
+	sellOrderTSPAS = "LODB-trade-sellorder-tspas:"
+	buyOrderSHTAS  = "LODB-trade-buyorder-shtas:"
+	buyOrderASTS   = "LODB-trade-buyorder-asts:"
+	buyOrderATSS   = "LODB-trade-buyorder-atss:"
+	buyOrderTSPAS  = "LODB-trade-buyorder-tspas:"
 	sellIDPrefix   = "mavl-trade-sell-"
 	buyIDPrefix    = "mavl-trade-buy-"
 	// Addr-Status-Type-Height-Key
-	orderASTHK = "LODB-trade-token-order-asthk:"
+	orderASTHK = "LODB-trade-order-asthk:"
 )
 
 // sell order 4 key, 4prefix
