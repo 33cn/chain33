63e0465effadbbb25e1a37eaf6c57fd02b060413
kingwang <kingwang@kingwangdeiMac.local>
2018-10-30 17:51:20 +0800

    add rpc real test
    Former-commit-id: 195624996c4466a628bd15aee866da3699a464ff
    Former-commit-id: fec7db405bd301bef62f4b761420de8e32d0ae66

---
+++ rpc/rpc_real_test.go
@@ -0,0 +1,59 @@
+package rpc_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gitlab.33.cn/chain33/chain33/common"
+	commonlog "gitlab.33.cn/chain33/chain33/common/log"
+	"gitlab.33.cn/chain33/chain33/rpc/jsonclient"
+	rpctypes "gitlab.33.cn/chain33/chain33/rpc/types"
+	"gitlab.33.cn/chain33/chain33/types"
+	"gitlab.33.cn/chain33/chain33/util"
+	"gitlab.33.cn/chain33/chain33/util/testnode"
+
+	_ "gitlab.33.cn/chain33/chain33/plugin"
+	_ "gitlab.33.cn/chain33/chain33/system"
+)
+
+func init() {
+	commonlog.SetLogLevel("error")
+}
+
+func TestErrLog(t *testing.T) {
+	// 启动RPCmocker
+	mocker := testnode.New("--notset--", nil)
+	defer mocker.Close()
+	mocker.Listen()
+
+	rpcCfg := mocker.GetCfg().Rpc
+	jrpcClient, err := jsonclient.NewJSONClient(fmt.Sprintf("http://%s/", rpcCfg.JrpcBindAddr))
+	assert.NoError(t, err)
+	assert.NotNil(t, jrpcClient)
+	gen := mocker.GetGenesisKey()
+	//发送交易到区块链
+	addr1, key1 := util.Genaddress()
+	addr2, _ := util.Genaddress()
+	tx1 := util.CreateCoinsTx(gen, addr1, 1*types.Coin)
+	mocker.GetAPI().SendTx(tx1)
+	mocker.WaitHeight(1)
+
+	tx11 := util.CreateCoinsTx(key1, addr2, 6*int64(1e7))
+	reply, err := mocker.GetAPI().SendTx(tx11)
+	assert.Nil(t, err)
+	assert.Equal(t, reply.GetMsg(), tx11.Hash())
+	tx12 := util.CreateCoinsTx(key1, addr2, 6*int64(1e7))
+	reply, err = mocker.GetAPI().SendTx(tx12)
+	assert.Nil(t, err)
+	assert.Equal(t, reply.GetMsg(), tx12.Hash())
+	mocker.WaitHeight(2)
+	var testResult interface{}
+	req := rpctypes.QueryParm{
+		Hash: common.ToHex(tx12.Hash()),
+	}
+	//query transaction
+	err = jrpcClient.Call("Chain33.QueryTransaction", req, &testResult)
+	assert.Nil(t, err)
+	t.Log(testResult)
+}
