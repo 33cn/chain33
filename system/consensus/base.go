13f9ab9f3870f68e336833292ac49d29ed0887ad
king wang <kingwang@kingdeMacBook-Pro.local>
2018-11-05 12:54:02 +0800

    fix query data bug

--- system/consensus/base.go
+++ system/consensus/base.go
@@ -232,7 +232,7 @@ func (bc *BaseClient) CheckBlock(block *types.BlockDetail) error {
 	if parent.Height+1 != block.Block.Height {
 		return types.ErrBlockHeight
 	}
-	if types.IsFork(block.Block.Height, "ForkCheckBlockTime") && parent.BlockTime >= block.Block.BlockTime {
+	if types.IsFork(block.Block.Height, "ForkCheckBlockTime") && parent.BlockTime > block.Block.BlockTime {
 		return types.ErrBlockTime
 	}
 	//check parent hash
