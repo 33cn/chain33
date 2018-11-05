f808053c123bc0b9f02e6c95c1b64f81d682f5bb
king wang <kingwang@kingdeMacBook-Pro.local>
2018-11-05 13:20:15 +0800

    fix test
    Former-commit-id: 53787c11f5a28ba551f7db61014f408dce797301
    Former-commit-id: 9df44a53ec307be7a259c2433fffde0c256ddf09

--- types/reflect_test.go
+++ types/reflect_test.go
@@ -23,6 +23,7 @@ func TestValue(t *testing.T) {
 	b := 20
 	data := &T{10, &b}
 	qdata.Register("T", data)
+	qdata.SetThis("T", reflect.ValueOf(data))
 
 	reply, err := qdata.Call("T", "Add", &ReqNil{})
 	assert.Nil(t, err)
@@ -60,7 +61,9 @@ func init() {
 	bb := 20
 	data := &T{10, &bb}
 	qdata.Register("TT", data)
+	qdata.SetThis("TT", reflect.ValueOf(data))
 }
+
 func BenchmarkCallQueryData(b *testing.B) {
 	result := int64(0)
 	for i := 0; i < b.N; i++ {
