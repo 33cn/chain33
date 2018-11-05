cf34434fdfedb4808024dcb8b85382e881480f7f
king wang <kingwang@kingdeMacBook-Pro.local>
2018-11-05 23:32:14 +0800

    add mversion support

--- build/util/manage_list.sh
+++ build/util/manage_list.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-guodun="$(../chain33-cli config config_tx -k token-finisher -o add -v 13KDST7ndBmzunGYAkCRabooWGRc95hM3W --paraName "user.p.guodun.")"
-fzmtest="$(../chain33-cli config config_tx -k token-finisher -o add -v 16ReZHzMCGtPt8B7XbnZQ2jeXsPG9wEufh --paraName "user.p.fzmtest.")"
-echo "cli wallet sign -d $guodun -a 1JmFaA6unrCFYEWPGRi7uuXY1KthTJxJEP -e 1h > d:/b.txt"
-echo "cli wallet sign -d $fzmtest -a 1JmFaA6unrCFYEWPGRi7uuXY1KthTJxJEP -e 1h >> d:/b.txt"
+guodun1="$(../chain33-cli config config_tx -k token-finisher -o del -v 13KDST7ndBmzunGYAkCRabooWGRc95hM3W --paraName "user.p.starsunny.")"
+guodun2="$(../chain33-cli config config_tx -k token-finisher -o add -v 1AtQcjpDYaJSxvAE3aacfJUknsFNb7eWLd  --paraName "user.p.starsunny.")"
+echo "cli wallet sign -d $guodun1 -a 1JmFaA6unrCFYEWPGRi7uuXY1KthTJxJEP -e 1h > d:/b.txt"
+echo "cli wallet sign -d $guodun2 -a 1JmFaA6unrCFYEWPGRi7uuXY1KthTJxJEP -e 1h >> d:/b.txt"
