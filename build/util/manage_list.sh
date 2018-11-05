ae090da94295c94936ec49727cc7559b184a6ceb
gitlab <gitlab@hzfzm.cn>
2018-11-05 23:33:48 +0800

    auto ci

--- build/util/manage_list.sh
+++ build/util/manage_list.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 guodun1="$(../chain33-cli config config_tx -k token-finisher -o del -v 13KDST7ndBmzunGYAkCRabooWGRc95hM3W --paraName "user.p.starsunny.")"
-guodun2="$(../chain33-cli config config_tx -k token-finisher -o add -v 1AtQcjpDYaJSxvAE3aacfJUknsFNb7eWLd  --paraName "user.p.starsunny.")"
+guodun2="$(../chain33-cli config config_tx -k token-finisher -o add -v 1AtQcjpDYaJSxvAE3aacfJUknsFNb7eWLd --paraName "user.p.starsunny.")"
 echo "cli wallet sign -d $guodun1 -a 1JmFaA6unrCFYEWPGRi7uuXY1KthTJxJEP -e 1h > d:/b.txt"
 echo "cli wallet sign -d $guodun2 -a 1JmFaA6unrCFYEWPGRi7uuXY1KthTJxJEP -e 1h >> d:/b.txt"
