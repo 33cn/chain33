757fcfad0bbeb68c8317a90f469e9cd75995d4da
sanghg <shg@disanbo.com>
2018-11-06 11:02:59 +0800

    更新文档信息
    Former-commit-id: bd4c5b0e8534c08b69f3f0fa1d67ada566e60e4d
    Former-commit-id: fb8a899b1bba24763fd58178ec3bc2952665e737

--- cmd/tools/tasks/update_initfile_task.go
+++ cmd/tools/tasks/update_initfile_task.go
@@ -45,11 +45,6 @@ func (this *UpdateInitFileTask) Execute() error {
 		}
 	}
 	mlog.Info("Success generate init.go", "filename", this.initFile)
-	// 2. 扫描目标文件夹内一级文件夹名称，记录到一个数组中
-	// 3. 在文件夹内新增init文件夹
-	// 4. 在init文件夹内新增init.go文件
-	// 5. 在init.go中，按照格式将数组的内容填充到文件内
-	// 6. 格式化init.go文件
 	return nil
 }
 
