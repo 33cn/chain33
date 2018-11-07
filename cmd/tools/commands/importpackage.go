233ee481c9e4bc4bd211b43881e6529075fbf001
king wang <kingwang@kingdeMacBook-Pro.local>
2018-11-07 23:54:24 +0800

    update tool
    Former-commit-id: 41ca4e8246d853bee217f81201c3df56753133a8

--- cmd/tools/commands/importpackage.go
+++ cmd/tools/commands/importpackage.go
@@ -14,6 +14,9 @@ func ImportCmd() *cobra.Command {
 		Short: "Import plugin package",
 		Run:   importPackage,
 	}
+	cmd.Flags().StringP("conf", "c", "plugin/plugin.toml", "path of plugin config file")
+	cmd.Flags().StringP("path", "p", "plugin", "path of plugin")
+	cmd.Flags().StringP("packname", "", "", "project package name")
 	return cmd
 }
 
@@ -23,5 +26,11 @@ func importPackage(cmd *cobra.Command, args []string) {
 		fmt.Println(types.KeyImportPackage, "Not support")
 		return
 	}
+	conf, _ := cmd.Flags().GetString("conf")
+	path, _ := cmd.Flags().GetString("path")
+	packname, _ := cmd.Flags().GetString("packname")
+	s.SetParam("path", path)
+	s.SetParam("packname", packname)
+	s.SetParam("conf", conf)
 	s.Run()
 }
