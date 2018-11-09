832b4469d4381f840c12d85e16ae0bf79131b15c
林敬 <linj@disanbo.com>
2018-11-09 13:43:37 +0800

    rpc return asset in tx detail
    Former-commit-id: af2950eeb9a064d036c553db78a76d4e9efae862
    Former-commit-id: c8877f62bcd040ab3b14947e3a925c7f0937a5a6

--- rpc/types/types.go
+++ rpc/types/types.go
@@ -124,6 +124,7 @@ type TransactionDetail struct {
 	Amount     int64              `json:"amount"`
 	Fromaddr   string             `json:"fromAddr"`
 	ActionName string             `json:"actionName"`
+	Assets     []*types.Asset     `json:"assets"`
 }
 
 type ReplyTxInfos struct {
