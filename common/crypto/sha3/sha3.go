a1dd93ebc3dd57b897d11bc132b91dedfd91a199
king wang <kingwang@kingdeMacBook-Pro.local>
2018-11-12 00:57:16 +0800

    add license info
    Former-commit-id: 2cfa6d5e741ea6160b4383ecb5422c94e8947a8d

--- common/crypto/sha3/sha3.go
+++ common/crypto/sha3/sha3.go
@@ -1,3 +1,7 @@
+// Copyright Fuzamei Corp. 2018 All Rights Reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Copyright 2014 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
