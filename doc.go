b6c9bdadf14db86bc31c6f7ee4bd2f8ae72b166c
33cn <462829236@qq.com>
2018-11-12 12:14:24 +0800

    Update doc.go
    Former-commit-id: 34517f183b075364b7a9f049ac025a53b05ce790

--- doc.go
+++ doc.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package chain33
-
 /*
 chain33 是由复杂美科技有限公司开发的区块链链框架
 
@@ -13,6 +11,8 @@ chain33 是由复杂美科技有限公司开发的区块链链框架
 
 3. 创新的 合约 调用和组合方式
 */
+package chain33
+
 //有些包国内需要翻墙才能下载，我们把部分参见的包含在这里
 import (
 	_ "golang.org/x/crypto/nacl/box"
