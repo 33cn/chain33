be07e5c068c8a50f698d5b0484c4b8f7b344f681
vipwzw <vipwzw@vip.qq.com>
2018-11-18 13:47:06 +0800

    fix rpc test
    Former-commit-id: 91e911e8cc4a2c5361dba50d5b21132d86efa425

--- rpc/server_test.go
+++ rpc/server_test.go
@@ -46,13 +46,9 @@ func TestJSONClient_Call(t *testing.T) {
 	rpcCfg.JrpcFuncWhitelist = []string{"*"}
 	rpcCfg.GrpcFuncWhitelist = []string{"*"}
 	InitCfg(rpcCfg)
-	server := NewJSONRPCServer(&qmocks.Client{}, nil)
-	assert.NotNil(t, server)
-
 	api := new(mocks.QueueProtocolAPI)
-	testChain33 := newTestChain33(api)
-	assert.NotNil(t, testChain33)
-	server.jrpc = testChain33
+	server := NewJSONRPCServer(&qmocks.Client{}, api)
+	assert.NotNil(t, server)
 	done := make(chan struct{}, 1)
 	go func() {
 		done <- struct{}{}
@@ -87,7 +83,6 @@ func TestJSONClient_Call(t *testing.T) {
 	err = jsonClient.Call("Chain33.IsSync", &types.ReqNil{}, &isSnyc)
 	assert.Nil(t, err)
 	assert.Equal(t, ret.GetIsOk(), isSnyc)
-	return
 	var nodeInfo rpctypes.NodeNetinfo
 	api.On("GetNetInfo", mock.Anything).Return(&types.NodeNetInfo{Externaladdr: "123"}, nil)
 	err = jsonClient.Call("Chain33.GetNetInfo", &types.ReqNil{}, &nodeInfo)
@@ -128,11 +123,9 @@ func TestGrpc_Call(t *testing.T) {
 	rpcCfg.JrpcFuncWhitelist = []string{"*"}
 	rpcCfg.GrpcFuncWhitelist = []string{"*"}
 	InitCfg(rpcCfg)
-	server := NewGRpcServer(&qmocks.Client{}, nil)
-	assert.NotNil(t, server)
-
 	api := new(mocks.QueueProtocolAPI)
-	server.grpc.cli.QueueProtocolAPI = api
+	server := NewGRpcServer(&qmocks.Client{}, api)
+	assert.NotNil(t, server)
 	go server.Listen()
 	time.Sleep(time.Second)
 	ret := &types.Reply{
