2a1e988ebef69949de1e11422171f40dbb951f3e
vipwzw <vipwzw@vip.qq.com>
2018-11-18 13:38:27 +0800

    auto ci
    Former-commit-id: 001867d39a58cb8c21d03662be6c18aa19ec1ca9

--- build/tools/coverage.sh
+++ build/tools/coverage.sh
@@ -5,7 +5,7 @@ set -e -o pipefail
 
 COVERAGE_DIR="${COVERAGE_DIR:-build/coverage}"
 PKG_LIST=$(go list ./... | grep -v "vendor" | grep -v "mock" | grep -v "mocks" \
-    | grep -v "cmd" | grep -v "types" |grep -v "nat" | grep -v "pbft")
+    | grep -v "cmd" | grep -v "types" | grep -v "nat" | grep -v "pbft")
 
 # Create the coverage files directory
 mkdir -p "$COVERAGE_DIR"
