88384270d2b2c8a651fbf24c6bd9e6ebec1218f1
vipwzw <vipwzw@vip.qq.com>
2018-11-23 18:01:30 +0800

    auto ci

--- golinter.sh
+++ golinter.sh
@@ -5,27 +5,27 @@ set +e
 OP="${1}"
 path="${2}"
 
-function  filterLinter() {
+function filterLinter() {
     res=$(gometalinter.v2 -t --sort=linter --enable-gc --deadline=2m --disable-all \
-	    --enable=gofmt \
-	    --enable=gosimple \
-	    --enable=deadcode \
-	    --enable=unconvert \
-	    --enable=interfacer \
-	    --enable=varcheck \
-	    --enable=structcheck \
-	    --enable=goimports \
-	    --enable=vet \
-	    --vendor ./...)
-#	    --enable=staticcheck \
-#	    --enable=gocyclo \
-#	    --enable=staticcheck \
-#	    --enable=golint \
-#	    --enable=unused \
-#	    --enable=gotype \
-#	    --enable=gotypex \
+        --enable=gofmt \
+        --enable=gosimple \
+        --enable=deadcode \
+        --enable=unconvert \
+        --enable=interfacer \
+        --enable=varcheck \
+        --enable=structcheck \
+        --enable=goimports \
+        --enable=vet \
+        --vendor ./...)
+    #	    --enable=staticcheck \
+    #	    --enable=gocyclo \
+    #	    --enable=staticcheck \
+    #	    --enable=golint \
+    #	    --enable=unused \
+    #	    --enable=gotype \
+    #	    --enable=gotypex \
 
-    if [[ "${#res}" -gt "0" ]]; then
+    if [[ ${#res} -gt "0" ]]; then
         resNoSpace=$(echo "${res}" | tr ' ' '@')
         array=($(echo "${resNoSpace}" | tr '\n' '\n'))
         str=""
@@ -37,28 +37,28 @@ function  filterLinter() {
         res=""
         res=$(echo "${str}" | tr '@' ' ')
     fi
-    if [[ "${#res}" -gt "0" ]]; then \
-	    echo -e "${res}"
-	    exit 1
+    if [[ ${#res} -gt "0" ]]; then
+        echo -e "${res}"
+        exit 1
     fi
 }
 
-function  testLinter() {
-    cd "${path}" > /dev/null || exit
+function testLinter() {
+    cd "${path}" >/dev/null || exit
     gometalinter.v2 -t --sort=linter --enable-gc --deadline=2m --disable-all \
-	    --enable=gofmt \
-	    --enable=gosimple \
-	    --enable=deadcode \
-	    --enable=unconvert \
-	    --enable=interfacer \
-	    --enable=varcheck \
-	    --enable=structcheck \
-	    --enable=goimports \
-	    --enable=vet \
-	    --enable=golint \
-	    --enable=ineffassign \
-	    --vendor ./...
-    cd - > /dev/null || exit
+        --enable=gofmt \
+        --enable=gosimple \
+        --enable=deadcode \
+        --enable=unconvert \
+        --enable=interfacer \
+        --enable=varcheck \
+        --enable=structcheck \
+        --enable=goimports \
+        --enable=vet \
+        --enable=golint \
+        --enable=ineffassign \
+        --vendor ./...
+    cd - >/dev/null || exit
 }
 
 function main() {
