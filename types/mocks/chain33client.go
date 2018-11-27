1011f5ecbbae64103311c32071a6fa3ea923c982
vipwzw <vipwzw@vip.qq.com>
2018-11-27 21:09:26 +0800

    auto ci

--- types/mocks/chain33client.go
+++ types/mocks/chain33client.go
@@ -1122,6 +1122,36 @@ func (_m *Chain33Client) QueryConsensus(ctx context.Context, in *types.ChainExec
 	return r0, r1
 }
 
+// QueryRandNum provides a mock function with given fields: ctx, in, opts
+func (_m *Chain33Client) QueryRandNum(ctx context.Context, in *types.ReqInt, opts ...grpc.CallOption) (*types.ReplyHash, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *types.ReplyHash
+	if rf, ok := ret.Get(0).(func(context.Context, *types.ReqInt, ...grpc.CallOption) *types.ReplyHash); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*types.ReplyHash)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *types.ReqInt, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // QueryTransaction provides a mock function with given fields: ctx, in, opts
 func (_m *Chain33Client) QueryTransaction(ctx context.Context, in *types.ReqHash, opts ...grpc.CallOption) (*types.TransactionDetail, error) {
 	_va := make([]interface{}, len(opts))
