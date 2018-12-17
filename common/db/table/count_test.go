8d15492ba9b978ab1d922ecc9ade0a1de26fae79
vipwzw <vipwzw@vip.qq.com>
2018-12-17 20:02:03 +0800

    test count

---
+++ common/db/table/count_test.go
@@ -0,0 +1,27 @@
+package table
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCount(t *testing.T) {
+	dir, leveldb, kvdb := getdb()
+	defer dbclose(dir, leveldb)
+	count := NewCount("prefix", "name#hello", kvdb)
+	count.Inc()
+	count.Dec()
+	count.Inc()
+	i, err := count.Get()
+	assert.Nil(t, err)
+	assert.Equal(t, i, int64(1))
+	kvs, err := count.Save()
+	assert.Nil(t, err)
+	setKV(leveldb, kvs)
+
+	count = NewCount("prefix", "name#hello", kvdb)
+	i, err = count.Get()
+	assert.Nil(t, err)
+	assert.Equal(t, i, int64(1))
+}
