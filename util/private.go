78b4bf1c06a4161f809df2f8a03cf7eb9643d87e
Hugo <harbour_982@126.com>
2018-12-25 14:46:12 +0800

    增加testnode中私钥对应的地址信息

--- util/private.go
+++ util/private.go
@@ -21,6 +21,14 @@ var TestPrivkeyHex = []string{
 }
 
 //TestPrivkeyList ：
+/*
+addr:12qyocayNF7Lv6C9qW4avxs2E7U41fKSfv
+addr:14KEKbYtKKQm4wMthSK9J4La4nAiidGozt
+addr:1EbDHAXpoiewjPLX9uqoz38HsKqMXayZrF
+addr:1PUiGcbsccfxW3zuvHXZBJfznziph5miAo
+addr:1KcCVZLSQYRUwE5EXTsAoQs9LuJW6xwfQa
+addr:1EDnnePAZN48aC2hiTDzhkczfF39g1pZZX
+ */
 var TestPrivkeyList = []crypto.PrivKey{
 	HexToPrivkey("4257D8692EF7FE13C68B65D6A52F03933DB2FA5CE8FAF210B5B8B80C721CED01"),
 	HexToPrivkey("CC38546E9E659D15E6B4893F0AB32A06D103931A8230B0BDE71459D2B27D6944"),
