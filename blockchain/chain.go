6beb679a1b59a9194bf9c3a2b7d0ef9b472f6cf8
heyubin <hyb@disanbo.com>
2018-12-25 16:16:15 +0800

    add by hyb for isRecordBlockSequenceValid

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -222,6 +222,10 @@ func (chain *BlockChain) GetOrphanPool() *OrphanPool {
 
 //InitBlockChain 区块链初始化
 func (chain *BlockChain) InitBlockChain() {
+	//isRecordBlockSequence配置的合法性检测
+	if !chain.cfg.IsParaChain {
+		chain.blockStore.isRecordBlockSequenceValid()
+	}
 	//先缓存最新的128个block信息到cache中
 	curheight := chain.GetBlockHeight()
 	if types.IsEnable("TxHeight") {
