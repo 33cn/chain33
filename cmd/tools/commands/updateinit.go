b4e2336f69e3b5acbabb1119548451309de66948
cdh <cdh@33.cn>
2018-12-25 16:51:14 +0800

    pass linter

--- cmd/tools/commands/updateinit.go
+++ cmd/tools/commands/updateinit.go
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//Package commands 扫描指定path目录下所有的插件
-//根据扫描到的结果重新更新consensus、dapp和、store、mempool的初始化文件 init.go
-
 package commands
 
 import (
