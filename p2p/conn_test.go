a2d5e4a9c96b2d1b110f93d012dad488074ecee7
bangzhu.li <bangzhu.li@foxmail.com>
2018-12-29 14:59:39 +0800

    修改test 文件

--- p2p/conn_test.go
+++ p2p/conn_test.go
@@ -10,7 +10,7 @@ import (
 	"google.golang.org/grpc"
 )
 
-func mTestGrpcConns(t *testing.T) {
+func TestGrpcConns(t *testing.T) {
 	var conn *grpc.ClientConn
 	var err error
 	for i := 0; i < 30; i++ {
@@ -42,9 +42,9 @@ func mTestGrpcConns(t *testing.T) {
 func TestGrpcStreamConns(t *testing.T) {
 	var conn *grpc.ClientConn
 	var err error
-	for i := 0; i < 1; i++ {
+	for i := 0; i < 30; i++ {
 		fmt.Println("index:", i)
-		conn, err = grpc.Dial("47.106.117.142:13802", grpc.WithInsecure(),
+		conn, err = grpc.Dial("localhost:13802", grpc.WithInsecure(),
 			grpc.WithDefaultCallOptions(grpc.UseCompressor("gzip")))
 		if err != nil {
 			fmt.Println("grpc DialCon", "did not connect", err)
@@ -53,30 +53,26 @@ func TestGrpcStreamConns(t *testing.T) {
 
 		cli := pb.NewP2PgserviceClient(conn)
 		//流测试
-		ping := &pb.P2PPing{Nonce: int64(123456), Addr: "192.168.1.1:12345", Port: 12345}
-		_, err := P2pComm.Signature("a7769f8ca43b0b694105e165b9c94eda71f374bafb6979f923c6f4593fea10b9", ping)
-		if err != nil {
-			log.Error("Signature", "Error", err.Error())
-			return
-		}
-		ctx, _ := context.WithCancel(context.Background())
+		//		ping := &pb.P2PPing{Nonce: int64(123456), Addr: "192.168.1.1:12345", Port: 12345}
+		//		_, err := P2pComm.Signature("a7769f8ca43b0b694105e165b9c94eda71f374bafb6979f923c6f4593fea10b9", ping)
+		//		if err != nil {
+		//			log.Error("Signature", "Error", err.Error())
+		//			return
+		//		}
+		var p2pdata pb.P2PGetData
+		//ctx, _ := context.WithCancel(context.Background(), &p2pdata)
 
-		resp, err := cli.ServerStreamSend(ctx, ping)
+		resp, err := cli.GetData(context.Background(), &p2pdata)
+		if err != nil {
+			log.Error("GetData", "err:", err.Error())
+			continue
+		}
+		_, err = resp.Recv()
 		if err != nil {
 			log.Error("ServerStreamSend", "err:", err.Error())
 			continue
 		}
 
-		for {
-			data, err := resp.Recv()
-			if err != nil {
-				fmt.Println("GetData error:", err.Error())
-				continue
-
-			}
-
-			fmt.Println(data.GetBlock())
-		}
-		fmt.Println(resp)
 	}
+
 }
