e82834f777bcaac082dc8646601018df2a0e359a
vipwzw <vipwzw@vip.qq.com>
2018-12-31 14:03:53 +0800

    add some test

--- system/dapp/util_test.go
+++ system/dapp/util_test.go
@@ -1,7 +1,26 @@
 package dapp
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/33cn/chain33/types"
+	"github.com/33cn/chain33/util"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestKVCreator(t *testing.T) {
+	dir, ldb, kvdb := util.CreateTestDB()
+	defer util.CloseTestDB(dir, ldb)
+	creator := NewKVCreator(kvdb)
+	creator.AddKV([]byte("a"), []byte("b"))
+	_, err := kvdb.Get([]byte("a"))
+	assert.Equal(t, err, types.ErrNotFound)
+	creator.Add([]byte("a"), []byte("b"))
+	value, err := kvdb.Get([]byte("a"))
+	assert.Equal(t, err, nil)
+	assert.Equal(t, value, []byte("b"))
+}
 
+func TestHeightIndexStr(t *testing.T) {
+	assert.Equal(t, "000000000000100001", HeightIndexStr(1, 1))
 }
