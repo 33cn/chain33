9f8097ed806f68afcf42536b440abedaa3c214b8
vipwzw <vipwzw@vip.qq.com>
2019-01-20 19:36:46 +0800

    fix time

--- common/ntp_test.go
+++ common/ntp_test.go
@@ -4,21 +4,33 @@
 
 package common
 
-//var NtpHosts = []string{
-//	"time.windows.com:123",
-//	"tw.pool.ntp.org:123",
-//	"pool.ntp.org:123",
-//	"cn.pool.ntp.org:123",
-//	"time.apple.com:123",
-//}
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var NtpHosts = []string{
+	"time.windows.com:123",
+	"ntp.ubuntu.com:123",
+	"pool.ntp.org:123",
+	"cn.pool.ntp.org:123",
+	"time.apple.com:123",
+}
 
 // 在单元测试中对网络时间和本地时间进行单元测试的功能是没有意义的
 // 只有在实际运行过程中，通过网络时间来纠正本地时间
 // 本测试用例有概率导致误差大于1秒从而让测试用例运行失败
-//func TestGetRealTime(t *testing.T) {
-//	hosts := NtpHosts
-//	nettime := GetRealTimeRetry(hosts, 10)
-//	local := time.Now()
-//	t.Log(nettime.Sub(local))
-//	assert.Equal(t, int(local.Sub(nettime)/time.Second), 0)
-//}
+func TestGetRealTime(t *testing.T) {
+	hosts := NtpHosts
+	nettime := GetRealTimeRetry(hosts, 10)
+	//get nettime error, ignore
+	if nettime.IsZero() {
+		return
+	}
+	//nettime 获取和本地时间应该
+	local := time.Now()
+	t.Log(nettime.Sub(local))
+	assert.Equal(t, int(local.Sub(nettime)/time.Second), 0)
+}
