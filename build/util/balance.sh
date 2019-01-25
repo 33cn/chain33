bd066945958854e0ccae92813d759c9e8ca458b5
vipwzw <vipwzw@vip.qq.com>
2019-01-25 23:35:33 +0800

    add manage test

--- build/util/balance.sh
+++ build/util/balance.sh
@@ -3,5 +3,5 @@ i=0
 while IFS='' read -r line || [[ -n $line ]]; do
     [ -z "$line" ] && continue
     ((i++))
-    ../chain33-cli account balance -a "$line" -e ticket
+    ../chain33-cli --rpc_laddr https://mainnode.bityuan.com:8801 account balance -a "$line" -e ticket
 done <"$1"
