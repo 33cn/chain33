838d47f70b1199f6d50b8631832c50d6b415687a
jiangpeng <jiangp@33.cn>
2019-03-29 11:19:04 +0800

    fix unit test

--- cmd/tools/tasks/tasks_test.go
+++ cmd/tools/tasks/tasks_test.go
@@ -4,17 +4,4 @@
 
 package tasks
 
-import (
-	"testing"
 
-	"github.com/stretchr/testify/assert"
-
-	"github.com/33cn/chain33/cmd/tools/util"
-)
-
-func TestReplaceTarget(t *testing.T) {
-	fileName := "../config/template/executor/${CLASSNAME}.go.tmp"
-	bcontent, err := util.ReadFile(fileName)
-	assert.NoError(t, err)
-	t.Log(string(bcontent))
-}
