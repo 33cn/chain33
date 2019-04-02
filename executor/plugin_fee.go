199721276a0168f3237cd27dda2551a8c9f99ebb
mdj33 <travis@travis-ci.org>
2019-04-02 17:01:05 +0800

    rmv global fee variable

--- executor/plugin_fee.go
+++ executor/plugin_fee.go
@@ -12,7 +12,6 @@ func init() {
 
 type feePlugin struct {
 	pluginBase
-	fee types.TotalFee
 }
 
 func (p *feePlugin) CheckEnable(executor *executor, enable bool) (kvs []*types.KeyValue, ok bool, err error) {
@@ -20,13 +19,13 @@ func (p *feePlugin) CheckEnable(executor *executor, enable bool) (kvs []*types.K
 }
 
 func (p *feePlugin) ExecLocal(executor *executor, data *types.BlockDetail) ([]*types.KeyValue, error) {
-	p.fee = types.TotalFee{}
+	fee := &types.TotalFee{}
 	for i := 0; i < len(data.Block.Txs); i++ {
 		tx := data.Block.Txs[i]
-		p.fee.Fee += tx.Fee
-		p.fee.TxCount++
+		fee.Fee += tx.Fee
+		fee.TxCount++
 	}
-	kv, err := saveFee(executor, &p.fee, data.Block.ParentHash, data.Block.Hash())
+	kv, err := saveFee(executor, fee, data.Block.ParentHash, data.Block.Hash())
 	if err != nil {
 		return nil, err
 	}
