b141f916f8907e730637754555efab864552ca49
liuyuhang <liuyuhang203@sina.com>
2019-04-02 10:33:02 +0800

    add test

---
+++ blockchain/blockstore_test.go
@@ -0,0 +1,36 @@
+package blockchain_test
+
+import (
+	"testing"
+
+	"io/ioutil"
+	"os"
+
+	"github.com/33cn/chain33/blockchain"
+	dbm "github.com/33cn/chain33/common/db"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetStoreUpgradeMeta(t *testing.T) {
+	dir, err := ioutil.TempDir("", "example")
+	assert.Nil(t, err)
+	defer os.RemoveAll(dir) // clean up
+	os.RemoveAll(dir)       //删除已存在目录
+
+	blockStoreDB := dbm.NewDB("blockchain", "leveldb", dir, 100)
+
+	blockStore := blockchain.NewBlockStore(nil, blockStoreDB, nil)
+	require.NotNil(t, blockStore)
+
+	meta, err := blockStore.GetStoreUpgradeMeta()
+	require.NoError(t, err)
+	require.Equal(t, meta.Version, "0.0.0")
+
+	meta.Version = "1.0.0"
+	err = blockStore.SetStoreUpgradeMeta(meta)
+	require.NoError(t, err)
+	meta, err = blockStore.GetStoreUpgradeMeta()
+	require.NoError(t, err)
+	require.Equal(t, meta.Version, "1.0.0")
+}
