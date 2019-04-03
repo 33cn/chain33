596ef199c7eba9e0a7ce27bcbefaace88e104d44
李邦柱 <lbz@disanbo.com>
2019-04-03 16:57:48 +0800

    修复编译错误

--- p2p/monitor.go
+++ p2p/monitor.go
@@ -48,10 +48,7 @@ func (n *Node) monitorErrPeer() {
 				log.Debug("monitorErrPeer", "over maxattamps", pstat.GetAttempts())
 				n.destroyPeer(peer)
 			}
-		} else {
-			log.Debug("addrBook", "setAddrStat no this peerAddr", peer.Addr())
 		}
-
 	}
 }
 
@@ -348,7 +345,7 @@ func (n *Node) nodeReBalance() {
 			if localBlockHeight-peerBlockHeight < 2048 {
 				log.Info("noReBalance", "Repalce node new node", MinCacheInBoundPeer.Addr(), "old node", MaxInBoundPeer.Addr())
 				n.addPeer(MinCacheInBoundPeer)
-				n.nodeInfo.addrBook.AddAddress(MinCacheInBoundPeer.peerAddr(), nil)
+				n.nodeInfo.addrBook.AddAddress(MinCacheInBoundPeer.peerAddr, nil)
 
 				n.remove(MaxInBoundPeer.Addr())
 				n.RemoveCachePeer(MinCacheInBoundPeer.Addr())
