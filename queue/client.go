59f7e586083c1b97c33482650a2ed043441327d0
yann <yxq@disanbo.com>
2019-04-15 20:49:15 +0800

    modify timeout

--- queue/client.go
+++ queue/client.go
@@ -117,18 +117,24 @@ func (client *client) WaitTimeout(msg *Message, timeout time.Duration) (*Message
 	if msg.chReply == nil {
 		return &Message{}, errors.New("empty wait channel")
 	}
-	if timeout == -1 {
-		msg = <-msg.chReply
-		return msg, msg.Err()
+	//if timeout == -1 {
+	//	msg = <-msg.chReply
+	//	return msg, msg.Err()
+	//}
+	//t := time.NewTimer(timeout)
+	//defer t.Stop()
+	var t <-chan time.Time
+	if timeout > 0 {
+		timer := time.NewTimer(timeout)
+		defer timer.Stop()
+		t = timer.C
 	}
-	t := time.NewTimer(timeout)
-	defer t.Stop()
 	select {
 	case msg = <-msg.chReply:
 		return msg, msg.Err()
 	case <-client.done:
 		return &Message{}, ErrIsQueueClosed
-	case <-t.C:
+	case <-t:
 		return &Message{}, ErrQueueTimeout
 	}
 }
