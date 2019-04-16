96b0770d6b6763663ab2e33619bb484842724aac
yann <yxq@disanbo.com>
2019-04-16 11:15:17 +0800

    add queue unit test

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -58,6 +58,33 @@ func TestTimeout(t *testing.T) {
 	}
 }
 
+func TestClient_WaitTimeout(t *testing.T) {
+	q := New("channel")
+	//mempool
+	go func() {
+		client := q.Client()
+		client.Sub("mempool")
+		for msg := range client.Recv() {
+			if msg.Ty == types.EventTx {
+				time.Sleep(time.Second)
+				msg.Reply(client.NewMessage("mempool", types.EventReply, types.Reply{IsOk: true, Msg: []byte("word")}))
+			}
+		}
+	}()
+	client := q.Client()
+	msg := client.NewMessage("mempool", types.EventTx, "hello")
+	err := client.SendTimeout(msg, true, 0)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = client.WaitTimeout(msg, time.Minute)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
 func TestMultiTopic(t *testing.T) {
 	q := New("channel")
 
