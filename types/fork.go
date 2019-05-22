58b6391b76f2c38b6a83c5cf557e35cbe24bd96c
mdj33 <travis@travis-ci.org>
2019-05-22 10:03:24 +0800

    add fork for para exec register

--- types/fork.go
+++ types/fork.go
@@ -214,6 +214,8 @@ func SetTestNetFork() {
 	systemFork.SetFork("chain33", "ForkLocalDBAccess", 1572391)
 	systemFork.SetFork("chain33", "ForkTxGroupPara", 1687250)
 	systemFork.SetFork("chain33", "ForkBase58AddressCheck", 1800000)
+	//这个fork只影响平行链，注册类似user.p.x.exec的driver，新开的平行链设为0即可，老的平行链要设置新的高度
+	systemFork.SetFork("chain33", "ForkEnableParaRegExec", 0)
 
 }
 
