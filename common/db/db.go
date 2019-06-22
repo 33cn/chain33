8bd55346975b84bc7265a8975ee2792e7c415169
liuyuhang <liuyuhang203@sina.com>
2019-06-22 15:27:39 +0800

    modify linter

--- common/db/db.go
+++ common/db/db.go
@@ -96,6 +96,7 @@ type Batch interface {
 	Reset()         // Reset resets the batch for reuse
 }
 
+// MustWrite must write correct
 func MustWrite(batch Batch) {
 	err := batch.Write()
 	if err != nil {
