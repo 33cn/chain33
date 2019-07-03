7db4ab3c0e3c9533b3dec8899218e6f66ffd3995
vipwzw <vipwzw@vip.qq.com>
2019-07-03 15:58:48 +0800

    auto ci

--- blockchain/rollback_test.go
+++ blockchain/rollback_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
 func init() {
 	types.SetDappFork("local", "store-kvmvccmavl", "ForkKvmvccmavl", 20*10000)
 }
