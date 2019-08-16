5b61b74bff213a39a94965dae224dd411b30c3d4
harrylee <18761806026@163.com>
2019-08-16 18:10:39 +0800

    adjust shell scripts for shellcheck

--- build/docker-compose-dapp.sh
+++ build/docker-compose-dapp.sh
@@ -44,6 +44,7 @@ DAPP_TEST_FILE=""
 if [ -n "${DAPP}" ]; then
     DAPP_TEST_FILE="testcase.sh"
     if [ -e "$DAPP_TEST_FILE" ]; then
+        # shellcheck source=/dev/null
         source "${DAPP_TEST_FILE}"
     fi
 
