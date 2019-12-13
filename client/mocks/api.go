fc4180757002cc649d15c7b95b5c468d6278a0a2
vipwzw <vipwzw@vip.qq.com>
2019-12-13 14:45:40 +0800

    auto ci

--- client/mocks/api.go
+++ client/mocks/api.go
@@ -403,52 +403,6 @@ func (_m *QueueProtocolAPI) GetLastBlockMainSequence() (*types.Int64, error) {
 	return r0, r1
 }
 
-// GetPrivacyTransactionByHash provides a mock function with given fields: param
-func (_m *QueueProtocolAPI) GetPrivacyTransactionByHash(param *types.ReqPrivacyHashes) (*types.TransactionDetails, error) {
-	ret := _m.Called(param)
-
-	var r0 *types.TransactionDetails
-	if rf, ok := ret.Get(0).(func(*types.ReqPrivacyHashes) *types.TransactionDetails); ok {
-		r0 = rf(param)
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(*types.TransactionDetails)
-		}
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func(*types.ReqPrivacyHashes) error); ok {
-		r1 = rf(param)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
-// GetNewPrivacyTxQueryID provides a mock function with given fields:
-func (_m *QueueProtocolAPI) GetNewPrivacyTxQueryID() (*types.ReplyString, error) {
-	ret := _m.Called()
-
-	var r0 *types.ReplyString
-	if rf, ok := ret.Get(0).(func() *types.ReplyString); ok {
-		r0 = rf()
-	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(*types.ReplyString)
-		}
-	}
-
-	var r1 error
-	if rf, ok := ret.Get(1).(func() error); ok {
-		r1 = rf()
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
 // GetLastBlockSequence provides a mock function with given fields:
 func (_m *QueueProtocolAPI) GetLastBlockSequence() (*types.Int64, error) {
 	ret := _m.Called()
@@ -587,8 +541,8 @@ func (_m *QueueProtocolAPI) GetNetInfo() (*types.NodeNetInfo, error) {
 	return r0, r1
 }
 
-// GetNewPrivacyTxQueryId provides a mock function with given fields:
-func (_m *QueueProtocolAPI) GetNewPrivacyTxQueryId() (*types.ReplyString, error) {
+// GetNewPrivacyTxQueryID provides a mock function with given fields:
+func (_m *QueueProtocolAPI) GetNewPrivacyTxQueryID() (*types.ReplyString, error) {
 	ret := _m.Called()
 
 	var r0 *types.ReplyString
