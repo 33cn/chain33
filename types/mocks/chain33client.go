47b2a92d03b786867c0f1d9ab8376108523ea28c
vipwzw <vipwzw@vip.qq.com>
2020-01-19 11:11:14 +0800

    auto ci

--- types/mocks/chain33client.go
+++ types/mocks/chain33client.go
@@ -227,6 +227,36 @@ func (_m *Chain33Client) DumpPrivkey(ctx context.Context, in *types.ReqString, o
 	return r0, r1
 }
 
+// DumpPrivkeysFile provides a mock function with given fields: ctx, in, opts
+func (_m *Chain33Client) DumpPrivkeysFile(ctx context.Context, in *types.ReqPrivkeysFile, opts ...grpc.CallOption) (*types.Reply, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *types.Reply
+	if rf, ok := ret.Get(0).(func(context.Context, *types.ReqPrivkeysFile, ...grpc.CallOption) *types.Reply); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*types.Reply)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *types.ReqPrivkeysFile, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // ExecWallet provides a mock function with given fields: ctx, in, opts
 func (_m *Chain33Client) ExecWallet(ctx context.Context, in *types.ChainExecutor, opts ...grpc.CallOption) (*types.Reply, error) {
 	_va := make([]interface{}, len(opts))
@@ -1089,6 +1119,36 @@ func (_m *Chain33Client) ImportPrivkey(ctx context.Context, in *types.ReqWalletI
 
 	var r1 error
 	if rf, ok := ret.Get(1).(func(context.Context, *types.ReqWalletImportPrivkey, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// ImportPrivkeysFile provides a mock function with given fields: ctx, in, opts
+func (_m *Chain33Client) ImportPrivkeysFile(ctx context.Context, in *types.ReqPrivkeysFile, opts ...grpc.CallOption) (*types.Reply, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *types.Reply
+	if rf, ok := ret.Get(0).(func(context.Context, *types.ReqPrivkeysFile, ...grpc.CallOption) *types.Reply); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*types.Reply)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *types.ReqPrivkeysFile, ...grpc.CallOption) error); ok {
 		r1 = rf(ctx, in, opts...)
 	} else {
 		r1 = ret.Error(1)
