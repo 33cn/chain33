8878494beb690858a1e7304e39ff5d1f6b2a65b8
linj-disanbo <linj@disanbo.com>
2020-02-10 12:55:36 +0800

    fix package

--- system/plugin/kvmvcc/kvmvcc.go
+++ system/plugin/kvmvcc/kvmvcc.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package executor
+package kvmvcc
 
 import (
 	dbm "github.com/33cn/chain33/common/db"
