cc15c5ed84c75789f741effd34efb4a1e002d764
linj-disanbo <linj@disanbo.com>
2020-02-11 18:58:18 +0800

    new localkv for plugin/addrindex

--- system/plugin/addrindex/addrindex.go
+++ system/plugin/addrindex/addrindex.go
@@ -15,14 +15,21 @@ import (
 	"github.com/33cn/chain33/types"
 )
 
-var elog = log.New("module", "system/plugin/txindex")
+var (
+	name = "addrindex"
+	elog = log.New("module", "system/plugin/txindex")
+)
 
 func init() {
-	plugin.RegisterPlugin("addrindex", &addrindexPlugin{})
+	plugin.RegisterPlugin(name, newAddrindex())
 }
 
 type addrindexPlugin struct {
-	plugin.Base
+	*plugin.Base
+}
+
+func newAddrindex() *addrindexPlugin {
+	return &addrindexPlugin{Base: &plugin.Base{}}
 }
 
 func (p *addrindexPlugin) CheckEnable(enable bool) (kvs []*types.KeyValue, ok bool, err error) {
@@ -38,8 +45,8 @@ func (p *addrindexPlugin) ExecLocal(data *types.BlockDetail) ([]*types.KeyValue,
 		txindex := getTxIndex(p, tx, receipt, i)
 		txinfobyte := types.Encode(txindex.index)
 		if len(txindex.from) != 0 {
-			fromkey1 := types.CalcTxAddrDirHashKey(txindex.from, drivers.TxIndexFrom, txindex.heightstr)
-			fromkey2 := types.CalcTxAddrHashKey(txindex.from, txindex.heightstr)
+			fromkey1 := CalcTxAddrDirHashKey(name, txindex.from, drivers.TxIndexFrom, txindex.heightstr)
+			fromkey2 := CalcTxAddrHashKey(name, txindex.from, txindex.heightstr)
 			set.KV = append(set.KV, &types.KeyValue{Key: fromkey1, Value: txinfobyte})
 			set.KV = append(set.KV, &types.KeyValue{Key: fromkey2, Value: txinfobyte})
 			types.AssertConfig(p.GetAPI())
@@ -49,8 +56,8 @@ func (p *addrindexPlugin) ExecLocal(data *types.BlockDetail) ([]*types.KeyValue,
 			}
 		}
 		if len(txindex.to) != 0 {
-			tokey1 := types.CalcTxAddrDirHashKey(txindex.to, drivers.TxIndexTo, txindex.heightstr)
-			tokey2 := types.CalcTxAddrHashKey(txindex.to, txindex.heightstr)
+			tokey1 := CalcTxAddrDirHashKey(name, txindex.to, drivers.TxIndexTo, txindex.heightstr)
+			tokey2 := CalcTxAddrHashKey(name, txindex.to, txindex.heightstr)
 			set.KV = append(set.KV, &types.KeyValue{Key: tokey1, Value: txinfobyte})
 			set.KV = append(set.KV, &types.KeyValue{Key: tokey2, Value: txinfobyte})
 			types.AssertConfig(p.GetAPI())
@@ -72,8 +79,8 @@ func (p *addrindexPlugin) ExecDelLocal(data *types.BlockDetail) ([]*types.KeyVal
 		//del: addr index
 		txindex := getTxIndex(p, tx, receipt, i)
 		if len(txindex.from) != 0 {
-			fromkey1 := types.CalcTxAddrDirHashKey(txindex.from, drivers.TxIndexFrom, txindex.heightstr)
-			fromkey2 := types.CalcTxAddrHashKey(txindex.from, txindex.heightstr)
+			fromkey1 := CalcTxAddrDirHashKey(name, txindex.from, drivers.TxIndexFrom, txindex.heightstr)
+			fromkey2 := CalcTxAddrHashKey(name, txindex.from, txindex.heightstr)
 			set.KV = append(set.KV, &types.KeyValue{Key: fromkey1, Value: nil})
 			set.KV = append(set.KV, &types.KeyValue{Key: fromkey2, Value: nil})
 			kv, err := updateAddrTxsCount(p.GetAPI().GetConfig(), p.GetLocalDB(), txindex.from, 1, false)
@@ -82,8 +89,8 @@ func (p *addrindexPlugin) ExecDelLocal(data *types.BlockDetail) ([]*types.KeyVal
 			}
 		}
 		if len(txindex.to) != 0 {
-			tokey1 := types.CalcTxAddrDirHashKey(txindex.to, drivers.TxIndexTo, txindex.heightstr)
-			tokey2 := types.CalcTxAddrHashKey(txindex.to, txindex.heightstr)
+			tokey1 := CalcTxAddrDirHashKey(name, txindex.to, drivers.TxIndexTo, txindex.heightstr)
+			tokey2 := CalcTxAddrHashKey(name, txindex.to, txindex.heightstr)
 			set.KV = append(set.KV, &types.KeyValue{Key: tokey1, Value: nil})
 			set.KV = append(set.KV, &types.KeyValue{Key: tokey2, Value: nil})
 			kv, err := updateAddrTxsCount(p.GetAPI().GetConfig(), p.GetLocalDB(), txindex.to, 1, false)
@@ -98,13 +105,13 @@ func (p *addrindexPlugin) ExecDelLocal(data *types.BlockDetail) ([]*types.KeyVal
 func getAddrTxsCountKV(addr string, count int64) *types.KeyValue {
 	counts := &types.Int64{Data: count}
 	countbytes := types.Encode(counts)
-	kv := &types.KeyValue{Key: types.CalcAddrTxsCountKey(addr), Value: countbytes}
+	kv := &types.KeyValue{Key: CalcAddrTxsCountKey(name, addr), Value: countbytes}
 	return kv
 }
 
 func getAddrTxsCount(db dbm.KVDB, addr string) (int64, error) {
 	count := types.Int64{}
-	TxsCount, err := db.Get(types.CalcAddrTxsCountKey(addr))
+	TxsCount, err := db.Get(CalcAddrTxsCountKey(name, addr))
 	if err != nil && err != types.ErrNotFound {
 		return 0, err
 	}
@@ -177,4 +184,28 @@ func getTxIndex(p plugin.Plugin, tx *types.Transaction, receipt *types.ReceiptDa
 	txIndexInfo.from = address.PubKeyToAddress(tx.GetSignature().GetPubkey()).String()
 	txIndexInfo.to = tx.GetRealToAddr()
 	return &txIndexInfo
+}
+
+// local keys
+var (
+	TxAddrHash    = []byte("TxAddrHash")
+	TxAddrDirHash = []byte("TxAddrDirHash")
+	AddrTxsCount  = []byte("AddrTxsCount")
+)
+
+//CalcTxAddrHashKey 用于存储地址相关的hash列表，key=TxAddrHash:addr:height*100000 + index
+//地址下面所有的交易
+func CalcTxAddrHashKey(name, addr string, heightindex string) []byte {
+	return []byte(fmt.Sprintf("%s-%s-%s:%s:%s", types.LocalPluginPrefix, name, TxAddrHash, addr, heightindex))
+}
+
+//CalcTxAddrDirHashKey 用于存储地址相关的hash列表，key=TxAddrHash:addr:flag:height*100000 + index
+//地址下面某个分类的交易
+func CalcTxAddrDirHashKey(name, addr string, flag int32, heightindex string) []byte {
+	return []byte(fmt.Sprintf("%s-%s-%s:%s:%s", types.LocalPluginPrefix, name, TxAddrDirHash, addr, flag, heightindex))
+}
+
+//CalcAddrTxsCountKey 存储地址参与的交易数量。add时加一，del时减一
+func CalcAddrTxsCountKey(name, addr string) []byte {
+	return []byte(fmt.Sprintf("%s-%s-%s:%s", types.LocalPluginPrefix, name, AddrTxsCount, addr))
 }
