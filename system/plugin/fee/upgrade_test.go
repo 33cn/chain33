0934191ba26fcacd79427e8b72955b53c836aefd
linj-disanbo <linj@disanbo.com>
2020-02-13 11:40:49 +0800

    plugin/fee upgrade  test

---
+++ system/plugin/fee/upgrade_test.go
@@ -0,0 +1,54 @@
+package fee
+
+import (
+	"testing"
+
+	plugins "github.com/33cn/chain33/system/plugin"
+	"github.com/33cn/chain33/types"
+	"github.com/33cn/chain33/util"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Upgrade(t *testing.T) {
+	dir, db, localdb := util.CreateTestDB()
+	defer util.CloseTestDB(dir, db)
+	assert.NotNil(t, localdb)
+
+	// test empty db
+	p := newFee()
+	p.SetLocalDB(localdb)
+	err := p.Upgrade()
+	assert.Nil(t, err)
+
+	// test again
+	plugins.SetVersion(localdb, name, 1)
+	err = p.Upgrade()
+	assert.Nil(t, err)
+
+	// test with data
+	addresses := []string{"addr1", "addr2", "addr3"}
+	data := []types.TotalFee{
+		{Fee: 100, TxCount: 2},
+		{Fee: 200, TxCount: 1},
+		{Fee: 300, TxCount: 3},
+	}
+	for i := 0; i < 3; i++ {
+		localdb.Set(types.TotalFeeKey([]byte(addresses[i])), types.Encode(&data[i]))
+	}
+
+	plugins.SetVersion(localdb, name, 1)
+	err = p.Upgrade()
+	assert.Nil(t, err)
+
+	// 已经是升级后的版本了， 不需要再升级
+	err = p.Upgrade()
+	assert.Nil(t, err)
+
+	// 先修改版本去升级，但数据已经升级了， 所以处理数据量为0
+	plugins.SetVersion(localdb, name, 1)
+	err = p.Upgrade()
+	assert.Nil(t, err)
+
+	// just print log
+	//assert.NotNil(t, nil)
+}
