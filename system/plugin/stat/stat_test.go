b974a84c2cf34f6d428c05a3d5bf913011a1372e
linj-disanbo <linj@disanbo.com>
2020-02-14 16:12:35 +0800

    add plugin/stat test

---
+++ system/plugin/stat/stat_test.go
@@ -0,0 +1,69 @@
+// Copyright Fuzamei Corp. 2018 All Rights Reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package stat
+
+import (
+	"testing"
+
+	"github.com/33cn/chain33/types"
+	"github.com/33cn/chain33/util"
+	"github.com/stretchr/testify/assert"
+
+	apimock "github.com/33cn/chain33/client/mocks"
+)
+
+func TestPlugin(t *testing.T) {
+	dir, ldb, kvdb := util.CreateTestDB()
+	defer util.CloseTestDB(dir, ldb)
+
+	var txs []*types.Transaction
+	_, priv := util.Genaddress()
+	tx := &types.Transaction{Fee: 1000000, Execer: []byte("none"), To: "15xMtKs3Ca89X1KNoPZibChBtRPa5BYth0"}
+	tx.Sign(types.SECP256K1, priv)
+	tx2 := &types.Transaction{Fee: 2000000, Execer: []byte("none"), To: "15xMtKs3Ca89X1KNoPZibChBtRPa5BYth1"}
+	tx.Sign(types.SECP256K1, priv)
+	txs = append(txs, tx, tx2)
+	var stateHash [32]byte
+	stateHash[0] = 30
+
+	cfg := types.NewChain33ConfigNoInit("")
+	cfg.S("dbversion", 1)
+	api := new(apimock.QueueProtocolAPI)
+	api.On("GetConfig").Return(cfg)
+
+	p := newStat()
+	p.SetEnv(2, 20, 300)
+	p.SetLocalDB(kvdb)
+	p.SetAPI(api)
+
+	detail := &types.BlockDetail{
+		Block:    &types.Block{Txs: txs, StateHash: stateHash[:]},
+		Receipts: []*types.ReceiptData{{}, {}},
+	}
+
+	flag := types.Int64{Data: 1}
+	v := types.Encode(&flag)
+	ldb.Set(types.StatisticFlag(), v)
+
+	_, ok, err := p.CheckEnable(true)
+	assert.NoError(t, err)
+	assert.True(t, ok)
+
+	kvs, err := p.ExecLocal(detail)
+	assert.NoError(t, err)
+	for _, kv := range kvs {
+		assert.Contains(t, string(kv.Key), "LODBP-"+name+"-")
+	}
+
+	for _, kv := range kvs {
+		err = kvdb.Set(kv.Key, kv.Value)
+		assert.NoError(t, err)
+	}
+	kvs, err = p.ExecDelLocal(detail)
+	assert.NoError(t, err)
+	for _, kv := range kvs {
+		assert.Contains(t, string(kv.Key), "LODBP-"+name+"-")
+	}
+}
