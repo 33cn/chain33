02da4c9013ff3c62897c788769474027f2484ccb
linj-disanbo <linj@disanbo.com>
2020-02-25 14:50:34 +0800

    test  plugin/addrindex Query

--- system/plugin/addrindex/addrindex_test.go
+++ system/plugin/addrindex/addrindex_test.go
@@ -23,15 +23,17 @@ func TestPlugin(t *testing.T) {
 	tx := &types.Transaction{Fee: 1000000, Execer: []byte("none"), To: "15xMtKs3Ca89X1KNoPZibChBtRPa5BYth0"}
 	tx.Sign(types.SECP256K1, priv)
 	tx2 := &types.Transaction{Fee: 1000000, Execer: []byte("none"), To: "15xMtKs3Ca89X1KNoPZibChBtRPa5BYth1"}
-	tx.Sign(types.SECP256K1, priv)
+	tx2.Sign(types.SECP256K1, priv)
 	txs = append(txs, tx, tx2)
 	var stateHash [32]byte
 	stateHash[0] = 30
 
 	cfg := types.NewChain33ConfigNoInit("")
-	cfg.S("dbversion", 1)
+	cfg.S("dbversion", int64(1))
 	api := new(apimock.QueueProtocolAPI)
 	api.On("GetConfig").Return(cfg)
+	ver := cfg.GInt("dbversion")
+	assert.Equal(t, int64(1), ver)
 
 	p := newAddrindex()
 	p.SetEnv(2, 20, 300)
@@ -54,6 +56,27 @@ func TestPlugin(t *testing.T) {
 		err = kvdb.Set(kv.Key, kv.Value)
 		assert.NoError(t, err)
 	}
+
+	// found
+	var req types.ReqKey
+	req.Key = []byte(tx2.From())
+	counts, err := p.Query("GetAddrTxsCount", types.Encode(&req))
+	assert.Nil(t, err)
+	c := counts.(*types.Int64)
+	assert.Equal(t, int64(2), c.GetData())
+
+	req2 := types.ReqAddr{
+		Addr:   tx2.From(),
+		Count:  1,
+		Flag:   0,
+		Height: -1,
+	}
+
+	r2, err := p.Query("GetTxsByAddr", types.Encode(&req2))
+	assert.Nil(t, err)
+	result2 := r2.(*types.ReplyTxInfos)
+	assert.Equal(t, 1, len(result2.GetTxInfos()))
+
 	kvs, err = p.ExecDelLocal(detail)
 	assert.NoError(t, err)
 	for _, kv := range kvs {
