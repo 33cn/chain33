99e7c130fc29baf32dfad8d203e565aafdc2b5f1
bangzhu.li-matbook <bangzhu.li@foxmail.com>
2020-03-04 00:01:14 +0800

    fix init EventType

--- p2pnext/protocol/init.go
+++ p2pnext/protocol/init.go
@@ -1,7 +1,12 @@
 package protocol
 
 import (
+	_ "github.com/33cn/chain33/p2pnext/protocol/broadcast"
+	_ "github.com/33cn/chain33/p2pnext/protocol/download"
+	_ "github.com/33cn/chain33/p2pnext/protocol/headers"
+	_ "github.com/33cn/chain33/p2pnext/protocol/peer"
 	prototypes "github.com/33cn/chain33/p2pnext/protocol/types"
+	_ "github.com/libp2p/go-libp2p-core/protocol"
 )
 
 // Init init p2p protocol
