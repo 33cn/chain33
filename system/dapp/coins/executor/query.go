980e44c52f8ccb13ac8b4a5ece01cbabdcc9ff07
linj-disanbo <linj@disanbo.com>
2020-03-03 15:25:50 +0800

    fix  保证现有调用有效, 保留空函数, 和jrpc的实现有关

--- system/dapp/coins/executor/query.go
+++ system/dapp/coins/executor/query.go
@@ -32,3 +32,16 @@ func (c *Coins) GetAddrReciver(addr *types.ReqAddr) (types.Message, error) {
 	}
 	return &reciver, nil
 }
+
+// 保证现有调用有效, 保留空函数, 和jrpc的实现有关
+// 在现有的接口实现时, 需要看执行器的接口的返回类型, 做json到pb的输出数据转化.
+
+// Query_GetTxsByAddr query get txs by address
+func (c *Coins) Query_GetTxsByAddr(in *types.ReqAddr) (types.Message, error) {
+	return nil, nil
+}
+
+// Query_GetAddrTxsCount query count of  txs in the address
+func (c *Coins) Query_GetAddrTxsCount(in *types.ReqKey) (types.Message, error) {
+	return nil, nil
+}
