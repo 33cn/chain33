f47d632b291780b1b6900a2943a64c8e9f9b7795
linj-disanbo <linj@disanbo.com>
2020-03-04 14:55:45 +0800

    更新插件的升级实现

--- system/plugin/mvcc/upgrade.go
+++ system/plugin/mvcc/upgrade.go
@@ -8,10 +8,8 @@ import (
 	"fmt"
 
 	log "github.com/33cn/chain33/common/log/log15"
-	"github.com/33cn/chain33/system/plugin"
 	plugins "github.com/33cn/chain33/system/plugin"
 	"github.com/33cn/chain33/types"
-	"github.com/pkg/errors"
 )
 
 var elog = log.New("module", "system/plugin/mvcc")
@@ -26,38 +24,10 @@ func Prefix(name string) []byte {
 	return []byte(fmt.Sprintf("%s-%s-", types.LocalPluginPrefix, name))
 }
 
-func (p *mvccPlugin) Upgrade() error {
+func (p *mvccPlugin) Upgrade(count int32) (bool, error) {
 	toVersion := 2
-	elog.Info("Upgrade start", "to_version", toVersion, "plugin", name)
-	version, err := plugins.GetVersion(p.GetLocalDB(), name)
-	if err != nil {
-		return errors.Wrap(err, "Upgrade get version")
-	}
-	if version >= toVersion {
-		elog.Debug("Upgrade not need to upgrade", "current_version", version, "to_version", toVersion)
-		return nil
-	}
-
-	// 规则不变, 直接换前缀, 不通过各类数据分别升级
-	prefixes := []struct {
-		from []byte
-		to   []byte
-	}{
+	prefixes := []plugins.Prefixes{
 		{PrefixOld(), Prefix(name)},
 	}
-
-	for _, prefix := range prefixes {
-		err := plugin.UpgradeOneKey(p.GetLocalDB(), prefix.from, prefix.to)
-		if err != nil {
-			return err
-		}
-	}
-
-	err = plugins.SetVersion(p.GetLocalDB(), name, toVersion)
-	if err != nil {
-		return errors.Wrap(err, "Upgrade setVersion")
-	}
-
-	elog.Info("Upgrade upgrade done")
-	return nil
+	return plugins.Upgrade(p.GetLocalDB(), name, toVersion, prefixes, count)
 }
--- system/plugin/stat/upgrade.go
+++ system/plugin/stat/upgrade.go
@@ -10,31 +10,31 @@ import (
 	"github.com/pkg/errors"
 )
 
-func (p *statPlugin) Upgrade() error {
+func (p *statPlugin) Upgrade(count int32) (bool, error) {
 	toVersion := 2
 	elog.Info("Upgrade start", "to_version", toVersion, "plugin", name)
 	version, err := plugins.GetVersion(p.GetLocalDB(), name)
 	if err != nil {
-		return errors.Wrap(err, "Upgrade get version")
+		return false, errors.Wrap(err, "Upgrade get version")
 	}
 	if version >= toVersion {
 		elog.Debug("Upgrade not need to upgrade", "current_version", version, "to_version", toVersion)
-		return nil
+		return true, nil
 	}
 
 	enable, err := plugins.UpgradeFlag(p.GetLocalDB(), name, types.StatisticFlag())
 	if err != nil {
-		return err
+		return false, err
 	}
 	if enable == false {
-		return nil
+		return true, nil
 	}
 
 	err = plugins.SetVersion(p.GetLocalDB(), name, toVersion)
 	if err != nil {
-		return errors.Wrap(err, "Upgrade setVersion")
+		return false, errors.Wrap(err, "Upgrade setVersion")
 	}
 
 	elog.Info("Upgrade upgrade done")
-	return nil
+	return true, nil
 }
