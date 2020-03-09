a6d7597b9a35b47c6a1186282a5444732552588a
linj-disanbo <linj@disanbo.com>
2020-03-09 14:50:02 +0800

    删除执行器插件mvcc

--- system/index/init/init.go
+++ system/index/init/init.go
@@ -3,7 +3,6 @@ package init
 import (
 	_ "github.com/33cn/chain33/system/index/addrindex"
 	_ "github.com/33cn/chain33/system/index/fee"
-	_ "github.com/33cn/chain33/system/index/mvcc"
 	_ "github.com/33cn/chain33/system/index/stat"
 	_ "github.com/33cn/chain33/system/index/txindex"
 )
