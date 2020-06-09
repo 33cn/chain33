cde0ff1ed38e27e8e673bdea9e534fe718e34f4d
jiangpeng <jiangp@33.cn>
2020-06-09 14:45:32 +0800

    solo:add bench test case

--- system/consensus/solo/solo.go
+++ system/consensus/solo/solo.go
@@ -113,7 +113,7 @@ func (client *Client) CreateBlock() {
 		txs := client.RequestTx(maxTxNum, nil)
 		// 为方便测试，设定基准测试模式，每个块交易数保持恒定，为配置的最大交易数
 		if len(txs) == 0 || (client.subcfg.BenchMode && len(txs) < maxTxNum) {
-			log.Warn("======SoloWaitMoreTxs======", "currTxNum", len(txs))
+			log.Debug("======SoloWaitMoreTxs======", "currTxNum", len(txs))
 			issleep = true
 			continue
 		}
