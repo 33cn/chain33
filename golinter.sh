ed519d18d5e21f8b32d6bc0f9b63684ffb7a3fa4
mdj33 <mdj@disanbo.com>
2020-07-10 15:52:53 +0800

    linter

--- golinter.sh
+++ golinter.sh
@@ -60,7 +60,8 @@ function testLinter() {
         --enable=goimports \
         --enable=misspell \
         --enable=golint \
-        --exclude=underscores
+        --exclude=underscores \
+        --exclude-use-default=false
 
     cd - >/dev/null || exit
 }
