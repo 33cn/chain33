411e24a0579feb41bc23a49eb28d1394f4653e35
yxq <yxq@disanbo.com>
2020-08-13 15:59:28 +0800

    get all chunks related to the peer before notifying

--- system/p2p/dht/protocol/p2pstore/republish.go
+++ system/p2p/dht/protocol/p2pstore/republish.go
@@ -21,33 +21,45 @@ func (p *Protocol) republish() {
 		m[k] = v
 	}
 	p.localChunkInfoMutex.RUnlock()
-	log.Info("republish", ">>>>>>>>>>>>>> record amount:", len(m))
-	log.Info("republish", "rt count", len(p.RoutingTable.ListPeers()), "healthy count", len(p.healthyRoutingTable.ListPeers()))
+	invertedIndex := make(map[peer.ID][]*types.ChunkInfoMsg)
 	for hash, info := range m {
 		if time.Since(info.Time) > types2.ExpiredTime {
+			log.Info("republish deleteChunkBlock", "hash", hash, "start", info.Start)
 			if err := p.deleteChunkBlock(info.ChunkHash); err != nil {
 				log.Error("republish deleteChunkBlock error", "hash", hash, "error", err)
 			}
 			continue
 		}
 		log.Info("local chunk", "hash", hash, "start", info.Start)
-		p.notifyStoreChunk(info.ChunkInfoMsg)
+		peers := p.healthyRoutingTable.NearestPeers(genDHTID(info.ChunkHash), Backup-1)
+		for _, pid := range peers {
+			invertedIndex[pid] = append(invertedIndex[pid], info.ChunkInfoMsg)
+		}
 	}
+
+	log.Info("republish", "invertedIndex length", len(invertedIndex))
+	for pid, infos := range invertedIndex {
+		log.Info("republish", "pid", pid, "info len", len(infos))
+		if err := p.storeChunksOnPeer(pid, infos...); err != nil {
+			log.Error("republish", "storeChunksOnPeer error", err, "pid", pid)
+		}
+	}
+	log.Info("republish ok")
 }
 
 // 通知最近的 *BackUp-1* 个节点备份数据，加上本节点共Backup个
 func (p *Protocol) notifyStoreChunk(req *types.ChunkInfoMsg) {
 	peers := p.healthyRoutingTable.NearestPeers(genDHTID(req.ChunkHash), Backup-1)
 	for _, pid := range peers {
-		err := p.storeChunkOnPeer(req, pid)
+		err := p.storeChunksOnPeer(pid, req)
 		if err != nil {
 			log.Error("notifyStoreChunk", "peer id", pid, "error", err)
 		}
 	}
 }
 
-func (p *Protocol) storeChunkOnPeer(req *types.ChunkInfoMsg, pid peer.ID) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+func (p *Protocol) storeChunksOnPeer(pid peer.ID, reqs ...*types.ChunkInfoMsg) error {
+	ctx, cancel := context.WithTimeout(p.Ctx, time.Minute)
 	defer cancel()
 	stream, err := p.Host.NewStream(ctx, pid, protocol.StoreChunk)
 	if err != nil {
@@ -55,8 +67,10 @@ func (p *Protocol) storeChunkOnPeer(req *types.ChunkInfoMsg, pid peer.ID) error 
 		return err
 	}
 	defer protocol.CloseStream(stream)
-	msg := types.P2PRequest{
-		Request: &types.P2PRequest_ChunkInfoMsg{ChunkInfoMsg: req},
-	}
+	msg := types.P2PRequest{}
+	msg.Request = &types.P2PRequest_ChunkInfoList{
+		ChunkInfoList: &types.ChunkInfoList{
+			Items: reqs,
+		}}
 	return protocol.SignAndWriteStream(&msg, stream)
 }
