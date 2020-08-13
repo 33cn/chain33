9556f6ede74afbf8461638319860be79dcccf80b
vipwzw <vipwzw@vip.qq.com>
2020-08-13 15:30:38 +0800

    auto ci

--- types/p2pnext.pb.go
+++ types/p2pnext.pb.go
@@ -5,8 +5,9 @@ package types
 
 import (
 	fmt "fmt"
-	proto "github.com/golang/protobuf/proto"
 	math "math"
+
+	proto "github.com/golang/protobuf/proto"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -2102,7 +2103,9 @@ func init() {
 	proto.RegisterType((*RemoveTopicReply)(nil), "types.RemoveTopicReply")
 }
 
-func init() { proto.RegisterFile("p2pnext.proto", fileDescriptor_d81e96199caf00d1) }
+func init() {
+	proto.RegisterFile("p2pnext.proto", fileDescriptor_d81e96199caf00d1)
+}
 
 var fileDescriptor_d81e96199caf00d1 = []byte{
 	// 1191 bytes of a gzipped FileDescriptorProto
