a1f38535047e0ccfeaacb316b9d42380bc9a79ef
yxq <yxq@disanbo.com>
2020-11-26 15:02:30 +0800

    update db test script

--- test/main.go
+++ test/main.go
@@ -6,45 +6,31 @@ import (
 	"encoding/binary"
 	"fmt"
 	"log"
-	"math/rand"
 	"time"
 
-	"github.com/33cn/chain33/types"
 	"github.com/cockroachdb/pebble"
 	"github.com/cockroachdb/pebble/bloom"
 )
 
 func main() {
-	level := &pebble.LevelOptions{}
-	level.EnsureDefaults()
-	level.FilterPolicy = bloom.FilterPolicy(10)
-
+	level := pebble.LevelOptions{
+		FilterPolicy: bloom.FilterPolicy(10),
+	}
 	db, err := pebble.Open("/home/yann/test/pebble", &pebble.Options{
 		BytesPerSync: 4 << 20,
-		Levels:       []pebble.LevelOptions{{FilterPolicy: bloom.FilterPolicy(10)}},
+		Levels:       []pebble.LevelOptions{level},
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	tx := &types.Transaction{
-		Execer:    []byte("dajfilwejwpohgg3"),
-		Payload:   []byte("fhieuwkytoehgoegowhgo324goi3oi34rehgothei4goh34itvndeoigl3hpgh p43thtoi3hto38t3g4oigeoitheirbfjksdhflj4lkgg43ig ehgoihewioghow;eg ihgewgo h eigerh go;"),
-		To:        "hfi23uytowhfskhfuiegfskfhskjfgwu",
-		Header:    []byte("gf2i3ur23u9urhoihfishfoehwf  fsoi hs go3 3 wo fyosfh owi fhs hifos hosf  "),
-		HashCache: []byte("hfui23yfweoihfifgiogfiwegowlf "),
-	}
-	_ = tx
 
+	testValue := []byte("giu398egfisugdiwegfaiulfffffffffffglgieruiugfiuadgugfuihfd3ugfogeiof;hdfheodgfeugfdj;gf;3orfigfdagfuiehrfuiehiafbdjafhariugfhuaiegfriuhfeagauiffaeferhi;hgoaigf")
 	start := time.Now()
 	for i := 0; i < 1e8; i++ {
 		fmt.Println(i)
 		sum := sha256.Sum256(IntToBytes(i))
 		key := sum[:]
-		tx.Fee = rand.Int63()
-		tx.Expire = rand.Int63()
-		tx.Nonce = rand.Int63()
-		tx.GroupCount = rand.Int31()
-		if err := db.Set(key, types.Encode(tx), pebble.NoSync); err != nil {
+		if err := db.Set(key, testValue, pebble.NoSync); err != nil {
 			log.Fatal(err)
 		}
 	}
