07d5d77dcb4023093149c1bc1109e5276edbe368
yxq <yxq@disanbo.com>
2021-01-29 10:31:56 +0800

    add disable shard config

--- system/p2p/dht/protocol/p2pstore/republish.go
+++ system/p2p/dht/protocol/p2pstore/republish.go
@@ -14,7 +14,7 @@ import (
 
 func (p *Protocol) republish() {
 	//全节点的p2pstore保存所有chunk, 不进行republish操作
-	if p.SubConfig.IsFullNode {
+	if p.SubConfig.DisableShard || p.SubConfig.IsFullNode {
 		return
 	}
 	reply, err := p.API.IsSync()
