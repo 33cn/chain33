ba5da7fb9906f7d9c5caa0598aa75a8792b6d53d
mdj33 <mdj@disanbo.com>
2021-03-19 11:18:00 +0800

    ntp server configue support

--- util/cli/chain33.go
+++ util/cli/chain33.go
@@ -106,8 +106,11 @@ func RunChain33(name, defCfg string) {
 		cfg.P2P.WaitPid = *waitPid
 	}
 
+	if len(cfg.NtpHosts) <= 0 {
+		cfg.NtpHosts = append(cfg.NtpHosts, types.NtpHosts...)
+	}
 	if cfg.FixTime {
-		go fixtimeRoutine()
+		go fixtimeRoutine(cfg.NtpHosts)
 	}
 	//compare minFee in wallet, mempool, exec
 	//set file log
@@ -257,8 +260,7 @@ func pwd() string {
 	return dir
 }
 
-func fixtimeRoutine() {
-	hosts := types.NtpHosts
+func fixtimeRoutine(hosts []string) {
 	for i := 0; i < len(hosts); i++ {
 		t, err := common.GetNtpTime(hosts[i])
 		if err == nil {
