5edb950c02ae92a583f97f6f9171f89a91723796
vipwzw <vipwzw@vip.qq.com>
2021-04-19 16:05:26 +0800

    auto ci

--- types/mocks/chain33client.go
+++ types/mocks/chain33client.go
@@ -617,6 +617,36 @@ func (_m *Chain33Client) GetBlocks(ctx context.Context, in *types.ReqBlocks, opt
 	return r0, r1
 }
 
+// GetCryptoList provides a mock function with given fields: ctx, in, opts
+func (_m *Chain33Client) GetCryptoList(ctx context.Context, in *types.ReqNil, opts ...grpc.CallOption) (*types.CryptoList, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *types.CryptoList
+	if rf, ok := ret.Get(0).(func(context.Context, *types.ReqNil, ...grpc.CallOption) *types.CryptoList); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*types.CryptoList)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *types.ReqNil, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetFatalFailure provides a mock function with given fields: ctx, in, opts
 func (_m *Chain33Client) GetFatalFailure(ctx context.Context, in *types.ReqNil, opts ...grpc.CallOption) (*types.Int32, error) {
 	_va := make([]interface{}, len(opts))
