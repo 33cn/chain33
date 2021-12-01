10f2ea598f17788ea51c9cf17ccd40104bf153f9
jiangpeng <jiangp@33.cn>
2021-12-01 17:53:29 +0800

    dht:add broadcast validator

--- system/p2p/dht/protocol/broadcast/validate.go
+++ system/p2p/dht/protocol/broadcast/validate.go
@@ -8,76 +8,90 @@ import (
 	"bytes"
 	"context"
 	"encoding/hex"
+	"sync"
 	"sync/atomic"
 
 	"github.com/33cn/chain33/common/difficulty"
 	"github.com/33cn/chain33/types"
 	"github.com/libp2p/go-libp2p-core/peer"
-	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	ps "github.com/libp2p/go-libp2p-pubsub"
 )
 
-func (p *pubSub) addBlockHeader(header *types.Header) {
+type validator struct {
+	*pubSub
+	blkHeaderCache   map[int64]*types.Header
+	maxRecvBlkHeight int64
+	lock             sync.RWMutex
+}
 
-	p.blkHeaderCache[header.Height] = header
-	delete(p.blkHeaderCache, header.Height-blkHeaderCacheSize)
+func newValidator(p *pubSub) *validator {
+	v := &validator{pubSub: p}
+	v.blkHeaderCache = make(map[int64]*types.Header)
+	return v
+}
+
+func (v *validator) addBlockHeader(header *types.Header) {
+
+	v.blkHeaderCache[header.Height] = header
+	delete(v.blkHeaderCache, header.Height-blkHeaderCacheSize)
 	// 如果丢包，可能遗留未删除历史数据，控制缓存大小
-	if len(p.blkHeaderCache) >= 2*blkHeaderCacheSize {
-		for _, h := range p.blkHeaderCache {
+	if len(v.blkHeaderCache) >= 2*blkHeaderCacheSize {
+		for _, h := range v.blkHeaderCache {
 			if h.Height <= header.Height-blkHeaderCacheSize {
-				delete(p.blkHeaderCache, h.Height)
+				delete(v.blkHeaderCache, h.Height)
 			}
 		}
 	}
 }
 
-func (p *pubSub) validateBlock(ctx context.Context, id peer.ID, msg *pubsub.Message) pubsub.ValidationResult {
+func (v *validator) validateBlock(ctx context.Context, id peer.ID, msg *ps.Message) ps.ValidationResult {
 
-	if id == p.Host.ID() {
-		return pubsub.ValidationAccept
+	if id == v.Host.ID() {
+		return ps.ValidationAccept
 	}
 
 	block := &types.Block{}
-	err := p.decodeMsg(msg.Data, nil, block)
+	err := v.decodeMsg(msg.Data, nil, block)
 	if err != nil {
 		log.Error("validateBlock", "decodeMsg err", err)
-		return pubsub.ValidationReject
+		return ps.ValidationReject
 	}
 
-	blockHash := block.Hash(p.ChainCfg)
+	blockHash := block.Hash(v.ChainCfg)
 	blockHashHex := hex.EncodeToString(blockHash)
 	//重复检测
-	if p.blockFilter.AddWithCheckAtomic(blockHashHex, struct{}{}) {
+	if v.blockFilter.AddWithCheckAtomic(blockHashHex, struct{}{}) {
 		log.Debug("validateBlock", "recvDupBlk", block.GetHeight())
-		return pubsub.ValidationIgnore
+		return ps.ValidationIgnore
 	}
 
 	// 丢弃收到高度落后较多的区块
-	maxHeight := atomic.LoadInt64(&p.maxRecvBlkHeight)
+	maxHeight := atomic.LoadInt64(&v.maxRecvBlkHeight)
 	if block.GetHeight() <= maxHeight-int64(blkHeaderCacheSize) {
 		log.Debug("validateBlock", "recvHisBlk", block.GetHeight())
-		return pubsub.ValidationReject
+		return ps.ValidationReject
 	}
 
 	if block.GetHeight() > maxHeight {
-		atomic.StoreInt64(&p.maxRecvBlkHeight, block.GetHeight())
+		atomic.StoreInt64(&v.maxRecvBlkHeight, block.GetHeight())
 	}
 
-	p.lock.Lock()
-	defer p.lock.Unlock()
+	v.lock.Lock()
+	defer v.lock.Unlock()
 
 	// 分叉区块，选择性广播
-	if h, ok := p.blkHeaderCache[block.Height]; ok && bytes.Equal(h.ParentHash, block.ParentHash) {
+	if h, ok := v.blkHeaderCache[block.Height]; ok && bytes.Equal(h.ParentHash, block.ParentHash) {
 
 		log.Debug("validateBlock", "recvForkBlk", block.GetHeight())
 		// 区块时间小的优先
 		if block.BlockTime > h.BlockTime {
-			return pubsub.ValidationIgnore
+			return ps.ValidationIgnore
 		}
 		// 难度系数高的优先
 		if block.BlockTime == h.BlockTime &&
 			difficulty.CalcWork(block.Difficulty).Cmp(difficulty.CalcWork(h.Difficulty)) < 0 {
 
-			return pubsub.ValidationIgnore
+			return ps.ValidationIgnore
 		}
 	} else {
 		recvHeader := &types.Header{
@@ -87,33 +101,33 @@ func (p *pubSub) validateBlock(ctx context.Context, id peer.ID, msg *pubsub.Mess
 			Difficulty: block.Difficulty,
 			ParentHash: block.ParentHash,
 		}
-		p.addBlockHeader(recvHeader)
+		v.addBlockHeader(recvHeader)
 	}
 
 	log.Debug("validateBlock", "height", block.GetHeight(), "fromPid", msg.ReceivedFrom.String(),
 		"hash", blockHashHex)
 
-	return pubsub.ValidationAccept
+	return ps.ValidationAccept
 }
 
 //
-func (p *pubSub) validateTx(ctx context.Context, id peer.ID, msg *pubsub.Message) pubsub.ValidationResult {
+func (v *validator) validateTx(ctx context.Context, id peer.ID, msg *ps.Message) ps.ValidationResult {
 
-	if id == p.Host.ID() {
-		return pubsub.ValidationAccept
+	if id == v.Host.ID() {
+		return ps.ValidationAccept
 	}
 
 	tx := &types.Transaction{}
-	err := p.decodeMsg(msg.Data, nil, tx)
+	err := v.decodeMsg(msg.Data, nil, tx)
 	if err != nil {
 		log.Error("validateTx", "decodeMsg err", err)
-		return pubsub.ValidationReject
+		return ps.ValidationReject
 	}
 
 	//重复检测
-	if p.txFilter.AddWithCheckAtomic(hex.EncodeToString(tx.Hash()), struct{}{}) {
-		return pubsub.ValidationIgnore
+	if v.txFilter.AddWithCheckAtomic(hex.EncodeToString(tx.Hash()), struct{}{}) {
+		return ps.ValidationIgnore
 	}
 
-	return pubsub.ValidationAccept
+	return ps.ValidationAccept
 }
