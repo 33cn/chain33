0251e8c14cb1ea2bb4c868a3fdc09ddee68ee254
jiangpeng <11565373+bysomeone@users.noreply.github.com>
2022-02-08 18:19:12 +0800

    remove from address field for transaction

--- types/tx.go
+++ types/tx.go
@@ -883,7 +883,6 @@ func CloneTx(tx *Transaction) *Transaction {
 	copytx.Header = tx.Header
 	copytx.Next = tx.Next
 	copytx.ChainID = tx.ChainID
-	copytx.FromAddr = tx.FromAddr
 	return copytx
 }
 
