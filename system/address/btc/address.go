1a45b6d388c01b4ac6856c7aff91720e82b93556
jiangpeng <11565373+bysomeone@users.noreply.github.com>
2022-02-11 21:31:41 +0800

    add btc address driver

---
+++ system/address/btc/address.go
@@ -0,0 +1,102 @@
+package btc
+
+import (
+	"errors"
+	"github.com/33cn/chain33/common"
+	"github.com/33cn/chain33/common/address"
+	"github.com/decred/base58"
+	lru "github.com/hashicorp/golang-lru"
+	"strings"
+)
+
+const (
+	// NormalAddressID normal address id
+	NormalAddressID = 0
+	normalName      = "btc"
+	// MultiSignAddressID multi sign address id
+	MultiSignAddressID = 1
+	multiSignName      = "btc-multi-sign"
+)
+
+var (
+	normalAddrCache    *lru.Cache
+	multiSignAddrCache *lru.Cache
+	normalAddrPrefix string
+	multiSignAddrPrefix string
+	// ErrInvalidAddrFormat invalid address format
+	ErrInvalidAddrFormat = errors.New("ErrInvalidAddrFormat")
+)
+
+func init() {
+	address.RegisterDriver(NormalAddressID, &btc{}, 0)
+	address.RegisterDriver(MultiSignAddressID, &btcMultiSign{}, 0)
+
+	var err error
+	multiSignAddrCache, err = lru.New(10240)
+	if err != nil {
+		panic(err)
+	}
+	normalAddrCache, err = lru.New(10240)
+	if err != nil {
+		panic(err)
+	}
+}
+
+type btc struct{}
+
+// PubKeyToAddr public key to address
+func (b *btc) PubKeyToAddr(pubKey []byte) string {
+	pubStr := string(pubKey)
+	if value, ok := normalAddrCache.Get(pubStr); ok {
+		return value.(string)
+	}
+	addr := formatBtcAddr(address.NormalVer, pubKey)
+	normalAddrCache.Add(pubStr, addr)
+	return addr
+}
+
+// ValidateAddr address validation
+func (b *btc) ValidateAddr(addr string) error {
+	return address.CheckBtcAddress(addr)
+}
+
+// GetName get driver name
+func (b *btc) GetName(id int32) string {
+	return normalName
+}
+
+type btcMultiSign struct{}
+
+// PubKeyToAddr public key to address
+func (b *btcMultiSign) PubKeyToAddr(pubKey []byte) string {
+
+	pubStr := string(pubKey)
+	if value, ok := multiSignAddrCache.Get(pubStr); ok {
+		return value.(string)
+	}
+	addr := formatBtcAddr(address.MultiSignVer, pubKey)
+	multiSignAddrCache.Add(pubStr, addr)
+	return addr
+}
+
+// ValidateAddr address validation
+func (b *btcMultiSign) ValidateAddr(addr string) error {
+	if !strings.HasPrefix(addr, "3") {
+		return ErrInvalidAddrFormat
+	}
+	return address.CheckBtcMultiSignAddress(addr)
+}
+
+// GetName get driver name
+func (b *btcMultiSign) GetName(id int32) string {
+	return multiSignName
+}
+
+func formatBtcAddr(version byte, pubKey []byte) string {
+
+	var ad [25]byte
+	ad[0] = version
+	copy(ad[1:21], common.Rimp160(pubKey))
+	copy(ad[21:25], common.Sha2Sum(ad[0:21])[:4])
+	return base58.Encode(ad[:])
+}
