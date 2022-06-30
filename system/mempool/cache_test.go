59e409aee30fa1493f28d0589ea61f1177cd3a81
jiangpeng <11565373+bysomeone@users.noreply.github.com>
2022-06-30 14:21:50 +0800

    fix delay tx test

--- system/mempool/cache_test.go
+++ system/mempool/cache_test.go
@@ -7,6 +7,7 @@ package mempool
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/33cn/chain33/types"
 	"github.com/stretchr/testify/require"
@@ -42,10 +43,11 @@ func Test_delDelayTx(t *testing.T) {
 		})
 		require.Nilf(t, err, "payload:%s, delay:%d", payload, delay)
 	}
+	beginTime := time.Now().Unix()
 	//add delay tx
 	for i := 0; i < 10; i++ {
 		addFunc(fmt.Sprintf("height%d", i), i)
-		addFunc(fmt.Sprintf("time%d", i), int(types.ExpireBound)+1+i)
+		addFunc(fmt.Sprintf("time%d", i), int(beginTime)+i)
 	}
 	//no match tx
 	txList := cache.delExpiredTxs(0, 0, -1)
@@ -54,15 +56,15 @@ func Test_delDelayTx(t *testing.T) {
 	txList = cache.delExpiredTxs(0, 0, 0)
 	require.Equal(t, 1, len(txList))
 	require.Equal(t, []byte("height0"), txList[0].Payload)
-	//del delay time <= expireBound+5, height=1
-	txList = cache.delExpiredTxs(0, types.ExpireBound+5, 1)
-	require.Equal(t, 6, len(txList))
+	//del delay time <= beginTime+5, height=1
+	txList = cache.delExpiredTxs(beginTime-1, beginTime+5, 1)
+	require.Equal(t, 7, len(txList))
 	require.Equal(t, []byte("time0"), txList[0].Payload)
-	require.Equal(t, []byte("height1"), txList[5].Payload)
+	require.Equal(t, []byte("height1"), txList[6].Payload)
 
-	//del delay block time <= types.ExpireBount+100
-	txList = cache.delExpiredTxs(0, types.ExpireBound+100, 0)
-	require.Equal(t, 5, len(txList))
+	//del delay block time <= beginTime+100
+	txList = cache.delExpiredTxs(beginTime-1, beginTime+100, 0)
+	require.Equal(t, 4, len(txList))
 	//check delay height=2, 5 tx
 	_, exist := cache.contains((&types.Transaction{Payload: []byte("height1")}).Hash())
 	require.False(t, exist)
