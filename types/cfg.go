0f5d33bb278f6bad2614a2040a7911770e83df35
libangzhu <601276804@qq.com>
2023-05-15 15:42:20 +0800

    fix data race

--- types/cfg.go
+++ types/cfg.go
@@ -118,7 +118,7 @@ type Mempool struct {
 	MaxTxFee int64 `json:"maxTxFee,omitempty"`
 	// 目前execCheck效率较低，支持关闭交易execCheck，提升性能
 	DisableExecCheck bool `json:"disableExecCheck,omitempty"`
-	//开启ETH交易类型的检查
+	// Deprecated:default enable check
 	EnableEthCheck bool `json:"enableEthCheck,omitempty"`
 }
 
