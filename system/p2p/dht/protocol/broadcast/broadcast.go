0f62d5ea69d6e3c578eac9c0363b72e1517f62a3
jiangpeng <11565373+bysomeone@users.noreply.github.com>
2023-06-01 15:43:45 +0800

    record error broadcast count and make corrsponding punishment

--- system/p2p/dht/protocol/broadcast/broadcast.go
+++ system/p2p/dht/protocol/broadcast/broadcast.go
@@ -278,7 +278,7 @@ func (p *broadcastProtocol) pubPeerMsg(peerID peer.ID, msgID int32, msg types.Me
 }
 
 func (p *broadcastProtocol) postBlockChain(blockHash, receiveFrom string, block *types.Block, publisher peer.ID) error {
-	msg, err := p.P2PManager.PubBroadCast(blockHash, &types.BlockPid{Pid: receiveFrom, Block: block}, types.EventBroadcastAddBlock)
+	msg, err := p.P2PManager.PubBroadCast(blockHash, &types.BlockPid{Pid: publisher.Pretty(), Block: block}, types.EventBroadcastAddBlock)
 	if err == nil {
 		p.val.addBroadcastMsg(&broadcastMsg{msg: msg, publisher: publisher, hash: blockHash})
 	}
