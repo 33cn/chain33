3f1f4ffa51293bcca08b65ffbe7a0de4b26cf690
libangzhu <601276804@qq.com>
2023-06-05 19:45:48 +0800

    update testcase

--- system/p2p/dht/protocol/peer/peerinfo_test.go
+++ system/p2p/dht/protocol/peer/peerinfo_test.go
@@ -153,54 +153,6 @@ func testMempoolReq(q queue.Queue) {
 	time.Sleep(time.Second)
 }
 
-func TestCheckVerisonLimit(t *testing.T) {
-	q := queue.New("test-x")
-	p, cancel := initEnv(t, q)
-	defer cancel()
-
-	testV0 := "1.67.1-64139470@5.7.0"
-	testV1 := "1.67.1-64139470@6.7.0"
-	testV2 := "1.67.3-5b3c44b5@6.8.0"
-	testV3 := "1.68.0-9d3383c5@6.8.8"
-	testV4 := "1.68.0-a612c9a6@6.8.9"
-	testV5 := "1.68.0-a612c9a6"
-	testV6 := "1.68.0-a612c9a6@6.8"
-	testV7 := "1.68.0-a612c9a6@"
-	//初始状态VerLimit 为空
-	var testVs []string
-	testVs = append(testVs, testV1, testV2, testV3, testV4, testV5, testV6, testV7)
-	for _, testV := range testVs {
-		isAllow := p.checkVerisonLimit(testV)
-		require.True(t, isAllow)
-	}
-	p.SubConfig.VerLimit = "6.8.8"
-	isAllow := p.checkVerisonLimit(testV0)
-	require.False(t, isAllow)
-	isAllow = p.checkVerisonLimit(testV1)
-	require.False(t, isAllow)
-	isAllow = p.checkVerisonLimit(testV2)
-	require.False(t, isAllow)
-	isAllow = p.checkVerisonLimit(testV3)
-	require.True(t, isAllow)
-	isAllow = p.checkVerisonLimit(testV4)
-	require.True(t, isAllow)
-	isAllow = p.checkVerisonLimit(testV5)
-	require.False(t, isAllow)
-	isAllow = p.checkVerisonLimit(testV6)
-	require.False(t, isAllow)
-	isAllow = p.checkVerisonLimit(testV7)
-	require.False(t, isAllow)
-	p.SubConfig.VerLimit = "6.8.9"
-	for i, testV := range testVs {
-		isAllow := p.checkVerisonLimit(testV)
-		if i == 3 {
-			require.True(t, isAllow)
-			continue
-		}
-		require.False(t, isAllow)
-	}
-}
-
 func TestPeerInfoHandler(t *testing.T) {
 	q := queue.New("test")
 	p, cancel := initEnv(t, q)
@@ -259,6 +211,8 @@ func TestPeerInfoHandler(t *testing.T) {
 	fmt.Println(p.PeerInfoManager.FetchAll())
 	fmt.Println(p.Host.ID())
 	require.Equal(t, 2, len(p.PeerInfoManager.FetchAll()))
+	//test checkVerionLimit
+	testCheckVerisonLimit(t, p)
 }
 
 func Test_isPublicIP(t *testing.T) {
@@ -327,3 +281,47 @@ func (c *connManager) BandTrackerByProtocol() *types.NetProtocolInfos {
 	return &types.NetProtocolInfos{}
 }
 func (c *connManager) RateCalculate(ratebytes float64) string { return "" }
+
+func testCheckVerisonLimit(t *testing.T, p *Protocol) {
+	testV0 := "1.67.1-64139470@5.7.0"
+	testV1 := "1.67.1-64139470@6.7.0"
+	testV2 := "1.67.3-5b3c44b5@6.8.0"
+	testV3 := "1.68.0-9d3383c5@6.8.8"
+	testV4 := "1.68.0-a612c9a6@6.8.9"
+	testV5 := "1.68.0-a612c9a6"
+	testV6 := "1.68.0-a612c9a6@6.8"
+	testV7 := "1.68.0-a612c9a6@"
+	//初始状态VerLimit 为空
+	var testVs []string
+	testVs = append(testVs, testV1, testV2, testV3, testV4, testV5, testV6, testV7)
+	for _, testV := range testVs {
+		isAllow := p.checkVerisonLimit(testV)
+		require.True(t, isAllow)
+	}
+	p.SubConfig.VerLimit = "6.8.8"
+	isAllow := p.checkVerisonLimit(testV0)
+	require.False(t, isAllow)
+	isAllow = p.checkVerisonLimit(testV1)
+	require.False(t, isAllow)
+	isAllow = p.checkVerisonLimit(testV2)
+	require.False(t, isAllow)
+	isAllow = p.checkVerisonLimit(testV3)
+	require.True(t, isAllow)
+	isAllow = p.checkVerisonLimit(testV4)
+	require.True(t, isAllow)
+	isAllow = p.checkVerisonLimit(testV5)
+	require.False(t, isAllow)
+	isAllow = p.checkVerisonLimit(testV6)
+	require.False(t, isAllow)
+	isAllow = p.checkVerisonLimit(testV7)
+	require.False(t, isAllow)
+	p.SubConfig.VerLimit = "6.8.9"
+	for i, testV := range testVs {
+		isAllow := p.checkVerisonLimit(testV)
+		if i == 3 {
+			require.True(t, isAllow)
+			continue
+		}
+		require.False(t, isAllow)
+	}
+}
