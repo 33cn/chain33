fe776d22afcd256c1a271035f50102805c16e6e9
jiangpeng <11565373+bysomeone@users.noreply.github.com>
2023-11-14 17:32:14 +0800

    add snow config construct

--- system/consensus/snowman/config.go
+++ system/consensus/snowman/config.go
@@ -8,11 +8,13 @@ import (
 	"github.com/33cn/chain33/types"
 	"github.com/ava-labs/avalanchego/snow"
 	"github.com/ava-labs/avalanchego/snow/consensus/snowball"
-	"github.com/ava-labs/avalanchego/snow/consensus/snowman"
+	smcon "github.com/ava-labs/avalanchego/snow/consensus/snowman"
 	"github.com/ava-labs/avalanchego/snow/engine/common"
+	smeng "github.com/ava-labs/avalanchego/snow/engine/snowman"
 	"github.com/ava-labs/avalanchego/snow/validators"
 	"github.com/ava-labs/avalanchego/utils/logging"
 	"github.com/prometheus/client_golang/prometheus"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 // Config wraps all the parameters needed for a snowman engine
@@ -29,5 +31,42 @@ type Config struct {
 	//Sender     common.Sender
 	validators validators.Set
 	params     snowball.Parameters
-	consensus  snowman.Consensus
+	consensus  smcon.Consensus
+}
+
+func newSnowmanConfig(vm *chain33VM, params snowball.Parameters, ctx *snow.ConsensusContext) smeng.Config {
+
+	engineConfig := smeng.Config{
+		Ctx:         ctx,
+		VM:          vm,
+		Validators:  newVdrSet(),
+		Params:      params,
+		Consensus:   &smcon.Topological{},
+		PartialSync: false,
+		Sender:      newMsgSender(ctx),
+	}
+
+	return engineConfig
+}
+
+func newSnowContext(cfg *types.Chain33Config) *snow.ConsensusContext {
+
+	ctx := snow.DefaultConsensusContextTest()
+
+	// TODO use chain33 log writer
+	logCfg := cfg.GetModuleConfig().Log
+
+	logger := &lumberjack.Logger{
+		Filename:   logCfg.LogFile,
+		MaxSize:    int(logCfg.MaxFileSize),
+		MaxBackups: int(logCfg.MaxBackups),
+		MaxAge:     int(logCfg.MaxAge),
+		LocalTime:  logCfg.LocalTime,
+		Compress:   logCfg.Compress,
+	}
+
+	fileCore := logging.NewWrappedCore(logging.Debug, logger, logging.Colors.FileEncoder())
+	ctx.Context.Log = logging.NewLogger("", fileCore)
+
+	return ctx
 }
