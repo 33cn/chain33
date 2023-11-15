7fcc2a0f7f538ad8c8a3e8618ccde2daf5800f53
jiangpeng <11565373+bysomeone@users.noreply.github.com>
2023-11-15 17:37:01 +0800

    add snowman finalizer

--- system/consensus/snowman/finalize.go
+++ system/consensus/snowman/finalize.go
@@ -7,15 +7,58 @@ package snowman
 
 import (
 	"github.com/33cn/chain33/common/log/log15"
+	"github.com/33cn/chain33/queue"
 	"github.com/33cn/chain33/system/consensus"
+	"github.com/ava-labs/avalanchego/snow/consensus/snowball"
+
+	smeng "github.com/ava-labs/avalanchego/snow/engine/snowman"
 )
 
 var (
 	log = log15.New("module", "snowman")
+
+	_ consensus.Finalizer = (*snowman)(nil)
 )
 
 func init() {
 
-	consensus.RegFinalizer("snowman", &Transitive{})
+	consensus.RegFinalizer("snowman", &snowman{})
 
+}
+
+type snowman struct {
+	engine smeng.Engine
+	ctx    *consensus.Context
+}
+
+func (s *snowman) Initialize(ctx *consensus.Context) error {
+
+	params := snowball.Parameters{}
+	engineConfig := newSnowmanConfig(&chain33VM{}, params, newSnowContext(ctx.Base.GetAPI().GetConfig()))
+
+	engine, err := smeng.New(engineConfig)
+
+	if err != nil {
+		panic("Initialize snowman engine err:" + err.Error())
+	}
+	s.engine = engine
+
+	return nil
+
+}
+
+func (s *snowman) Start() error {
+
+	err := s.engine.Start(s.ctx.Base.Context, 0)
+
+	if err != nil {
+
+		return err
+	}
+	return nil
+
+}
+func (s *snowman) ProcessMsg(msg *queue.Message) (processed bool) {
+
+	return false
 }
