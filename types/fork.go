176df2a3819a7864581aa0102aaeef83a68e3b58
libangzhu <601276804@qq.com>
2024-01-17 11:51:18 +0800

    update test

--- types/fork.go
+++ types/fork.go
@@ -145,7 +145,7 @@ func (f *Forks) SetTestNetFork() {
 	f.setFork("ForkCheckEthTxSort", 0)
 	f.setFork("ForkProxyExec", 0)
 	f.setFork("ForkMaxTxFeeV1", 0)
-	f.setFork("ForkMaxTxFeeV2", 0)
+
 }
 
 func (f *Forks) setLocalFork() {
