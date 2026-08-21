aab7c9738f7922022318387647d8a6a162c48152
king <king@kingdeMacBook-Pro-2.local>
2026-08-21 10:56:04 +0800

    fix(coins): add missing ExecDelLocal_Genesis to rollback genesis receiver stats
    ExecLocal_Genesis writes the address receiver stat (LODB-coins-Addr:*)
    but exec_del_local.go had no matching ExecDelLocal_Genesis. Rolling back
    a height-0 block (reorg / index rebuild) therefore left the stat stale,
    making local data inconsistent with chain state. Mirror ExecLocal_Genesis
    with isadd=false, consistent with the other ExecDelLocal_* methods.

---
+++ system/dapp/coins/executor/exec_del_local_test.go
@@ -0,0 +1,94 @@
+// Copyright Fuzamei Corp. 2018 All Rights Reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package executor
+
+import (
+	"testing"
+
+	"github.com/33cn/chain33/client"
+	"github.com/33cn/chain33/common/crypto"
+	"github.com/33cn/chain33/common/db"
+	"github.com/33cn/chain33/queue"
+	dtypes "github.com/33cn/chain33/system/dapp/coins/types"
+	"github.com/33cn/chain33/types"
+	"github.com/33cn/chain33/util"
+	"github.com/stretchr/testify/require"
+)
+
+var testCfg = types.NewChain33Config(types.GetDefaultCfgstring())
+
+const testCoinUnit = int64(1e8)
+
+func init() {
+	Init(driverName, testCfg, nil)
+}
+
+func initTestCoins(t *testing.T) (string, *Coins) {
+	q := queue.New("testcoinsdellocal")
+	q.SetConfig(testCfg)
+	api, err := client.New(q.Client(), nil)
+	require.Nil(t, err)
+	dbDir, stateDB, kvDB := util.CreateTestDB()
+	c := newCoins()
+	c.SetAPI(api)
+	c.SetStateDB(stateDB)
+	c.SetLocalDB(kvDB)
+	return dbDir, c.(*Coins)
+}
+
+func closeTestCoins(t *testing.T, dbDir string, c *Coins) {
+	util.CloseTestDB(dbDir, c.GetStateDB().(db.DB))
+}
+
+func signCoinsTx(priv crypto.PrivKey, to string, action *dtypes.CoinsAction) *types.Transaction {
+	tx := &types.Transaction{
+		Execer:  []byte("coins"),
+		Payload: types.Encode(action),
+		To:      to,
+		Fee:     int64(1e6),
+	}
+	tx.Sign(types.SECP256K1, priv)
+	return tx
+}
+
+func genesisAction(amount int64) *dtypes.CoinsAction {
+	return &dtypes.CoinsAction{
+		Ty:    dtypes.CoinsActionGenesis,
+		Value: &dtypes.CoinsAction_Genesis{Genesis: &types.AssetsGenesis{Amount: amount}},
+	}
+}
+
+// TestExecDelLocalGenesis checks that ExecDelLocal of a genesis tx reverts the
+// address receiver stat (LODB-coins-Addr:*) written by ExecLocal, so rolling
+// back a block at height 0 leaves no stale local data.
+func TestExecDelLocalGenesis(t *testing.T) {
+	dbDir, c := initTestCoins(t)
+	defer closeTestCoins(t, dbDir, c)
+
+	addr, _ := util.Genaddress()
+	_, priv := util.Genaddress()
+	c.SetEnv(0, 1539918074, 1)
+
+	tx := signCoinsTx(priv, addr, genesisAction(100*testCoinUnit))
+	receiptData := &types.ReceiptData{Ty: types.ExecOk}
+
+	// ExecLocal writes the address receiver stat
+	lset, err := c.ExecLocal(tx, receiptData, 0)
+	require.Nil(t, err)
+	require.Equal(t, 1, len(lset.KV))
+	require.Nil(t, c.GetLocalDB().Set(lset.KV[0].Key, lset.KV[0].Value))
+	recv, err := getAddrReciver(c.GetLocalDB(), addr)
+	require.Nil(t, err)
+	require.Equal(t, 100*testCoinUnit, recv)
+
+	// ExecDelLocal must produce the rollback KV, and applying it reverts the stat
+	delSet, err := c.ExecDelLocal(tx, receiptData, 0)
+	require.Nil(t, err)
+	require.Equal(t, 1, len(delSet.KV))
+	require.Nil(t, c.GetLocalDB().Set(delSet.KV[0].Key, delSet.KV[0].Value))
+	recv, err = getAddrReciver(c.GetLocalDB(), addr)
+	require.Nil(t, err)
+	require.Equal(t, int64(0), recv)
+}
